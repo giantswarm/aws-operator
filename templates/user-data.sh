253c36534d0ccdfbf07853f147158419060e27c5
Iago López Galeiras <iago@kinvolk.io>
2017-03-14 11:38:38 +0100

    templates: don't use latest for quay.io/coreos/awscli

--- templates/user-data.sh
+++ templates/user-data.sh
@@ -8,6 +8,6 @@ USERDATA_FILE={{.MachineType}}
     --volume=dns,kind=host,source=/etc/resolv.conf,readOnly=true --mount volume=dns,target=/etc/resolv.conf  \
     --volume=awsenv,kind=host,source=/var/run/coreos,readOnly=false --mount volume=awsenv,target=/var/run/coreos \
     --trust-keys-from-https \
-    quay.io/coreos/awscli:latest -- aws s3 --region {{.Region}} cp s3://{{.S3Bucket}}/{{.ClusterID}}/cloudconfig/$USERDATA_FILE /var/run/coreos/temp.txt
+    quay.io/coreos/awscli:025a357f05242fdad6a81e8a6b520098aa65a600 -- aws s3 --region {{.Region}} cp s3://{{.S3Bucket}}/{{.ClusterID}}/cloudconfig/$USERDATA_FILE /var/run/coreos/temp.txt
 base64 -d /var/run/coreos/temp.txt | gunzip > /var/run/coreos/$USERDATA_FILE
 exec /usr/bin/coreos-cloudinit --from-file /var/run/coreos/$USERDATA_FILE
