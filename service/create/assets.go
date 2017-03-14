73b2f4c2db8e17b029d5b79540defa8a70d52c51
Iago López Galeiras <iago@kinvolk.io>
2017-03-14 14:34:42 +0100

    client: initialize clients at the beginning (#46)
    * vendor: add s3 package
    
    * client: initialize clients at the beginning
    
    For the services we're gonna use, let's initialize their clients only
    once and at the beginning.

--- service/create/assets.go
+++ service/create/assets.go
@@ -1,18 +1,18 @@
 package create
 
 import (
-	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/kms"
 	"github.com/giantswarm/k8scloudconfig"
 	microerror "github.com/giantswarm/microkit/error"
 )
 
-func (s *Service) encodeTLSAssets(awsSession *session.Session, kmsKeyArn string) (*cloudconfig.CompactTLSAssets, error) {
+func (s *Service) encodeTLSAssets(svc *kms.KMS, kmsKeyArn string) (*cloudconfig.CompactTLSAssets, error) {
 	rawTLS, err := readRawTLSAssets(s.certsDir)
 	if err != nil {
 		return nil, microerror.MaskAny(err)
 	}
 
-	encTLS, err := rawTLS.encrypt(awsSession, kmsKeyArn)
+	encTLS, err := rawTLS.encrypt(svc, kmsKeyArn)
 	if err != nil {
 		return nil, microerror.MaskAny(err)
 	}
