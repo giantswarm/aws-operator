98c66c224080f30cf7656e02c11b4dce7ad1adaf
Michal Rostecki <michal@kinvolk.io>
2017-04-13 14:17:39 +0200

    resources: Add alias for KMS keys
    Fixes #106

--- resources/aws/kms.go
+++ resources/aws/kms.go
@@ -3,13 +3,19 @@ package aws
 import (
 	"fmt"
 
+	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/service/kms"
 	microerror "github.com/giantswarm/microkit/error"
 )
 
 type KMSKey struct {
-	arn string
+	Name string
+	arn  string
 	AWSEntity
+}
+
+func (kk KMSKey) fullAlias() string {
+	return fmt.Sprintf("alias/%s", kk.Name)
 }
 
 func (kk *KMSKey) CreateIfNotExists() (bool, error) {
@@ -21,14 +27,44 @@ func (kk *KMSKey) CreateOrFail() error {
 	if err != nil {
 		return microerror.MaskAny(err)
 	}
+
+	if _, err := kk.Clients.KMS.CreateAlias(&kms.CreateAliasInput{
+		// Alias names need to start from "alias/" prefix.
+		AliasName:   aws.String(kk.fullAlias()),
+		TargetKeyId: key.KeyMetadata.Arn,
+	}); err != nil {
+		return microerror.MaskAny(err)
+	}
+
 	kk.arn = *key.KeyMetadata.Arn
+
 	return nil
 }
 
-func (kms *KMSKey) Delete() error {
-	return microerror.MaskAny(notImplementedMethodError)
+func (kk *KMSKey) Delete() error {
+	key, err := kk.Clients.KMS.DescribeKey(&kms.DescribeKeyInput{
+		KeyId: aws.String(kk.fullAlias()),
+	})
+	if err != nil {
+		return microerror.MaskAny(err)
+	}
+
+	if _, err := kk.Clients.KMS.DeleteAlias(&kms.DeleteAliasInput{
+		AliasName: aws.String(kk.fullAlias()),
+	}); err != nil {
+		return microerror.MaskAny(err)
+	}
+
+	// AWS API doesn't allow to delete the KMS key immediately, but we can schedule its deletion
+	if _, err := kk.Clients.KMS.ScheduleKeyDeletion(&kms.ScheduleKeyDeletionInput{
+		KeyId: key.KeyMetadata.KeyId,
+	}); err != nil {
+		return microerror.MaskAny(err)
+	}
+
+	return nil
 }
 
-func (kms *KMSKey) Arn() string {
-	return kms.arn
+func (kk KMSKey) Arn() string {
+	return kk.arn
 }
