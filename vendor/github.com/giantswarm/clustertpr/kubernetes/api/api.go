c33fdea73b95d6df3c89433c3d19d12329ed9d01
Michal Rostecki <michal@kinvolk.io>
2017-04-25 11:33:59 +0200

    vendor: Update vendor

--- vendor/github.com/giantswarm/clustertpr/kubernetes/api/api.go
+++ vendor/github.com/giantswarm/clustertpr/kubernetes/api/api.go
@@ -7,15 +7,15 @@ import (
 type API struct {
 	// AltNames is the alternative names used to generate certificates for the
 	// Kubernetes API server.
-	AltNames string `json:"alt_names" yaml:"altNames"`
+	AltNames string `json:"altNames" yaml:"altNames"`
 	// ClusterIpRange is the value for command line flag
 	// --service-cluster-ip-range of the Kubernetes API server, e.g. 10.0.3.0/24.
-	ClusterIPRange string `json:"cluster_ip_range" yaml:"clusterIPRange"`
+	ClusterIPRange string `json:"clusterIPRange" yaml:"clusterIPRange"`
 	// Domain is the API domain of the Kubernetes cluster, e.g.
 	// api.<cluster-id>.g8s.fra-1.giantswarm.io.
-	Domain       string `json:"domain" yaml:"domain"`
-	InsecurePort int    `json:"insecure_port" yaml:"insecurePort"`
+	Domain string `json:"domain" yaml:"domain"`
 	// IP is the Kubernetes API IP, e.g. 172.29.0.1.
-	IP         net.IP `json:"ip" yaml:"ip"`
-	SecurePort int    `json:"secure_port" yaml:"securePort"`
+	IP           net.IP `json:"ip" yaml:"ip"`
+	InsecurePort int    `json:"insecurePort" yaml:"insecurePort"`
+	SecurePort   int    `json:"securePort" yaml:"securePort"`
 }
