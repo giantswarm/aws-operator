dca846d3c4b5e0c490141dda3906df76d8b836d3
rossf7 <ross@giantswarm.io>
2017-05-02 19:36:07 +0200

    Add calico env file and use internal IP for bridge

--- service/create/templates.go
+++ service/create/templates.go
@@ -69,4 +69,7 @@ USERDATA_FILE={{.MachineType}}
     quay.io/coreos/awscli:025a357f05242fdad6a81e8a6b520098aa65a600 -- aws s3 --region {{.Region}} cp s3://{{.S3DirURI}}/$USERDATA_FILE /var/run/coreos/temp.txt
 base64 -d /var/run/coreos/temp.txt | gunzip > /var/run/coreos/$USERDATA_FILE
 exec /usr/bin/coreos-cloudinit --from-file /var/run/coreos/$USERDATA_FILE`
+
+	calicoEnvironmentFileTemplate = `# On AWS use internal IP as bridge IP
+BRIDGE_IP=$DEFAULT_IPV4`
 )
