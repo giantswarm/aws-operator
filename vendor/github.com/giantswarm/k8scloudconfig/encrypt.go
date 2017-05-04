8129edb5c1158b14522a5fb3dbbdf8bf36788b7e
rossf7 <ross@giantswarm.io>
2017-05-04 15:25:00 +0200

    vendor: updated certificatetpr and k8scloudconfig

--- vendor/github.com/giantswarm/k8scloudconfig/encrypt.go
+++ vendor/github.com/giantswarm/k8scloudconfig/encrypt.go
@@ -1,16 +1,19 @@
 package cloudconfig
 
 type CompactTLSAssets struct {
-	APIServerCA     string
-	APIServerKey    string
-	APIServerCrt    string
-	WorkerCA        string
-	WorkerKey       string
-	WorkerCrt       string
-	CalicoClientCA  string
-	CalicoClientKey string
-	CalicoClientCrt string
-	EtcdServerCA    string
-	EtcdServerKey   string
-	EtcdServerCrt   string
+	APIServerCA       string
+	APIServerKey      string
+	APIServerCrt      string
+	WorkerCA          string
+	WorkerKey         string
+	WorkerCrt         string
+	ServiceAccountCA  string
+	ServiceAccountKey string
+	ServiceAccountCrt string
+	CalicoClientCA    string
+	CalicoClientKey   string
+	CalicoClientCrt   string
+	EtcdServerCA      string
+	EtcdServerKey     string
+	EtcdServerCrt     string
 }
