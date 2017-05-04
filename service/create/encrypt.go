61544e5b935ee7a6f7c05b2c98131a3951a6db2e
rossf7 <ross@giantswarm.io>
2017-05-04 15:25:59 +0200

    Add service-account to TLS assets and encrypt them

--- service/create/encrypt.go
+++ service/create/encrypt.go
@@ -13,18 +13,21 @@ import (
 )
 
 type TLSassets struct {
-	APIServerCA     []byte
-	APIServerKey    []byte
-	APIServerCrt    []byte
-	WorkerCA        []byte
-	WorkerKey       []byte
-	WorkerCrt       []byte
-	CalicoClientCA  []byte
-	CalicoClientKey []byte
-	CalicoClientCrt []byte
-	EtcdServerCA    []byte
-	EtcdServerKey   []byte
-	EtcdServerCrt   []byte
+	APIServerCA       []byte
+	APIServerKey      []byte
+	APIServerCrt      []byte
+	WorkerCA          []byte
+	WorkerKey         []byte
+	WorkerCrt         []byte
+	ServiceAccountCA  []byte
+	ServiceAccountKey []byte
+	ServiceAccountCrt []byte
+	CalicoClientCA    []byte
+	CalicoClientKey   []byte
+	CalicoClientCrt   []byte
+	EtcdServerCA      []byte
+	EtcdServerKey     []byte
+	EtcdServerCrt     []byte
 }
 
 // PEM encoded TLS assets.
@@ -36,18 +39,21 @@ type encryptedTLSAssets TLSassets
 func createRawTLSAssets(assets certificatetpr.AssetsBundle) *rawTLSAssets {
 	// TODO refactor this with a for loop iterating over components and asset types
 	return &rawTLSAssets{
-		APIServerCA:     assets[certificatetpr.AssetsBundleKey{certificatetpr.APIComponent, certificatetpr.CA}],
-		APIServerCrt:    assets[certificatetpr.AssetsBundleKey{certificatetpr.APIComponent, certificatetpr.Crt}],
-		APIServerKey:    assets[certificatetpr.AssetsBundleKey{certificatetpr.APIComponent, certificatetpr.Key}],
-		WorkerCA:        assets[certificatetpr.AssetsBundleKey{certificatetpr.WorkerComponent, certificatetpr.CA}],
-		WorkerCrt:       assets[certificatetpr.AssetsBundleKey{certificatetpr.WorkerComponent, certificatetpr.Crt}],
-		WorkerKey:       assets[certificatetpr.AssetsBundleKey{certificatetpr.WorkerComponent, certificatetpr.Key}],
-		EtcdServerCA:    assets[certificatetpr.AssetsBundleKey{certificatetpr.EtcdComponent, certificatetpr.CA}],
-		EtcdServerCrt:   assets[certificatetpr.AssetsBundleKey{certificatetpr.EtcdComponent, certificatetpr.Crt}],
-		EtcdServerKey:   assets[certificatetpr.AssetsBundleKey{certificatetpr.EtcdComponent, certificatetpr.Key}],
-		CalicoClientCA:  assets[certificatetpr.AssetsBundleKey{certificatetpr.CalicoComponent, certificatetpr.CA}],
-		CalicoClientCrt: assets[certificatetpr.AssetsBundleKey{certificatetpr.CalicoComponent, certificatetpr.Crt}],
-		CalicoClientKey: assets[certificatetpr.AssetsBundleKey{certificatetpr.CalicoComponent, certificatetpr.Key}],
+		APIServerCA:       assets[certificatetpr.AssetsBundleKey{certificatetpr.APIComponent, certificatetpr.CA}],
+		APIServerCrt:      assets[certificatetpr.AssetsBundleKey{certificatetpr.APIComponent, certificatetpr.Crt}],
+		APIServerKey:      assets[certificatetpr.AssetsBundleKey{certificatetpr.APIComponent, certificatetpr.Key}],
+		WorkerCA:          assets[certificatetpr.AssetsBundleKey{certificatetpr.WorkerComponent, certificatetpr.CA}],
+		WorkerCrt:         assets[certificatetpr.AssetsBundleKey{certificatetpr.WorkerComponent, certificatetpr.Crt}],
+		WorkerKey:         assets[certificatetpr.AssetsBundleKey{certificatetpr.WorkerComponent, certificatetpr.Key}],
+		ServiceAccountCA:  assets[certificatetpr.AssetsBundleKey{certificatetpr.ServiceAccountComponent, certificatetpr.CA}],
+		ServiceAccountCrt: assets[certificatetpr.AssetsBundleKey{certificatetpr.ServiceAccountComponent, certificatetpr.Crt}],
+		ServiceAccountKey: assets[certificatetpr.AssetsBundleKey{certificatetpr.ServiceAccountComponent, certificatetpr.Key}],
+		EtcdServerCA:      assets[certificatetpr.AssetsBundleKey{certificatetpr.EtcdComponent, certificatetpr.CA}],
+		EtcdServerCrt:     assets[certificatetpr.AssetsBundleKey{certificatetpr.EtcdComponent, certificatetpr.Crt}],
+		EtcdServerKey:     assets[certificatetpr.AssetsBundleKey{certificatetpr.EtcdComponent, certificatetpr.Key}],
+		CalicoClientCA:    assets[certificatetpr.AssetsBundleKey{certificatetpr.CalicoComponent, certificatetpr.CA}],
+		CalicoClientCrt:   assets[certificatetpr.AssetsBundleKey{certificatetpr.CalicoComponent, certificatetpr.Crt}],
+		CalicoClientKey:   assets[certificatetpr.AssetsBundleKey{certificatetpr.CalicoComponent, certificatetpr.Key}],
 	}
 }
 
@@ -70,18 +76,21 @@ func (r *rawTLSAssets) encrypt(svc *kms.KMS, kmsKeyARN string) (*encryptedTLSAss
 		return encryptOutput.CiphertextBlob
 	}
 	encryptedAssets := encryptedTLSAssets{
-		APIServerCA:     encrypt(r.APIServerCA),
-		APIServerKey:    encrypt(r.APIServerKey),
-		APIServerCrt:    encrypt(r.APIServerCrt),
-		WorkerCA:        encrypt(r.WorkerCA),
-		WorkerCrt:       encrypt(r.WorkerCrt),
-		WorkerKey:       encrypt(r.WorkerKey),
-		CalicoClientCA:  encrypt(r.CalicoClientCA),
-		CalicoClientKey: encrypt(r.CalicoClientKey),
-		CalicoClientCrt: encrypt(r.CalicoClientCrt),
-		EtcdServerCA:    encrypt(r.EtcdServerCA),
-		EtcdServerKey:   encrypt(r.EtcdServerKey),
-		EtcdServerCrt:   encrypt(r.EtcdServerCrt),
+		APIServerCA:       encrypt(r.APIServerCA),
+		APIServerKey:      encrypt(r.APIServerKey),
+		APIServerCrt:      encrypt(r.APIServerCrt),
+		WorkerCA:          encrypt(r.WorkerCA),
+		WorkerCrt:         encrypt(r.WorkerCrt),
+		WorkerKey:         encrypt(r.WorkerKey),
+		ServiceAccountCA:  encrypt(r.ServiceAccountCA),
+		ServiceAccountCrt: encrypt(r.ServiceAccountCrt),
+		ServiceAccountKey: encrypt(r.ServiceAccountKey),
+		CalicoClientCA:    encrypt(r.CalicoClientCA),
+		CalicoClientKey:   encrypt(r.CalicoClientKey),
+		CalicoClientCrt:   encrypt(r.CalicoClientCrt),
+		EtcdServerCA:      encrypt(r.EtcdServerCA),
+		EtcdServerKey:     encrypt(r.EtcdServerKey),
+		EtcdServerCrt:     encrypt(r.EtcdServerCrt),
 	}
 	if err != nil {
 		return nil, microerror.MaskAny(err)
@@ -108,18 +117,21 @@ func (r *encryptedTLSAssets) compact() (*cloudconfig.CompactTLSAssets, error) {
 	}
 
 	compactAssets := cloudconfig.CompactTLSAssets{
-		APIServerCA:     compact(r.APIServerCA),
-		APIServerKey:    compact(r.APIServerKey),
-		APIServerCrt:    compact(r.APIServerCrt),
-		WorkerCA:        compact(r.WorkerCA),
-		WorkerKey:       compact(r.WorkerKey),
-		WorkerCrt:       compact(r.WorkerCrt),
-		CalicoClientCA:  compact(r.CalicoClientCA),
-		CalicoClientKey: compact(r.CalicoClientKey),
-		CalicoClientCrt: compact(r.CalicoClientCrt),
-		EtcdServerCA:    compact(r.EtcdServerCA),
-		EtcdServerKey:   compact(r.EtcdServerKey),
-		EtcdServerCrt:   compact(r.EtcdServerCrt),
+		APIServerCA:       compact(r.APIServerCA),
+		APIServerKey:      compact(r.APIServerKey),
+		APIServerCrt:      compact(r.APIServerCrt),
+		WorkerCA:          compact(r.WorkerCA),
+		WorkerKey:         compact(r.WorkerKey),
+		WorkerCrt:         compact(r.WorkerCrt),
+		ServiceAccountCA:  compact(r.ServiceAccountCA),
+		ServiceAccountKey: compact(r.ServiceAccountKey),
+		ServiceAccountCrt: compact(r.ServiceAccountCrt),
+		CalicoClientCA:    compact(r.CalicoClientCA),
+		CalicoClientKey:   compact(r.CalicoClientKey),
+		CalicoClientCrt:   compact(r.CalicoClientCrt),
+		EtcdServerCA:      compact(r.EtcdServerCA),
+		EtcdServerKey:     compact(r.EtcdServerKey),
+		EtcdServerCrt:     compact(r.EtcdServerCrt),
 	}
 
 	if err != nil {
