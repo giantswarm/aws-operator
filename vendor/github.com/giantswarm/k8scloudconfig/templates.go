926b9bce5827e97e57c40e7cbaa0c9935d244082
rossf7 <ross@giantswarm.io>
2017-05-04 15:25:00 +0200

    vendor: updated certificatetpr and k8scloudconfig

--- vendor/github.com/giantswarm/k8scloudconfig/templates.go
+++ vendor/github.com/giantswarm/k8scloudconfig/templates.go
@@ -429,6 +429,18 @@ write_files:
   encoding: gzip+base64
   content: {{.TLSAssets.APIServerKey}}
 
+- path: /etc/kubernetes/ssl/service-account-crt.pem.enc
+  encoding: gzip+base64
+  content: {{.TLSAssets.ServiceAccountCrt}}
+
+- path: /etc/kubernetes/ssl/service-account-ca.pem.enc
+  encoding: gzip+base64
+  content: {{.TLSAssets.ServiceAccountCA}}
+
+- path: /etc/kubernetes/ssl/service-account-key.pem.enc
+  encoding: gzip+base64
+  content: {{.TLSAssets.ServiceAccountKey}}
+
 - path: /etc/kubernetes/ssl/calico/client-crt.pem.enc
   encoding: gzip+base64
   content: {{.TLSAssets.CalicoClientCrt}}
@@ -725,7 +737,7 @@ coreos:
       --tls-cert-file=/etc/kubernetes/ssl/apiserver-crt.pem \
       --tls-private-key-file=/etc/kubernetes/ssl/apiserver-key.pem \
       --client-ca-file=/etc/kubernetes/ssl/apiserver-ca.pem \
-      --service-account-key-file=/etc/kubernetes/secrets/token_sign_key.pem
+      --service-account-key-file=/etc/kubernetes/ssl/service-account-key.pem
       ExecStop=-/usr/bin/docker stop -t 10 $NAME
       ExecStopPost=-/usr/bin/docker rm -f $NAME
   - name: k8s-api-server-restart.service
@@ -787,7 +799,7 @@ coreos:
       --v=2 \
       --kubeconfig=/etc/kubernetes/config/controller-manager-kubeconfig.yml \
       --root-ca-file=/etc/kubernetes/ssl/apiserver-ca.pem \
-      --service-account-private-key-file=/etc/kubernetes/secrets/token_sign_key.pem
+      --service-account-key-file=/etc/kubernetes/ssl/service-account-key.pem
       ExecStop=-/usr/bin/docker stop -t 10 $NAME
       ExecStopPost=-/usr/bin/docker rm -f $NAME
   - name: k8s-controller-manager-restart.service
