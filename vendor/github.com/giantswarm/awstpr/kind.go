6dc34e17830062fd2c328a2d0e8060beff77502e
rossf7 <ross@giantswarm.io>
2017-05-05 18:45:24 +0200

    vendor: updated k8scloudconfig

--- vendor/github.com/giantswarm/awstpr/kind.go
+++ vendor/github.com/giantswarm/awstpr/kind.go
@@ -3,4 +3,7 @@ package awstpr
 const (
 	// Kind is the TPR kind for awstpr resources.
 	Kind = "Aws"
+
+	// PluralKind is the plural TPR kind for use in resource URLs.
+	PluralKind = "awses"
 )
--- vendor/github.com/giantswarm/awstpr/version.go
+++ vendor/github.com/giantswarm/awstpr/version.go
@@ -2,5 +2,5 @@ package awstpr
 
 const (
 	// VersionV1 is the Kubernetes API version for v1 cluster resources.
-	VersionV1 = "aws.cluster.giantswarm.io/v1"
+	VersionV1 = "cluster.giantswarm.io/v1"
 )
