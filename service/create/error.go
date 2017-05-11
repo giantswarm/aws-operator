5aad1e59a6ff11d6c8d5df5d796d10d247fcde60
Tim Schindler <xh3b4sd@gmail.com>
2017-05-11 11:36:33 +0200

    aligned service errors with other microservices

--- service/create/error.go
+++ service/create/error.go
@@ -4,22 +4,23 @@ import (
 	"github.com/juju/errgo"
 )
 
-var (
-	invalidConfigError           = errgo.New("invalid config")
-	secretsRetrievalFailedError  = errgo.New("could not retrieve secrets")
-	missingCloudConfigKeyError   = errgo.New("missing required key in the cloudconfig")
-	malformedCloudConfigKeyError = errgo.New("malformed key in the cloudconfig")
-)
+var invalidConfigError = errgo.New("invalid config")
 
 // IsInvalidConfig asserts invalidConfigError.
 func IsInvalidConfig(err error) bool {
 	return errgo.Cause(err) == invalidConfigError
 }
 
-func IsMissingCloudConfigKey(err error) bool {
-	return errgo.Cause(err) == missingCloudConfigKeyError
-}
+var malformedCloudConfigKeyError = errgo.New("malformed key in the cloudconfig")
 
+// IsMalformedCloudConfigKey asserts malformedCloudConfigKeyError.
 func IsMalformedCloudConfigKey(err error) bool {
 	return errgo.Cause(err) == malformedCloudConfigKeyError
+}
+
+var missingCloudConfigKeyError = errgo.New("missing cloud config key")
+
+// IsMissingCloudConfigKey asserts missingCloudConfigKeyError.
+func IsMissingCloudConfigKey(err error) bool {
+	return errgo.Cause(err) == missingCloudConfigKeyError
 }
