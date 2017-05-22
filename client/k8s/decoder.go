acc3e1835963640daf57316033f46ad56b4ab8cd
Lorenzo Manacorda <lorenzo@kinvolk.io>
2017-05-22 15:55:26 +0200

    add debugging

--- client/k8s/decoder.go
+++ client/k8s/decoder.go
@@ -4,6 +4,7 @@ import (
 	"encoding/json"
 
 	"github.com/giantswarm/awstpr"
+	microerror "github.com/giantswarm/microkit/error"
 	"k8s.io/client-go/pkg/runtime"
 	"k8s.io/client-go/pkg/watch"
 )
@@ -26,7 +27,7 @@ func (cd *ClusterDecoder) Decode() (action watch.EventType, object runtime.Objec
 		Object awstpr.CustomObject
 	}
 	if err := cd.decoder.Decode(&e); err != nil {
-		return watch.Error, nil, err
+		return watch.Error, nil, microerror.MaskAnyf(err, "the message was %v", e)
 	}
 
 	return e.Type, &e.Object, nil
