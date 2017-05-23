ab36d5dedcf30d58656ca17af91a91fc7928648a
Lorenzo Manacorda <lorenzo@kinvolk.io>
2017-05-23 16:53:56 +0200

    try using api pkg instead of v1
    following example in https://github.com/kubernetes/client-go/blob/release-2.0/examples/third-party-resources/types.go

--- vendor/github.com/giantswarm/awstpr/custom_object.go
+++ vendor/github.com/giantswarm/awstpr/custom_object.go
@@ -4,17 +4,17 @@ import (
 	"encoding/json"
 
 	microerror "github.com/giantswarm/microkit/error"
+	"k8s.io/client-go/pkg/api"
 	"k8s.io/client-go/pkg/api/meta"
 	"k8s.io/client-go/pkg/api/unversioned"
-	"k8s.io/client-go/pkg/api/v1"
 )
 
 // CustomObject represents the AWS TPR's custom object. It holds the
 // specifications of the resource the AWS operator is interested in.
 type CustomObject struct {
 	unversioned.TypeMeta `json:",inline"`
-	Metadata             v1.ObjectMeta `json:"metadata,omitempty"`
-	Spec                 Spec          `json:"spec" yaml:"spec"`
+	Metadata             api.ObjectMeta `json:"metadata,omitempty"`
+	Spec                 Spec           `json:"spec" yaml:"spec"`
 }
 
 func (co *CustomObject) GetObjectMeta() meta.Object {
