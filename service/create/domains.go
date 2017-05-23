dcb6240692537c474ebfbe1bbc089def4f5450a2
Michal Rostecki <michal@kinvolk.io>
2017-05-23 17:05:06 +0200

    service: Create wildcard record
    Introduce a wildcard domain record for *.[cluster-id]. that is a CNAME to
    ingress.[cluster-id]. It will make the ingress easier to use without
    creating DNS records.
    
    Fixes #253

--- service/create/domains.go
+++ service/create/domains.go
@@ -14,16 +14,20 @@ type recordSetInput struct {
 	Cluster      awstpr.CustomObject
 	Client       *route53.Route53
 	Resource     resources.DNSNamedResource
+	Value        string
 	Domain       string
 	HostedZoneID string
+	Type         string
 }
 
 func (s *Service) deleteRecordSet(input recordSetInput) error {
 	rs := &awsresources.RecordSet{
 		Client:       input.Client,
 		Resource:     input.Resource,
+		Value:        input.Value,
 		Domain:       input.Domain,
 		HostedZoneID: input.HostedZoneID,
+		Type:         input.Type,
 	}
 
 	if err := rs.Delete(); err != nil {
@@ -38,8 +42,10 @@ func (s *Service) createRecordSet(input recordSetInput) error {
 	apiRecordSet := &awsresources.RecordSet{
 		Client:       input.Client,
 		Resource:     input.Resource,
+		Value:        input.Value,
 		Domain:       input.Domain,
 		HostedZoneID: input.HostedZoneID,
+		Type:         input.Type,
 	}
 
 	if err := apiRecordSet.CreateOrFail(); err != nil {
