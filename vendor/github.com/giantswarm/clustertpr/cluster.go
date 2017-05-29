62c8bae49f1c2b8c8f3fb7576111ed03c2505fd8
Ross Fairbanks <ross@giantswarm.io>
2017-05-29 11:54:43 +0200

    vendor: updated deps

--- vendor/github.com/giantswarm/clustertpr/cluster.go
+++ vendor/github.com/giantswarm/clustertpr/cluster.go
@@ -6,7 +6,6 @@ import (
 	"github.com/giantswarm/clustertpr/customer"
 	"github.com/giantswarm/clustertpr/docker"
 	"github.com/giantswarm/clustertpr/etcd"
-	"github.com/giantswarm/clustertpr/flannel"
 	"github.com/giantswarm/clustertpr/kubernetes"
 	"github.com/giantswarm/clustertpr/node"
 	"github.com/giantswarm/clustertpr/vault"
@@ -18,7 +17,6 @@ type Cluster struct {
 	Customer   customer.Customer     `json:"customer" yaml:"customer"`
 	Docker     docker.Docker         `json:"docker" yaml:"docker"`
 	Etcd       etcd.Etcd             `json:"etcd" yaml:"etcd"`
-	Flannel    flannel.Flannel       `json:"flannel" yaml:"flannel"`
 	Kubernetes kubernetes.Kubernetes `json:"kubernetes" yaml:"kubernetes"`
 	Masters    []node.Node           `json:"masters" yaml:"masters"`
 	Vault      vault.Vault           `json:"vault" yaml:"vault"`
