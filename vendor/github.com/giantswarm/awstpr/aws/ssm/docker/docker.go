bee51e4b4166be900e39548ee15a7c314bd78fe1
Lorenzo Manacorda <lorenzo@kinvolk.io>
2017-06-02 12:51:18 +0200

    update vendor

--- vendor/github.com/giantswarm/awstpr/aws/ssm/docker/docker.go
+++
@@ -1,7 +0,0 @@
-package docker
-
-type Docker struct {
-	// Image is the full qualified docker image.
-	// e.g. rlister/amazon-ssm-agent
-	Image string `json:"image" yaml:"image"`
-}
--- vendor/github.com/giantswarm/awstpr/aws/ssm/ssm.go
+++
@@ -1,9 +0,0 @@
-package ssm
-
-import (
-	"github.com/giantswarm/awstpr/aws/ssm/docker"
-)
-
-type SSMAgent struct {
-	Docker docker.Docker `json:"docker" yaml:"docker"`
-}
