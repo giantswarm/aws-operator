dcceadfd8a3a2fcbdcb6da3e51aec2efdfdc6f23
Ross Fairbanks <ross@giantswarm.io>
2017-06-22 18:51:04 +0200

    Replace tabs with spaces

--- service/create/templates.go
+++ service/create/templates.go
@@ -4,28 +4,28 @@ const (
 	decryptTLSAssetsScriptTemplate = `#!/bin/bash -e
 
 rkt run \
-	--volume=ssl,kind=host,source=/etc/kubernetes/ssl,readOnly=false \
-	--mount=volume=ssl,target=/etc/kubernetes/ssl \
-	--uuid-file-save=/var/run/coreos/decrypt-tls-assets.uuid \
-	--volume=dns,kind=host,source=/etc/resolv.conf,readOnly=true --mount volume=dns,target=/etc/resolv.conf \
-	--net=host \
-	--trust-keys-from-https \
-	quay.io/coreos/awscli:025a357f05242fdad6a81e8a6b520098aa65a600 --exec=/bin/bash -- \
-		-ec \
-		'echo decrypting tls assets
-		shopt -s nullglob
-		for encKey in $(find /etc/kubernetes/ssl -name "*.pem.enc"); do
-			echo decrypting $encKey
-			f=$(mktemp $encKey.XXXXXXXX)
-			/usr/bin/aws \
-				--region {{.AWS.Region}} kms decrypt \
-				--ciphertext-blob fileb://$encKey \
-				--output text \
-				--query Plaintext \
-			| base64 -d > $f
-			mv -f $f ${encKey%.enc}
-		done;
-		echo done.'
+  --volume=ssl,kind=host,source=/etc/kubernetes/ssl,readOnly=false \
+  --mount=volume=ssl,target=/etc/kubernetes/ssl \
+  --uuid-file-save=/var/run/coreos/decrypt-tls-assets.uuid \
+  --volume=dns,kind=host,source=/etc/resolv.conf,readOnly=true --mount volume=dns,target=/etc/resolv.conf \
+  --net=host \
+  --trust-keys-from-https \
+  quay.io/coreos/awscli:025a357f05242fdad6a81e8a6b520098aa65a600 --exec=/bin/bash -- \
+    -ec \
+    'echo decrypting tls assets
+    shopt -s nullglob
+    for encKey in $(find /etc/kubernetes/ssl -name "*.pem.enc"); do
+      echo decrypting $encKey
+      f=$(mktemp $encKey.XXXXXXXX)
+      /usr/bin/aws \
+        --region {{.AWS.Region}} kms decrypt \
+        --ciphertext-blob fileb://$encKey \
+        --output text \
+        --query Plaintext \
+      | base64 -d > $f
+      mv -f $f ${encKey%.enc}
+    done;
+    echo done.'
 
 rkt rm --uuid-file=/var/run/coreos/decrypt-tls-assets.uuid || :
 
@@ -120,17 +120,17 @@ write_files:
   permissions: 644
   content: |
     apiVersion: storage.k8s.io/v1beta1
-	kind: StorageClass
-	metadata:
-	name: gp2
-	annotations:
-		storageclass.beta.kubernetes.io/is-default-class: "true"
-	labels:
-		kubernetes.io/cluster-service: "true"
-		addonmanager.kubernetes.io/mode: EnsureExists
-	provisioner: kubernetes.io/aws-ebs
-	parameters:
-	type: gp2
+  kind: StorageClass
+  metadata:
+  name: gp2
+  annotations:
+    storageclass.beta.kubernetes.io/is-default-class: "true"
+  labels:
+    kubernetes.io/cluster-service: "true"
+    addonmanager.kubernetes.io/mode: EnsureExists
+  provisioner: kubernetes.io/aws-ebs
+  parameters:
+  type: gp2
 `
 
 	userDataScriptTemplate = `#!/bin/bash
