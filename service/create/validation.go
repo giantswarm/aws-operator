d2a14e43d5e97d989c6a5fb68bf2ef1650f85527
Ross Fairbanks <ross@giantswarm.io>
2017-08-11 17:15:37 +0200

    Extend validation checks for masters and workers

--- service/create/validation.go
+++ service/create/validation.go
@@ -3,17 +3,31 @@ package create
 import (
 	"github.com/giantswarm/awstpr"
 	"github.com/giantswarm/awstpr/spec/aws"
+	"github.com/giantswarm/clustertpr/spec"
 	"github.com/giantswarm/microerror"
 )
 
-func validateWorkers(workers []aws.Node) error {
-	if len(workers) < 1 {
+func validateMasters(awsMasters []aws.Node, masters []spec.Node) error {
+	// Currently only a single master is expected.
+	if len(awsMasters) != 1 || len(masters) != 1 {
+		return microerror.Mask(invalidMasterNodeCountError)
+	}
+
+	return nil
+}
+
+func validateWorkers(awsWorkers []aws.Node, workers []spec.Node) error {
+	if len(awsWorkers) < 1 || len(workers) < 1 {
 		return microerror.Mask(workersListEmptyError)
 	}
 
-	firstImageID := workers[0].ImageID
-	firstInstanceType := workers[0].InstanceType
-	for _, worker := range workers {
+	if len(awsWorkers) != len(workers) {
+		return microerror.Mask(invalidWorkerNodeCountError)
+	}
+
+	firstImageID := awsWorkers[0].ImageID
+	firstInstanceType := awsWorkers[0].InstanceType
+	for _, worker := range awsWorkers {
 		if worker.ImageID != firstImageID {
 			return microerror.Mask(differentImageIDsError)
 		}
@@ -26,7 +40,11 @@ func validateWorkers(workers []aws.Node) error {
 }
 
 func validateCluster(cluster awstpr.CustomObject) error {
-	if err := validateWorkers(cluster.Spec.AWS.Workers); err != nil {
+	if err := validateMasters(cluster.Spec.AWS.Masters, cluster.Spec.Cluster.Masters); err != nil {
+		return microerror.Mask(err)
+	}
+
+	if err := validateWorkers(cluster.Spec.AWS.Workers, cluster.Spec.Cluster.Workers); err != nil {
 		return microerror.Mask(err)
 	}
 
--- service/create/validation_test.go
+++ service/create/validation_test.go
@@ -4,6 +4,7 @@ import (
 	"testing"
 
 	"github.com/giantswarm/awstpr/spec/aws"
+	"github.com/giantswarm/clustertpr/spec"
 	"github.com/giantswarm/microerror"
 	"github.com/stretchr/testify/assert"
 )
@@ -11,12 +12,13 @@ import (
 func TestValidateWorkers(t *testing.T) {
 	tests := []struct {
 		name          string
-		workers       []aws.Node
+		awsWorkers    []aws.Node
+		workers       []spec.Node
 		expectedError error
 	}{
 		{
 			name: "Valid workers - image IDs and instance types are the same",
-			workers: []aws.Node{
+			awsWorkers: []aws.Node{
 				{
 					ImageID:      "example-image-id",
 					InstanceType: "example-instance-type",
@@ -26,16 +28,35 @@ func TestValidateWorkers(t *testing.T) {
 					InstanceType: "example-instance-type",
 				},
 			},
+			workers: []spec.Node{
+				{
+					ID: "worker-1",
+				},
+				{
+					ID: "worker-2",
+				},
+			},
 			expectedError: nil,
 		},
 		{
 			name:          "Invalid workers - list is empty",
-			workers:       []aws.Node{},
+			awsWorkers:    []aws.Node{},
+			workers:       []spec.Node{},
+			expectedError: workersListEmptyError,
+		},
+		{
+			name:       "Invalid workers - aws workers list is empty",
+			awsWorkers: []aws.Node{},
+			workers: []spec.Node{
+				{
+					ID: "worker-1",
+				},
+			},
 			expectedError: workersListEmptyError,
 		},
 		{
 			name: "Invalid workers - image IDs are different",
-			workers: []aws.Node{
+			awsWorkers: []aws.Node{
 				{
 					ImageID:      "example-image-id",
 					InstanceType: "example-instance-type",
@@ -45,11 +66,19 @@ func TestValidateWorkers(t *testing.T) {
 					InstanceType: "example-instance-type",
 				},
 			},
+			workers: []spec.Node{
+				{
+					ID: "worker-1",
+				},
+				{
+					ID: "worker-2",
+				},
+			},
 			expectedError: differentImageIDsError,
 		},
 		{
 			name: "Invalid workers - instance types are different",
-			workers: []aws.Node{
+			awsWorkers: []aws.Node{
 				{
 					ImageID:      "example-image-id",
 					InstanceType: "example-instance-type",
@@ -59,12 +88,39 @@ func TestValidateWorkers(t *testing.T) {
 					InstanceType: "another-instance-type",
 				},
 			},
+			workers: []spec.Node{
+				{
+					ID: "worker-1",
+				},
+				{
+					ID: "worker-2",
+				},
+			},
 			expectedError: differentInstanceTypesError,
+		},
+		{
+			name: "Invalid workers - node counts are different",
+			awsWorkers: []aws.Node{
+				{
+					ImageID:      "example-image-id",
+					InstanceType: "example-instance-type",
+				},
+				{
+					ImageID:      "example-image-id",
+					InstanceType: "another-instance-type",
+				},
+			},
+			workers: []spec.Node{
+				{
+					ID: "worker-1",
+				},
+			},
+			expectedError: invalidWorkerNodeCountError,
 		},
 	}
 
 	for _, tc := range tests {
-		err := validateWorkers(tc.workers)
+		err := validateWorkers(tc.awsWorkers, tc.workers)
 		assert.Equal(t, tc.expectedError, microerror.Cause(err), tc.name)
 	}
 }
