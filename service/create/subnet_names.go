12aa6d0230ebdf55553e9579b7fdd539a608968f
Bricktop <marcel.mueller1@rwth-aachen.de>
2017-09-12 12:15:37 +0200

    fix confusing naming of subnetName (#404)

--- service/create/subnet_names.go
+++ service/create/subnet_names.go
@@ -6,6 +6,6 @@ import (
 	"github.com/giantswarm/awstpr"
 )
 
-func subnetName(cluster awstpr.CustomObject, prefix string) string {
-	return fmt.Sprintf("%s-%s", cluster.Name, prefix)
+func subnetName(cluster awstpr.CustomObject, suffix string) string {
+	return fmt.Sprintf("%s-%s", cluster.Name, suffix)
 }
