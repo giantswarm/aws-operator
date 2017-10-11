26ca665576cb306dde41761ed6506d7189ea3d04
Julien Garcia Gonzalez <julien@giantswarm.io>
2017-10-11 10:26:30 +0200

    debug randomkey tpr
    Signed-off-by: Julien Garcia Gonzalez <julien@giantswarm.io>

--- vendor/github.com/giantswarm/randomkeytpr/service.go
+++ vendor/github.com/giantswarm/randomkeytpr/service.go
@@ -85,6 +85,8 @@ func (s *Service) SearchKeys(clusterID string) (map[Key][]byte, error) {
 // SearchKeysForKeytype watches for keys secrets of a single cluster keytype and
 // returns it as assets bundle.
 func (s *Service) SearchKeysForKeytype(clusterID, keyType string) (map[Key][]byte, error) {
+	s.logger.Log("debug", fmt.Sprintf("searching secret: %s=%s, %s=%s", KeyLabel, keyType, ClusterIDLabel, clusterID))
+
 	// TODO we should also do a list. In case the secrets have already been
 	// created we might miss them with only watching.
 	watcher, err := s.k8sClient.Core().Secrets(api.NamespaceDefault).Watch(apismetav1.ListOptions{
