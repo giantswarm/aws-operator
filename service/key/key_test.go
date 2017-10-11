c0724e5e1668a40e387f0996ec76a7c75aceb39c
Ross Fairbanks <ross@giantswarm.io>
2017-10-11 21:11:01 +0200

    Key helper for cluster version

--- service/key/key_test.go
+++ service/key/key_test.go
@@ -73,6 +73,24 @@ func Test_ClusterID(t *testing.T) {
 	}
 }
 
+func Test_ClusterVersion(t *testing.T) {
+	expectedVersion := "v_0_1_0"
+
+	cluster := clustertpr.Spec{
+		Version: expectedVersion,
+	}
+
+	customObject := awstpr.CustomObject{
+		Spec: awstpr.Spec{
+			Cluster: cluster,
+		},
+	}
+
+	if ClusterVersion(customObject) != expectedVersion {
+		t.Fatalf("Expected cluster version %s but was %s", expectedVersion, ClusterVersion(customObject))
+	}
+}
+
 func Test_MasterImageID(t *testing.T) {
 	tests := []struct {
 		customObject    awstpr.CustomObject
