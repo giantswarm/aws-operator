497eb862346e63c21a6fc6ab9f29b4f44b3618ad
Julien Garcia Gonzalez <julien@giantswarm.io>
2017-10-13 10:24:10 +0200

    remove debug log
    Signed-off-by: Julien Garcia Gonzalez <julien@giantswarm.io>

--- service/create/service.go
+++ service/create/service.go
@@ -566,10 +566,7 @@ func (s *Service) processCluster(cluster awstpr.CustomObject) error {
 	if err != nil {
 		return microerror.Maskf(executionFailedError, fmt.Sprintf("could not get keys from secrets: '%#v'", err))
 	}
-	for k, v := range keys {
-		s.logger.Log("debug", fmt.Sprintf("on receive k:%v -- v:%v", k, string(v)))
 
-	}
 	// Create KMS key.
 	kmsKey := &awsresources.KMSKey{
 		Name:      key.ClusterID(cluster),
