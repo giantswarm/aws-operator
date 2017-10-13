e9450a25ac1b23d9fe636b9c015009ddee978735
Julien Garcia Gonzalez <julien@giantswarm.io>
2017-10-13 17:25:01 +0200

    remove keys from tls script
    Signed-off-by: Julien Garcia Gonzalez <julien@giantswarm.io>

--- service/cloudconfig/templates.go
+++ service/cloudconfig/templates.go
@@ -5,9 +5,7 @@ const (
 
 rkt run \
   --volume=ssl,kind=host,source=/etc/kubernetes/ssl,readOnly=false \
-  --volume=keys,kind=host,source=/etc/kubernetes/encryption,readOnly=false \
   --mount=volume=ssl,target=/etc/kubernetes/ssl \
-  --mount=volume=keys,target=/etc/kubernetes/encryption \
   --uuid-file-save=/var/run/coreos/decrypt-tls-assets.uuid \
   --volume=dns,kind=host,source=/etc/resolv.conf,readOnly=true --mount volume=dns,target=/etc/resolv.conf \
   --net=host \
@@ -26,22 +24,7 @@ rkt run \
         --query Plaintext \
       | base64 -d > $f
       mv -f $f ${encKey%.enc}
-    done;
-    
-    echo decrypting keys assets
-    shopt -s nullglob
-    for encKey in $(find /etc/kubernetes/encryption -name "*.enc"); do
-      echo decrypting $encKey
-      f=$(mktemp $encKey.XXXXXXXX)
-      /usr/bin/aws \
-        --region {{.AWS.Region}} kms decrypt \
-        --ciphertext-blob fileb://$encKey \
-        --output text \
-        --query Plaintext \
-      | base64 -d > $f
-      mv -f $f ${encKey%.enc}
-    done;
-    echo done.'
+    done;'
 
 rkt rm --uuid-file=/var/run/coreos/decrypt-tls-assets.uuid || :
 
