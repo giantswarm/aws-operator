05da302c50792eeb66b0f9be87d273337934a4d1
Vaclav Rozsypalek <vaclav@giantswarm.io>
2017-11-03 15:39:16 +0100

    vendor-k8scc-changes-0311

--- vendor/github.com/giantswarm/versionbundle/collector.go
+++ vendor/github.com/giantswarm/versionbundle/collector.go
@@ -1,16 +1,20 @@
 package versionbundle
 
 import (
+	"context"
+	"encoding/json"
 	"net/url"
+	"sync"
 
 	"github.com/giantswarm/microerror"
 	"github.com/go-resty/resty"
+	"golang.org/x/sync/errgroup"
 )
 
 type CollectorConfig struct {
 	RestClient *resty.Client
 
-	Endpoints []url.URL
+	Endpoints []*url.URL
 }
 
 func DefaultCollectorConfig() CollectorConfig {
@@ -25,8 +29,9 @@ type Collector struct {
 	restClient *resty.Client
 
 	bundles []Bundle
+	mutex   sync.Mutex
 
-	endpoints []url.URL
+	endpoints []*url.URL
 }
 
 func NewCollector(config CollectorConfig) (*Collector, error) {
@@ -42,6 +47,7 @@ func NewCollector(config CollectorConfig) (*Collector, error) {
 		restClient: config.RestClient,
 
 		bundles: nil,
+		mutex:   sync.Mutex{},
 
 		endpoints: config.Endpoints,
 	}
@@ -50,10 +56,64 @@ func NewCollector(config CollectorConfig) (*Collector, error) {
 }
 
 func (c *Collector) Bundles() []Bundle {
-	return c.bundles
+	c.mutex.Lock()
+	defer c.mutex.Unlock()
+
+	return CopyBundles(c.bundles)
 }
 
-// TODO
-func (c *Collector) Collect() error {
+type CollectorEndpointResponse struct {
+	VersionBundles []Bundle `json:"version_bundles"`
+}
+
+func (c *Collector) Collect(ctx context.Context) error {
+	var responses [][]byte
+	{
+		var g errgroup.Group
+		responses = make([][]byte, len(c.endpoints))
+
+		for i, e := range c.endpoints {
+			i, e := i, e
+
+			g.Go(func() error {
+				res, err := c.restClient.NewRequest().Get(e.String())
+				if err != nil {
+					return microerror.Mask(err)
+				}
+
+				responses[i] = res.Body()
+
+				return nil
+			})
+
+			err := g.Wait()
+			if err != nil {
+				return microerror.Mask(err)
+			}
+		}
+	}
+
+	var bundles []Bundle
+	{
+
+		for _, b := range responses {
+			var er CollectorEndpointResponse
+			err := json.Unmarshal(b, &er)
+			if err != nil {
+				return microerror.Mask(err)
+			}
+
+			for _, bundle := range er.VersionBundles {
+				bundles = append(bundles, bundle)
+			}
+		}
+	}
+
+	{
+		c.mutex.Lock()
+		c.bundles = bundles
+		c.mutex.Unlock()
+	}
+
 	return nil
 }
