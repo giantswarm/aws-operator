1fce331081f6442f4b4f9faf9222c035077290cc
Federico Gimenez <federico@giantswarm.io>
2017-11-07 15:58:04 +0100

    add template body

--- service/resource/cloudformation/main_stack.go
+++ service/resource/cloudformation/main_stack.go
@@ -1,6 +1,9 @@
 package cloudformation
 
 import (
+	"bytes"
+	"text/template"
+
 	"github.com/aws/aws-sdk-go/aws"
 	awsCF "github.com/aws/aws-sdk-go/service/cloudformation"
 	"github.com/giantswarm/aws-operator/service/key"
@@ -11,8 +14,23 @@ func newMainStack(customObject awstpr.CustomObject) (*awsCF.CreateStackInput, er
 	stackName := key.MainStackName(customObject)
 
 	mainCF := &awsCF.CreateStackInput{
-		StackName: aws.String(stackName),
+		StackName:    aws.String(stackName),
+		TemplateBody: aws.String(MainTemplate),
 	}
 
 	return mainCF, nil
+}
+
+func getMainTemplateBody(customObject awstpr.CustomObject) (string, error) {
+	t, err := template.New("main").Parse(MainTemplate)
+	if err != nil {
+		return "", err
+	}
+
+	var tpl bytes.Buffer
+	if err := t.Execute(&tpl, customObject); err != nil {
+		return "", err
+	}
+
+	return tpl.String(), nil
 }
