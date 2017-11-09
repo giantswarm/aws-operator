97fbb4b72a71d361f8fa373e65a251c809d4a297
Federico Gimenez <fgimenez@users.noreply.github.com>
2017-11-09 13:30:13 +0100

    add current method to cloudformation resource (#494)
    * add current method to cloudformation resource
    
    * address review comments
    
    * address review comments
    
    * awsCF -> awscloudformation
    
    * address review comments

--- service/resource/cloudformation/current.go
+++ service/resource/cloudformation/current.go
@@ -1,7 +1,48 @@
 package cloudformation
 
-import "context"
+import (
+	"context"
+
+	"github.com/aws/aws-sdk-go/aws"
+	awscloudformation "github.com/aws/aws-sdk-go/service/cloudformation"
+	"github.com/giantswarm/aws-operator/service/key"
+	"github.com/giantswarm/microerror"
+)
 
 func (r *Resource) GetCurrentState(ctx context.Context, obj interface{}) (interface{}, error) {
-	return nil, nil
+	customObject, err := key.ToCustomObject(obj)
+	if err != nil {
+		return StackState{}, microerror.Mask(err)
+	}
+
+	r.logger.Log("cluster", key.ClusterID(customObject), "debug", "looking for AWS stack")
+
+	stackName := key.MainStackName(customObject)
+
+	describeInput := &awscloudformation.DescribeStacksInput{
+		StackName: aws.String(stackName),
+	}
+	describeOutput, err := r.awsClient.DescribeStacks(describeInput)
+
+	if IsStackNotFound(err) {
+
+		r.logger.Log("cluster", key.ClusterID(customObject), "debug", "did not find a stack in AWS API")
+		return StackState{}, nil
+
+	} else if err != nil {
+
+		return StackState{}, microerror.Mask(err)
+
+	}
+
+	if len(describeOutput.Stacks) > 1 {
+		return StackState{}, microerror.Mask(notFoundError)
+	}
+
+	outputStackState := StackState{
+		Name:    stackName,
+		Outputs: describeOutput.Stacks[0].Outputs,
+	}
+
+	return outputStackState, nil
 }
--- service/resource/cloudformation/spec.go
+++ service/resource/cloudformation/spec.go
@@ -1,7 +1,9 @@
 package cloudformation
 
+import awscloudformation "github.com/aws/aws-sdk-go/service/cloudformation"
+
 // StackState is the state representation on which the resource methods work
 type StackState struct {
-	Name         string
-	TemplateBody string
+	Name    string
+	Outputs []*awscloudformation.Output
 }
