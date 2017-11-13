c3d1391ad3634fce5cb0577ba0a898660d12191c
Federico Gimenez <fgimenez@users.noreply.github.com>
2017-11-13 14:37:31 +0100

    extend resource state (#502)
    * extend resource state
    
    * add awstpr restructure issue reference
    
    * address review comments

--- service/resource/cloudformation/main_stack.go
+++ service/resource/cloudformation/main_stack.go
@@ -2,19 +2,29 @@ package cloudformation
 
 import (
 	"bytes"
+	"strconv"
 	"text/template"
 
-	awscloudformation "github.com/aws/aws-sdk-go/service/cloudformation"
 	"github.com/giantswarm/aws-operator/service/key"
 	"github.com/giantswarm/awstpr"
 )
 
 func newMainStack(customObject awstpr.CustomObject) (StackState, error) {
 	stackName := key.MainStackName(customObject)
+	workers := len(customObject.Spec.AWS.Workers)
+	var imageID string
+	// FIXME: the imageID should nnot depend on the number of workers.
+	// issue: https://github.com/giantswarm/awstpr/issues/47
+	if workers > 0 {
+		imageID = customObject.Spec.AWS.Workers[0].ImageID
+	}
+	clusterVersion := key.ClusterVersion(customObject)
 
 	mainCF := StackState{
-		Name:    stackName,
-		Outputs: []*awscloudformation.Output{},
+		Name:           stackName,
+		Workers:        strconv.Itoa(workers),
+		ImageID:        imageID,
+		ClusterVersion: clusterVersion,
 	}
 
 	return mainCF, nil
--- service/resource/cloudformation/spec.go
+++ service/resource/cloudformation/spec.go
@@ -1,14 +1,18 @@
 package cloudformation
 
-import awscloudformation "github.com/aws/aws-sdk-go/service/cloudformation"
-
 const (
 	// defaultCreationTimeout is the timeout in minutes for the creation of the stack.
 	defaultCreationTimeout = 10
+
+	workersParameterKey        = "workers"
+	imageIDParameterKey        = "imageID"
+	clusterVersionParameterKey = "clusterVersion"
 )
 
 // StackState is the state representation on which the resource methods work.
 type StackState struct {
-	Name    string
-	Outputs []*awscloudformation.Output
+	Name           string
+	ImageID        string
+	Workers        string
+	ClusterVersion string
 }
