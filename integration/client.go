127543801bb7653d11148cbdfea36f613a9c096f
Federico Gimenez <federico@giantswarm.io>
2017-11-21 17:28:17 +0100

    literal kubeconfig

--- integration/client.go
+++ integration/client.go
@@ -6,12 +6,10 @@ import (
 	"github.com/giantswarm/microerror"
 	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/tools/clientcmd"
-
-	"github.com/giantswarm/e2e-harness/pkg/harness"
 )
 
 func getK8sClient() (kubernetes.Interface, error) {
-	config, err := clientcmd.BuildConfigFromFlags("", harness.DefaultKubeConfig)
+	config, err := clientcmd.BuildConfigFromFlags("", "/workdir/.shipyard/config")
 	if err != nil {
 		return nil, microerror.Mask(err)
 	}
