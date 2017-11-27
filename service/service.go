f8468067f16b0684bb0466bb30f895c1b8a013cb
Ross Fairbanks <ross@giantswarm.io>
2017-11-27 12:59:55 +0100

    Remove loggermeta context as self link is provided by operatorkit

--- service/service.go
+++ service/service.go
@@ -12,7 +12,6 @@ import (
 	"github.com/giantswarm/microendpoint/service/version"
 	"github.com/giantswarm/microerror"
 	"github.com/giantswarm/micrologger"
-	"github.com/giantswarm/micrologger/loggermeta"
 	"github.com/giantswarm/operatorkit/client/k8sclient"
 	"github.com/giantswarm/operatorkit/framework"
 	"github.com/giantswarm/operatorkit/framework/resource/metricsresource"
@@ -28,7 +27,6 @@ import (
 	"github.com/giantswarm/aws-operator/service/alerter"
 	"github.com/giantswarm/aws-operator/service/cloudconfig"
 	"github.com/giantswarm/aws-operator/service/healthz"
-	"github.com/giantswarm/aws-operator/service/key"
 	cloudformationresource "github.com/giantswarm/aws-operator/service/resource/cloudformation"
 	legacyresource "github.com/giantswarm/aws-operator/service/resource/legacy"
 )
@@ -254,17 +252,6 @@ func New(config Config) (*Service, error) {
 	}
 
 	initCtxFunc := func(ctx context.Context, obj interface{}) (context.Context, error) {
-		meta := loggermeta.New()
-
-		customObject, err := key.ToCustomObject(obj)
-		if err != nil {
-			return nil, microerror.Mask(err)
-		}
-
-		meta.KeyVals["cluster"] = key.ClusterID(customObject)
-
-		ctx = loggermeta.NewContext(ctx, meta)
-
 		return ctx, nil
 	}
 
