e101942167689ca27701b06f32bc169d98d97273
Federico Gimenez <federico@giantswarm.io>
2017-11-30 10:39:53 +0100

    address review comments

--- integration/error.go
+++ integration/error.go
@@ -1,11 +1,33 @@
+// +build k8srequired
+
 package integration
 
 import "github.com/giantswarm/microerror"
 
 var waitTimeoutError = microerror.New("waitTimeout")
 
+// IsWaitTimeout asserts invalidConfigError.
+func IsWaitTimeout(err error) bool {
+	return microerror.Cause(err) == waitTimeoutError
+}
+
 var tooManyResultsError = microerror.New("too many results")
+
+// IsTooManyResults asserts invalidConfigError.
+func IsTooManyResults(err error) bool {
+	return microerror.Cause(err) == tooManyResultsError
+}
 
 var unexpectedStatusPhaseError = microerror.New("unexpected status phase")
 
+// IsUnexpectedStatusPhase asserts notFoundError.
+func IsUnexpectedStatusPhase(err error) bool {
+	return microerror.Cause(err) == unexpectedStatusPhaseError
+}
+
 var notFoundError = microerror.New("not found")
+
+// IsNotFound asserts notFoundError.
+func IsNotFound(err error) bool {
+	return microerror.Cause(err) == notFoundError
+}
