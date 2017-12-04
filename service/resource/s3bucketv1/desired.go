08eba07167520659e351d91acf03f9c5de35b9ed
Ross Fairbanks <ross@giantswarm.io>
2017-12-04 09:13:02 +0100

    Get desired state returns the bucket name

--- service/resource/s3bucketv1/desired.go
+++ service/resource/s3bucketv1/desired.go
@@ -2,8 +2,20 @@ package s3bucketv1
 
 import (
 	"context"
+
+	"github.com/giantswarm/aws-operator/service/key"
+	"github.com/giantswarm/microerror"
 )
 
 func (r *Resource) GetDesiredState(ctx context.Context, obj interface{}) (interface{}, error) {
-	return nil, nil
+	customObject, err := key.ToCustomObject(obj)
+	if err != nil {
+		return nil, microerror.Mask(err)
+	}
+
+	bucketState := BucketState{
+		Name: key.BucketName(customObject, r.awsConfig.AccountID()),
+	}
+
+	return bucketState, nil
 }
