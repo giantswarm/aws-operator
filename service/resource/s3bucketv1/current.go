17bd861323b75ca62d46e65c9919578b1e0b0b81
Ross Fairbanks <ross@giantswarm.io>
2017-12-04 09:12:23 +0100

    Get current state checks if the bucket exists

--- service/resource/s3bucketv1/current.go
+++ service/resource/s3bucketv1/current.go
@@ -2,8 +2,40 @@ package s3bucketv1
 
 import (
 	"context"
+
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/service/s3"
+	"github.com/giantswarm/aws-operator/service/key"
+	"github.com/giantswarm/microerror"
 )
 
 func (r *Resource) GetCurrentState(ctx context.Context, obj interface{}) (interface{}, error) {
-	return nil, nil
+	customObject, err := key.ToCustomObject(obj)
+	if err != nil {
+		return nil, microerror.Mask(err)
+	}
+
+	r.logger.LogCtx(ctx, "debug", "looking for the S3 bucket")
+
+	bucketName := key.BucketName(customObject, r.awsConfig.AccountID())
+
+	headInput := &s3.HeadBucketInput{
+		Bucket: aws.String(bucketName),
+	}
+	_, err = r.clients.S3.HeadBucket(headInput)
+	if IsBucketNotFound(err) {
+		r.logger.LogCtx(ctx, "debug", "did not find the S3 bucket")
+		return BucketState{}, nil
+
+	} else if err != nil {
+		return BucketState{}, microerror.Mask(err)
+	}
+
+	bucketState := BucketState{
+		Name: bucketName,
+	}
+
+	r.logger.LogCtx(ctx, "debug", "found the S3 bucket")
+
+	return bucketState, nil
 }
--- service/resource/s3bucketv1/error.go
+++ service/resource/s3bucketv1/error.go
@@ -1,6 +1,8 @@
 package s3bucketv1
 
 import (
+	"github.com/aws/aws-sdk-go/aws/awserr"
+	"github.com/aws/aws-sdk-go/service/s3"
 	"github.com/giantswarm/microerror"
 )
 
@@ -9,4 +11,24 @@ var invalidConfigError = microerror.New("invalid config")
 // IsInvalidConfig asserts invalidConfigError.
 func IsInvalidConfig(err error) bool {
 	return microerror.Cause(err) == invalidConfigError
+}
+
+var notFoundError = microerror.New("not found")
+
+// IsNotFound asserts notFoundError.
+func IsNotFound(err error) bool {
+	return microerror.Cause(err) == notFoundError
+}
+
+// IsBucketNotFound asserts bucket not found error from upstream's API code.
+func IsBucketNotFound(err error) bool {
+	aerr, ok := err.(awserr.Error)
+	if !ok {
+		return false
+	}
+	if aerr.Code() == s3.ErrCodeNoSuchBucket {
+		return true
+	}
+
+	return false
 }
