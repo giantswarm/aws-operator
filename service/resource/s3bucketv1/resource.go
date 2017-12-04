b4522a2f88b7da9e270f2a195b13166c3b98173f
Ross Fairbanks <ross@giantswarm.io>
2017-12-04 09:13:58 +0100

    Add AWS config and clients to Resource

--- service/resource/s3bucketv1/resource.go
+++ service/resource/s3bucketv1/resource.go
@@ -1,10 +1,11 @@
 package s3bucketv1
 
 import (
-	"github.com/aws/aws-sdk-go/service/s3"
 	"github.com/giantswarm/microerror"
 	"github.com/giantswarm/micrologger"
 	"github.com/giantswarm/operatorkit/framework"
+
+	awsutil "github.com/giantswarm/aws-operator/client/aws"
 )
 
 const (
@@ -15,8 +16,11 @@ const (
 // Config represents the configuration used to create a new s3bucket resource.
 type Config struct {
 	// Dependencies.
-	Logger   micrologger.Logger
-	S3Client *s3.S3
+	Clients *awsutil.Clients
+	Logger  micrologger.Logger
+
+	// Settings.
+	AwsConfig awsutil.Config
 }
 
 // DefaultConfig provides a default configuration to create a new s3bucket
@@ -24,34 +28,54 @@ type Config struct {
 func DefaultConfig() Config {
 	return Config{
 		// Dependencies.
-		Logger:   nil,
-		S3Client: nil,
+		Clients: nil,
+		Logger:  nil,
+
+		// Settings.
+		AwsConfig: awsutil.Config{},
 	}
 }
 
 // Resource implements the s3bucket resource.
 type Resource struct {
 	// Dependencies.
-	s3Client *s3.S3
-	logger   micrologger.Logger
+	clients *awsutil.Clients
+	logger  micrologger.Logger
+
+	// Settings.
+	awsConfig awsutil.Config
 }
 
 // New creates a new configured s3bucket resource.
 func New(config Config) (*Resource, error) {
 	// Dependencies.
+	if config.Clients == nil {
+		return nil, microerror.Maskf(invalidConfigError, "config.Clients must not be empty")
+	}
 	if config.Logger == nil {
 		return nil, microerror.Maskf(invalidConfigError, "config.Logger must not be empty")
 	}
-	if config.S3Client == nil {
-		return nil, microerror.Maskf(invalidConfigError, "config.S3Client must not be empty")
+
+	// Settings.
+	var emptyAwsConfig awsutil.Config
+	if config.AwsConfig == emptyAwsConfig {
+		return nil, microerror.Maskf(invalidConfigError, "config.AwsConfig must not be empty")
+	}
+
+	err := config.AwsConfig.SetAccountID(config.Clients.IAM)
+	if err != nil {
+		return nil, microerror.Mask(err)
 	}
 
 	newResource := &Resource{
 		// Dependencies.
+		clients: config.Clients,
 		logger: config.Logger.With(
 			"resource", Name,
 		),
-		s3Client: config.S3Client,
+
+		// Settings.
+		awsConfig: config.AwsConfig,
 	}
 
 	return newResource, nil
