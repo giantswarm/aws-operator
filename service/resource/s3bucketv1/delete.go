e3e8484078caab5aacfc9c89351bdee8ab963110
Ross Fairbanks <rossf7@users.noreply.github.com>
2017-12-05 09:23:46 +0100

    Implement deleting the S3 bucket (#547)

--- service/resource/s3bucketv1/delete.go
+++ service/resource/s3bucketv1/delete.go
@@ -3,13 +3,66 @@ package s3bucketv1
 import (
 	"context"
 
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/service/s3"
+	"github.com/giantswarm/microerror"
 	"github.com/giantswarm/operatorkit/framework"
 )
 
 func (r *Resource) ApplyDeleteChange(ctx context.Context, obj, deleteChange interface{}) error {
+	bucketInput, err := toBucketState(deleteChange)
+	if err != nil {
+		return microerror.Mask(err)
+	}
+
+	if bucketInput.Name != "" {
+		r.logger.LogCtx(ctx, "debug", "deleting S3 bucket")
+
+		_, err := r.clients.S3.DeleteBucket(&s3.DeleteBucketInput{
+			Bucket: aws.String(bucketInput.Name),
+		})
+		if IsBucketNotFound(err) {
+			// Fall through.
+			return nil
+		}
+		if err != nil {
+			return microerror.Mask(err)
+		}
+
+		r.logger.LogCtx(ctx, "debug", "deleting S3 bucket: deleted")
+	} else {
+		r.logger.LogCtx(ctx, "debug", "deleting S3 bucket: already deleted")
+	}
+
 	return nil
 }
 
 func (r *Resource) NewDeletePatch(ctx context.Context, obj, currentState, desiredState interface{}) (*framework.Patch, error) {
-	return framework.NewPatch(), nil
+	delete, err := r.newDeleteChange(ctx, obj, currentState, desiredState)
+	if err != nil {
+		return nil, microerror.Mask(err)
+	}
+
+	patch := framework.NewPatch()
+	patch.SetDeleteChange(delete)
+
+	return patch, nil
+}
+
+func (r *Resource) newDeleteChange(ctx context.Context, obj, currentState, desiredState interface{}) (interface{}, error) {
+	currentBucket, err := toBucketState(currentState)
+	if err != nil {
+		return nil, microerror.Mask(err)
+	}
+	desiredBucket, err := toBucketState(desiredState)
+	if err != nil {
+		return nil, microerror.Mask(err)
+	}
+
+	var bucketToDelete BucketState
+	if currentBucket.Name != "" {
+		bucketToDelete = desiredBucket
+	}
+
+	return bucketToDelete, nil
 }
