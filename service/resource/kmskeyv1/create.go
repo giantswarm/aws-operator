0f0edc155931a22c3a1412b29fc480a1c1a2d4b3
Federico Gimenez <fgimenez@users.noreply.github.com>
2017-12-07 16:35:49 +0100

    add GetDesiredState and Create methods (#566)
    * add GetDesiredState and Create methods
    
    * address review comments

--- service/resource/kmskeyv1/create.go
+++ service/resource/kmskeyv1/create.go
@@ -1,11 +1,68 @@
 package kmskeyv1
 
-import "context"
+import (
+	"context"
+
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/service/kms"
+	"github.com/giantswarm/microerror"
+
+	"github.com/giantswarm/aws-operator/service/keyv1"
+)
 
 func (r *Resource) ApplyCreateChange(ctx context.Context, obj, createChange interface{}) error {
+	createKeyInput, err := toCreateKeyInput(createChange)
+	if err != nil {
+		return microerror.Mask(err)
+	}
+
+	if createKeyInput != nil {
+		customObject, err := keyv1.ToCustomObject(obj)
+		if err != nil {
+			return microerror.Mask(err)
+		}
+		key, err := r.awsClients.KMS.CreateKey(createKeyInput)
+		if err != nil {
+			return microerror.Mask(err)
+		}
+
+		clusterID := keyv1.ClusterID(customObject)
+		keyAlias := toAlias(clusterID)
+		if _, err := r.awsClients.KMS.CreateAlias(&kms.CreateAliasInput{
+			AliasName:   aws.String(keyAlias),
+			TargetKeyId: key.KeyMetadata.Arn,
+		}); err != nil {
+			return microerror.Mask(err)
+		}
+
+		if _, err := r.awsClients.KMS.EnableKeyRotation(&kms.EnableKeyRotationInput{
+			KeyId: key.KeyMetadata.KeyId,
+		}); err != nil {
+			return microerror.Mask(err)
+		}
+
+		r.logger.LogCtx(ctx, "debug", "creating KMS key: created")
+	} else {
+		r.logger.LogCtx(ctx, "debug", "creating KMS key: already created")
+	}
+
 	return nil
 }
 
 func (r *Resource) newCreateChange(ctx context.Context, obj, currentState, desiredState interface{}) (interface{}, error) {
+	currentKMSState, err := toKMSKeyState(currentState)
+	if err != nil {
+		return nil, microerror.Mask(err)
+	}
+
+	desiredKMSState, err := toKMSKeyState(desiredState)
+	if err != nil {
+		return nil, microerror.Mask(err)
+	}
+
+	if desiredKMSState.KeyID != currentKMSState.KeyID {
+		return &kms.CreateKeyInput{}, nil
+	}
+
 	return nil, nil
 }
--- service/resource/kmskeyv1/desired_test.go
+++ service/resource/kmskeyv1/desired_test.go
@@ -1,1 +1,61 @@
 package kmskeyv1
+
+import (
+	"context"
+	"testing"
+
+	"github.com/giantswarm/awstpr"
+	"github.com/giantswarm/clustertpr"
+	"github.com/giantswarm/clustertpr/spec"
+	"github.com/giantswarm/micrologger/microloggertest"
+)
+
+func Test_DesiredState(t *testing.T) {
+	clusterTpo := &awstpr.CustomObject{
+		Spec: awstpr.Spec{
+			Cluster: clustertpr.Spec{
+				Cluster: spec.Cluster{
+					ID: "test-cluster",
+				},
+			},
+		},
+	}
+
+	testCases := []struct {
+		obj           *awstpr.CustomObject
+		description   string
+		expectedKeyID string
+	}{
+		{
+			description:   "basic match",
+			obj:           clusterTpo,
+			expectedKeyID: "alias/test-cluster",
+		},
+	}
+	var err error
+	var newResource *Resource
+
+	resourceConfig := DefaultConfig()
+	resourceConfig.Logger = microloggertest.New()
+	for _, tc := range testCases {
+		t.Run(tc.description, func(t *testing.T) {
+			newResource, err = New(resourceConfig)
+			if err != nil {
+				t.Error("expected", nil, "got", err)
+			}
+
+			result, err := newResource.GetDesiredState(context.TODO(), tc.obj)
+			if err != nil {
+				t.Errorf("unexpected error %v", err)
+			}
+			currentState, ok := result.(KMSKeyState)
+			if !ok {
+				t.Errorf("expected '%T', got '%T'", currentState, result)
+			}
+
+			if currentState.KeyID != tc.expectedKeyID {
+				t.Errorf("expected keyID %q, got %q", tc.expectedKeyID, currentState.KeyID)
+			}
+		})
+	}
+}
