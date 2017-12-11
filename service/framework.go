fcbdeca824c80dd2fb83e03a7448b9e011a5527a
Tim Schindler <xh3b4sd@gmail.com>
2017-12-11 11:37:11 +0100

    enabled TPR migration code

--- service/framework.go
+++ service/framework.go
@@ -24,6 +24,7 @@ import (
 	"github.com/giantswarm/operatorkit/tpr"
 	"github.com/giantswarm/randomkeytpr"
 	apiextensionsclient "k8s.io/apiextensions-apiserver/pkg/client/clientset/clientset"
+	apierrors "k8s.io/apimachinery/pkg/api/errors"
 	apismetav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime"
 	"k8s.io/apimachinery/pkg/watch"
@@ -831,18 +832,17 @@ func migrateTPRsToCRDs(logger micrologger.Logger, clientSet *versioned.Clientset
 
 		// Create CRO in Kubernetes API.
 		{
-			// TODO enable.
-			// _, err := clientSet.ProviderV1alpha1().AWSConfigs(tpo.Namespace).Get(cro.Name, apismetav1.GetOptions{})
-			// if apierrors.IsNotFound(err) {
-			// 	_, err := clientSet.ProviderV1alpha1().AWSConfigs(tpo.Namespace).Create(cro)
-			// 	if err != nil {
-			// 		logger.Log("error", fmt.Sprintf("%#v", err))
-			// 		return
-			// 	}
-			// } else if err != nil {
-			// 	logger.Log("error", fmt.Sprintf("%#v", err))
-			// 	return
-			// }
+			_, err := clientSet.ProviderV1alpha1().AWSConfigs(tpo.Namespace).Get(cro.Name, apismetav1.GetOptions{})
+			if apierrors.IsNotFound(err) {
+				_, err := clientSet.ProviderV1alpha1().AWSConfigs(tpo.Namespace).Create(cro)
+				if err != nil {
+					logger.Log("error", fmt.Sprintf("%#v", err))
+					return
+				}
+			} else if err != nil {
+				logger.Log("error", fmt.Sprintf("%#v", err))
+				return
+			}
 		}
 	}
 
