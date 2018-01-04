c3288556982a89306e7b04ebf42dcab56da394c8
Ross Fairbanks <ross@giantswarm.io>
2018-01-04 16:24:05 +0100

    Reference route table in NAT gateway

--- service/resource/legacyv2/adapter/nat_gateway.go
+++ service/resource/legacyv2/adapter/nat_gateway.go
@@ -10,8 +10,7 @@ import (
 // template related to this adapter: service/templates/cloudformation/nat_gateway.yaml
 
 type natGatewayAdapter struct {
-	PublicSubnetID      string
-	PrivateRouteTableID string
+	PublicSubnetID string
 }
 
 func (n *natGatewayAdapter) getNatGateway(customObject v1alpha1.AWSConfig, clients Clients) error {
@@ -24,16 +23,6 @@ func (n *natGatewayAdapter) getNatGateway(customObject v1alpha1.AWSConfig, clien
 		return microerror.Mask(err)
 	}
 	n.PublicSubnetID = subnetID
-
-	// private route table ID
-	// TODO: remove this code once the subnet is created by cloudformation and add a
-	// reference in the template
-	routeTableName := keyv2.RouteTableName(customObject, suffixPrivate)
-	routeTableID, err := RouteTableID(clients, routeTableName)
-	if err != nil {
-		return microerror.Mask(err)
-	}
-	n.PrivateRouteTableID = routeTableID
 
 	return nil
 }
