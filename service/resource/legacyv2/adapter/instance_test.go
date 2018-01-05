d39df8e7c88fc8f9f5352689094d9fb8f8e6b0a9
Ross Fairbanks <ross@giantswarm.io>
2018-01-05 10:29:23 +0100

    Reference subnet in the master instance

--- service/resource/legacyv2/adapter/instance_test.go
+++ service/resource/legacyv2/adapter/instance_test.go
@@ -15,7 +15,6 @@ func TestAdapterInstanceRegularFields(t *testing.T) {
 		expectedImageID         string
 		expectedInstanceType    string
 		expectedSecurityGroupID string
-		expectedSubnetID        string
 	}{
 		{
 			description:  "empty custom object",
@@ -151,83 +150,6 @@ func TestAdapterInstanceSecurityGroupID(t *testing.T) {
 
 			if a.MasterSecurityGroupID != tc.expectedSecurityGroupID {
 				t.Errorf("unexpected SecurityGroupID, got %q, want %q", a.MasterSecurityGroupID, tc.expectedSecurityGroupID)
-			}
-		})
-	}
-}
-
-func TestAdapterInstanceSubnetID(t *testing.T) {
-	testCases := []struct {
-		description      string
-		customObject     v1alpha1.AWSConfig
-		expectedSubnetID string
-		expectedError    bool
-		unexistingSubnet bool
-	}{
-		{
-			description: "existent subnet",
-			customObject: v1alpha1.AWSConfig{
-				Spec: v1alpha1.AWSConfigSpec{
-					Cluster: v1alpha1.Cluster{
-						ID: "test-cluster",
-					},
-					AWS: v1alpha1.AWSConfigSpecAWS{
-						Masters: []v1alpha1.AWSConfigSpecAWSNode{
-							v1alpha1.AWSConfigSpecAWSNode{
-								ImageID:      "myimageid",
-								InstanceType: "myinstancetype",
-							},
-						},
-					},
-				},
-			},
-			expectedSubnetID: "subnet-1234",
-		},
-		{
-			description: "unexistent subnet",
-			customObject: v1alpha1.AWSConfig{
-				Spec: v1alpha1.AWSConfigSpec{
-					Cluster: v1alpha1.Cluster{
-						ID: "test-cluster",
-					},
-					AWS: v1alpha1.AWSConfigSpecAWS{
-						Masters: []v1alpha1.AWSConfigSpecAWSNode{
-							v1alpha1.AWSConfigSpecAWSNode{
-								ImageID:      "myimageid",
-								InstanceType: "myinstancetype",
-							},
-						},
-					},
-				},
-			},
-			unexistingSubnet: true,
-			expectedError:    true,
-			expectedSubnetID: "",
-		},
-	}
-
-	for _, tc := range testCases {
-		a := Adapter{}
-		clients := Clients{
-			EC2: &EC2ClientMock{
-				unexistingSubnet: tc.unexistingSubnet,
-				subnetID:         tc.expectedSubnetID,
-			},
-			IAM: &IAMClientMock{},
-		}
-
-		t.Run(tc.description, func(t *testing.T) {
-			err := a.getInstance(tc.customObject, clients)
-			if tc.expectedError && err == nil {
-				t.Error("expected error didn't happen")
-			}
-
-			if !tc.expectedError && err != nil {
-				t.Errorf("unexpected error %v", err)
-			}
-
-			if a.MasterSubnetID != tc.expectedSubnetID {
-				t.Errorf("unexpected SubnetID, got %q, want %q", a.MasterSubnetID, tc.expectedSubnetID)
 			}
 		})
 	}
