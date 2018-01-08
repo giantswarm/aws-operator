dfbdf6d5cff224c17874d464005aeeac9d962446
Ross Fairbanks <ross@giantswarm.io>
2018-01-08 12:46:55 +0100

    Add ingress sec group rules

--- service/resource/legacyv2/adapter/security_groups.go
+++ service/resource/legacyv2/adapter/security_groups.go
@@ -9,15 +9,47 @@ import (
 // template related to this adapter: service/templates/cloudformation/security_groups.yaml
 
 type securityGroupsAdapter struct {
-	MasterGroupName  string
-	WorkerGroupName  string
-	IngressGroupName string
+	MasterGroupName           string
+	WorkerGroupName           string
+	IngressGroupName          string
+	IngressSecurityGroupRules []securityGroupRule
 }
+
+type securityGroupRule struct {
+	Port              int
+	Protocol          string
+	SourceCIDR        string
+	SecurityGroupName string
+}
+
+const (
+	allProtocols = "-1"
+	tcpProtocol  = "tcp"
+
+	defaultCIDR = "0.0.0.0/0"
+)
 
 func (s *securityGroupsAdapter) getSecurityGroups(customObject v1alpha1.AWSConfig, clients Clients) error {
 	s.MasterGroupName = keyv2.SecurityGroupName(customObject, prefixMaster)
 	s.WorkerGroupName = keyv2.SecurityGroupName(customObject, prefixWorker)
+
 	s.IngressGroupName = keyv2.SecurityGroupName(customObject, prefixIngress)
+	s.IngressSecurityGroupRules = s.getIngressRules(customObject)
 
 	return nil
+}
+
+func (s *securityGroupsAdapter) getIngressRules(customObject v1alpha1.AWSConfig) []securityGroupRule {
+	return []securityGroupRule{
+		securityGroupRule{
+			Port:       httpPort,
+			Protocol:   tcpProtocol,
+			SourceCIDR: defaultCIDR,
+		},
+		securityGroupRule{
+			Port:       httpsPort,
+			Protocol:   tcpProtocol,
+			SourceCIDR: defaultCIDR,
+		},
+	}
 }
--- service/resource/legacyv2/adapter/security_groups_test.go
+++ service/resource/legacyv2/adapter/security_groups_test.go
@@ -1,6 +1,7 @@
 package adapter
 
 import (
+	"reflect"
 	"testing"
 
 	"github.com/giantswarm/apiextensions/pkg/apis/provider/v1alpha1"
@@ -8,12 +9,13 @@ import (
 
 func TestAdapterSecurityGroupsRegularFields(t *testing.T) {
 	testCases := []struct {
-		description              string
-		customObject             v1alpha1.AWSConfig
-		expectedError            bool
-		expectedMasterGroupName  string
-		expectedWorkerGroupName  string
-		expectedIngressGroupName string
+		description                       string
+		customObject                      v1alpha1.AWSConfig
+		expectedError                     bool
+		expectedMasterGroupName           string
+		expectedWorkerGroupName           string
+		expectedIngressGroupName          string
+		expectedIngressSecurityGroupRules []securityGroupRule
 	}{
 		{
 			description: "basic matching, all fields present",
@@ -28,6 +30,18 @@ func TestAdapterSecurityGroupsRegularFields(t *testing.T) {
 			expectedMasterGroupName:  "test-cluster-master",
 			expectedWorkerGroupName:  "test-cluster-worker",
 			expectedIngressGroupName: "test-cluster-ingress",
+			expectedIngressSecurityGroupRules: []securityGroupRule{
+				securityGroupRule{
+					Port:       80,
+					Protocol:   "tcp",
+					SourceCIDR: "0.0.0.0/0",
+				},
+				securityGroupRule{
+					Port:       443,
+					Protocol:   "tcp",
+					SourceCIDR: "0.0.0.0/0",
+				},
+			},
 		},
 	}
 	for _, tc := range testCases {
@@ -57,6 +71,10 @@ func TestAdapterSecurityGroupsRegularFields(t *testing.T) {
 
 			if a.IngressGroupName != tc.expectedIngressGroupName {
 				t.Errorf("unexpected IngressGroupName, got %q, want %q", a.IngressGroupName, tc.expectedIngressGroupName)
+			}
+
+			if !reflect.DeepEqual(a.IngressSecurityGroupRules, tc.expectedIngressSecurityGroupRules) {
+				t.Errorf("unexpected Ingress Security Group Rules, got %v, want %v", a.IngressSecurityGroupRules, tc.expectedIngressSecurityGroupRules)
 			}
 		})
 	}
