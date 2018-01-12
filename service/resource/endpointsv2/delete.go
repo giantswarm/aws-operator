3b891fb71f503623a2d7633cef8eda3e784e52e3
Ross Fairbanks <ross@giantswarm.io>
2018-01-12 16:47:42 +0100

    Implement endpoints resource, update and delete still to do

---
+++ service/resource/endpointsv2/delete.go
@@ -0,0 +1,95 @@
+package endpointsv2
+
+import (
+	"context"
+
+	"github.com/giantswarm/microerror"
+	"github.com/giantswarm/operatorkit/framework"
+)
+
+// TODO
+func (r *Resource) ApplyDeleteChange(ctx context.Context, obj, deleteChange interface{}) error {
+	return nil
+}
+
+func (r *Resource) NewDeletePatch(ctx context.Context, obj, currentState, desiredState interface{}) (*framework.Patch, error) {
+	delete, err := r.newDeleteChange(ctx, obj, currentState, desiredState)
+	if err != nil {
+		return nil, microerror.Mask(err)
+	}
+
+	patch := framework.NewPatch()
+	patch.SetDeleteChange(delete)
+
+	return patch, nil
+}
+
+// TODO
+func (r *Resource) newDeleteChange(ctx context.Context, obj, currentState, desiredState interface{}) (interface{}, error) {
+	return nil, nil
+}
+
+/*
+func (r *Resource) ApplyDeleteChange(ctx context.Context, obj, deleteChange interface{}) error {
+	customObject, err := keyv2.ToCustomObject(obj)
+	if err != nil {
+		return microerror.Mask(err)
+	}
+	endpointsToDelete, err := toEndpoints(deleteChange)
+	if err != nil {
+		return microerror.Mask(err)
+	}
+
+	if endpointsToDelete != nil {
+		r.logger.LogCtx(ctx, "debug", "deleting Kubernetes endpoints")
+
+		namespace := keyv2.ClusterNamespace(customObject)
+		err := r.k8sClient.CoreV1().Endpoints(namespace).Delete(endpointsToDelete.Name, &apismetav1.DeleteOptions{})
+		if apierrors.IsNotFound(err) {
+			// fall through
+		} else if err != nil {
+			return microerror.Mask(err)
+		}
+
+		r.logger.LogCtx(ctx, "debug", "deleting Kubernetes endpoints: deleted")
+	} else {
+		r.logger.LogCtx(ctx, "debug", "deleting Kubernetes endpoints: already deleted")
+	}
+
+	return nil
+}
+
+func (r *Resource) NewDeletePatch(ctx context.Context, obj, currentState, desiredState interface{}) (*framework.Patch, error) {
+	delete, err := r.newDeleteChange(ctx, obj, currentState, desiredState)
+	if err != nil {
+		return nil, microerror.Mask(err)
+	}
+
+	patch := framework.NewPatch()
+	patch.SetDeleteChange(delete)
+
+	return patch, nil
+}
+
+func (r *Resource) newDeleteChange(ctx context.Context, obj, currentState, desiredState interface{}) (interface{}, error) {
+	currentEndpoints, err := toEndpoints(currentState)
+	if err != nil {
+		return nil, microerror.Mask(err)
+	}
+	desiredEndpoints, err := toEndpoints(desiredState)
+	if err != nil {
+		return nil, microerror.Mask(err)
+	}
+
+	r.logger.LogCtx(ctx, "debug", "finding out if the endpoints has to be deleted")
+
+	var endpointsToDelete *apiv1.Endpoints
+	if currentEndpoints != nil && desiredEndpoints.Name == currentEndpoints.Name {
+		endpointsToDelete = desiredEndpoints
+	}
+
+	r.logger.LogCtx(ctx, "debug", "found out if the endpoints has to be deleted")
+
+	return endpointsToDelete, nil
+}
+*/
---
+++ service/resource/endpointsv2/spec.go
@@ -0,0 +1,14 @@
+package endpointsv2
+
+import (
+	"github.com/aws/aws-sdk-go/service/ec2"
+)
+
+type Clients struct {
+	EC2 EC2Client
+}
+
+// EC2Client describes the methods required to be implemented by a EC2 AWS client.
+type EC2Client interface {
+	DescribeInstances(*ec2.DescribeInstancesInput) (*ec2.DescribeInstancesOutput, error)
+}
