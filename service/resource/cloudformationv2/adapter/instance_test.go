2f9ea81f9834d7531df7a7163dfe3a4d7de009f0
Ross Fairbanks <ross@giantswarm.io>
2018-01-22 15:25:12 +0100

    Add test for instance cloud config

--- service/resource/cloudformationv2/adapter/instance_test.go
+++ service/resource/cloudformationv2/adapter/instance_test.go
@@ -1,6 +1,8 @@
 package adapter
 
 import (
+	"encoding/base64"
+	"strings"
 	"testing"
 
 	"github.com/giantswarm/apiextensions/pkg/apis/provider/v1alpha1"
@@ -77,6 +79,66 @@ func TestAdapterInstanceRegularFields(t *testing.T) {
 
 			if a.MasterInstanceType != tc.expectedInstanceType {
 				t.Errorf("unexpected MasterInstanceType, got %q, want %q", a.instanceAdapter.MasterInstanceType, tc.expectedInstanceType)
+			}
+		})
+	}
+}
+
+func TestAdapterInstanceSmallCloudConfig(t *testing.T) {
+	testCases := []struct {
+		description  string
+		expectedLine string
+	}{
+		{
+			description:  "userdata file",
+			expectedLine: "USERDATA_FILE=master",
+		},
+		{
+			description:  "s3 http uri",
+			expectedLine: `s3_http_uri="https://s3.myregion.amazonaws.com/000000000000-g8s-test-cluster/cloudconfig/v_3_0_0/$USERDATA_FILE"`,
+		},
+	}
+
+	a := Adapter{}
+	clients := Clients{
+		EC2: &EC2ClientMock{},
+		IAM: &IAMClientMock{accountID: "000000000000"},
+	}
+	customObject := v1alpha1.AWSConfig{
+		Spec: v1alpha1.AWSConfigSpec{
+			Cluster: v1alpha1.Cluster{
+				ID: "test-cluster",
+			},
+			AWS: v1alpha1.AWSConfigSpecAWS{
+				Region: "myregion",
+				Masters: []v1alpha1.AWSConfigSpecAWSNode{
+					v1alpha1.AWSConfigSpecAWSNode{
+						ImageID:      "ami-test",
+						InstanceType: "m3.large",
+					},
+				},
+			},
+		},
+	}
+	cfg := Config{
+		CustomObject: customObject,
+		Clients:      clients,
+	}
+	err := a.getInstance(cfg)
+
+	if err != nil {
+		t.Errorf("unexpected error %v", err)
+	}
+
+	data, err := base64.StdEncoding.DecodeString(a.MasterSmallCloudConfig)
+	if err != nil {
+		t.Errorf("unexpected error decoding SmallCloudConfig %v", err)
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.description, func(t *testing.T) {
+			if !strings.Contains(string(data), tc.expectedLine) {
+				t.Errorf("SmallCloudConfig didn't contain expected %q, complete: %q", tc.expectedLine, string(data))
 			}
 		})
 	}
