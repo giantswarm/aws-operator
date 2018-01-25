750bb3a2d8f04476f36e2e652f717d02ec567cd0
Fernando Ripoll <pipo02mix@gmail.com>
2018-01-25 17:26:23 +0100

    Add cluster tags in the bucket too

--- service/resource/s3bucketv2/create.go
+++ service/resource/s3bucketv2/create.go
@@ -6,9 +6,16 @@ import (
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/service/s3"
 	"github.com/giantswarm/microerror"
+
+	"github.com/giantswarm/aws-operator/service/keyv2"
 )
 
 func (r *Resource) ApplyCreateChange(ctx context.Context, obj, createChange interface{}) error {
+	customObject, err := keyv2.ToCustomObject(obj)
+	if err != nil {
+		return microerror.Mask(err)
+	}
+
 	bucketInput, err := toBucketState(createChange)
 	if err != nil {
 		return microerror.Mask(err)
@@ -28,11 +35,20 @@ func (r *Resource) ApplyCreateChange(ctx context.Context, obj, createChange inte
 			return microerror.Mask(err)
 		}
 
+		_, err := r.clients.S3.PutBucketTagging(&s3.PutBucketTaggingInput{
+			Bucket: aws.String(bucketInput.Name),
+			Tagging: &s3.Tagging{
+				TagSet: getS3BucketTags(customObject),
+			},
+		})
+		if err != nil {
+			return microerror.Mask(err)
+		}
+
 		r.logger.LogCtx(ctx, "debug", "creating S3 bucket: created")
 	} else {
 		r.logger.LogCtx(ctx, "debug", "creating S3 bucket: already created")
 	}
-
 	return nil
 }
 
