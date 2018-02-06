f6d306a568029037a8d53f0471c7bf8d27cf0590
Paweł Kopiczko <kopiczko@gmail.com>
2018-02-06 12:05:53 +0100

    awsconfig/v4: copy v3

---
+++ service/awsconfig/v4/cloudconfig/templates.go
@@ -0,0 +1,219 @@
+package cloudconfig
+
+const (
+	decryptTLSAssetsScriptTemplate = `#!/bin/bash -e
+
+rkt run \
+  --volume=ssl,kind=host,source=/etc/kubernetes/ssl,readOnly=false \
+  --mount=volume=ssl,target=/etc/kubernetes/ssl \
+  --uuid-file-save=/var/run/coreos/decrypt-tls-assets.uuid \
+  --volume=dns,kind=host,source=/etc/resolv.conf,readOnly=true --mount volume=dns,target=/etc/resolv.conf \
+  --net=host \
+  --trust-keys-from-https \
+  quay.io/coreos/awscli:025a357f05242fdad6a81e8a6b520098aa65a600 --exec=/bin/bash -- \
+    -ec \
+    'echo decrypting tls assets
+    shopt -s nullglob
+    for encKey in $(find /etc/kubernetes/ssl -name "*.pem.enc"); do
+      echo decrypting $encKey
+      f=$(mktemp $encKey.XXXXXXXX)
+      /usr/bin/aws \
+        --region {{.AWS.Region}} kms decrypt \
+        --ciphertext-blob fileb://$encKey \
+        --output text \
+        --query Plaintext \
+      | base64 -d > $f
+      mv -f $f ${encKey%.enc}
+    done;'
+
+rkt rm --uuid-file=/var/run/coreos/decrypt-tls-assets.uuid || :
+
+chown -R etcd:etcd /etc/kubernetes/ssl/etcd`
+
+	decryptTLSAssetsServiceTemplate = `
+[Unit]
+Description=Decrypt TLS certificates
+
+[Service]
+Type=oneshot
+ExecStart=/opt/bin/decrypt-tls-assets
+
+[Install]
+WantedBy=multi-user.target
+`
+
+	decryptKeysAssetsScriptTemplate = `#!/bin/bash -e
+
+rkt run \
+  --volume=keys,kind=host,source=/etc/kubernetes/encryption,readOnly=false \
+  --mount=volume=keys,target=/etc/kubernetes/encryption \
+  --uuid-file-save=/var/run/coreos/decrypt-keys-assets.uuid \
+  --volume=dns,kind=host,source=/etc/resolv.conf,readOnly=true --mount volume=dns,target=/etc/resolv.conf \
+  --net=host \
+  --trust-keys-from-https \
+  quay.io/coreos/awscli:025a357f05242fdad6a81e8a6b520098aa65a600 --exec=/bin/bash -- \
+    -ec \
+    'echo decrypting keys assets
+    shopt -s nullglob
+    for encKey in $(find /etc/kubernetes/encryption -name "*.enc"); do
+      echo decrypting $encKey
+      f=$(mktemp $encKey.XXXXXXXX)
+      /usr/bin/aws \
+        --region {{.AWS.Region}} kms decrypt \
+        --ciphertext-blob fileb://$encKey \
+        --output text \
+        --query Plaintext \
+      | base64 -d > $f
+      mv -f $f ${encKey%.enc}
+    done;
+    echo done.'
+
+rkt rm --uuid-file=/var/run/coreos/decrypt-keys-assets.uuid || :
+`
+
+	decryptKeysServiceTemplate = `
+[Unit]
+Description=Decrypt Secret Keys
+
+[Service]
+Type=oneshot
+ExecStart=/opt/bin/decrypt-keys-assets
+
+[Install]
+WantedBy=multi-user.target
+`
+
+	masterFormatVarLibDockerServiceTemplate = `
+[Unit]
+Description=Format /var/lib/docker to XFS
+Before=docker.service var-lib-docker.mount
+
+[Service]
+Type=oneshot
+ExecStart=/usr/sbin/mkfs.xfs -f /dev/xvdb
+
+[Install]
+WantedBy=multi-user.target
+`
+
+	workerFormatVarLibDockerServiceTemplate = `
+[Unit]
+Description=Format /var/lib/docker to XFS
+Before=docker.service var-lib-docker.mount
+ConditionPathExists=!/var/lib/docker
+
+[Service]
+Type=oneshot
+ExecStart=/usr/sbin/mkfs.xfs -f /dev/xvdh
+
+[Install]
+WantedBy=multi-user.target
+`
+
+	ephemeralVarLibDockerMountTemplate = `
+[Unit]
+Description=Mount ephemeral volume on /var/lib/docker
+
+[Mount]
+What=/dev/xvdb
+Where=/var/lib/docker
+Type=xfs
+
+[Install]
+RequiredBy=local-fs.target
+`
+	persistentVarLibDockerMountTemplate = `
+[Unit]
+Description=Mount persistent volume on /var/lib/docker
+
+[Mount]
+What=/dev/xvdh
+Where=/var/lib/docker
+Type=xfs
+
+[Install]
+RequiredBy=local-fs.target
+`
+
+	waitDockerConfTemplate = `
+[Unit]
+After=var-lib-docker.mount
+Requires=var-lib-docker.mount
+`
+
+	instanceStorageTemplate = `
+storage:
+  filesystems:
+    - name: ephemeral1
+      mount:
+        device: /dev/xvdb
+        format: xfs
+        create:
+          force: true
+`
+
+	instanceStorageClassTemplate = `
+write_files:
+- path: /srv/default-storage-class.yaml
+  owner: root
+  permissions: 644
+  content: |
+    apiVersion: storage.k8s.io/v1beta1
+    kind: StorageClass
+    metadata:
+      name: gp2
+      annotations:
+        storageclass.beta.kubernetes.io/is-default-class: "true"
+      labels:
+        kubernetes.io/cluster-service: "true"
+        addonmanager.kubernetes.io/mode: EnsureExists
+    provisioner: kubernetes.io/aws-ebs
+    parameters:
+      type: gp2
+      encrypted: "true"
+`
+
+	ingressControllerConfigMapTemplate = `kind: ConfigMap
+apiVersion: v1
+metadata:
+  name: ingress-nginx
+  namespace: kube-system
+  labels:
+    k8s-addon: ingress-nginx.addons.k8s.io
+data:
+  server-name-hash-bucket-size: "1024"
+  server-name-hash-max-size: "1024"
+  use-proxy-protocol: "true"
+`
+	formatEtcdVolume = `
+[Unit]
+Description=Formats EBS /dev/xvdh volume
+Requires=dev-xvdh.device
+After=dev-xvdh.device
+ConditionPathExists=!/var/lib/etcd-volume-formated
+
+[Service]
+Type=oneshot
+RemainAfterExit=yes
+ExecStart=/usr/sbin/mkfs.ext4 /dev/xvdh
+ExecStartPost=/usr/bin/touch /var/lib/etcd-volume-formated
+
+[Install]
+WantedBy=multi-user.target
+`
+	mountEtcdVolume = `
+[Unit]
+Description=etcd3 data volume
+Requires=format-etcd-ebs.service
+After=format-etcd-ebs.service
+Before=set-ownership-etcd-data-dir.service etcd3.service
+
+[Mount]
+What=/dev/xvdh
+Where=/etc/kubernetes/data/etcd
+Type=ext4
+
+[Install]
+WantedBy=multi-user.target
+`
+)
