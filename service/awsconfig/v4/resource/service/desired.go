7c48b6bfeedc412640ac8983ff3bb371da4e5626
Paweł Kopiczko <kopiczko@gmail.com>
2018-02-06 12:12:32 +0100

    awsconfig/v4: copy v3 (#716)

---
+++ service/awsconfig/v4/resource/service/desired.go
@@ -0,0 +1,45 @@
+package service
+
+import (
+	"context"
+
+	"github.com/giantswarm/microerror"
+	"k8s.io/api/core/v1"
+	apismetav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/util/intstr"
+
+	"github.com/giantswarm/aws-operator/service/awsconfig/v2/key"
+)
+
+func (r *Resource) GetDesiredState(ctx context.Context, obj interface{}) (interface{}, error) {
+	customObject, err := key.ToCustomObject(obj)
+	if err != nil {
+		return nil, microerror.Mask(err)
+	}
+
+	service := &v1.Service{
+		ObjectMeta: apismetav1.ObjectMeta{
+			Name:      "master",
+			Namespace: key.ClusterID(customObject),
+			Labels: map[string]string{
+				"app":      "master",
+				"cluster":  key.ClusterID(customObject),
+				"customer": key.CustomerID(customObject),
+			},
+			Annotations: map[string]string{
+				"giantswarm.io/prometheus-cluster": key.ClusterID(customObject),
+			},
+		},
+		Spec: v1.ServiceSpec{
+			Ports: []v1.ServicePort{
+				{
+					Protocol:   v1.ProtocolTCP,
+					Port:       httpsPort,
+					TargetPort: intstr.FromInt(httpsPort),
+				},
+			},
+		},
+	}
+
+	return service, nil
+}
