8f492355195942aba196c4425cc1f34335c6e612
Paweł Kopiczko <kopiczko@gmail.com>
2018-02-06 11:46:46 +0100

    awsconfig/v3: copy missing v2 resources (#717)
    Not imported yet.

---
+++ service/awsconfig/v3/resource/cloudformation/adapter/launch_configuration.go
@@ -0,0 +1,64 @@
+package adapter
+
+import (
+	"fmt"
+
+	"github.com/giantswarm/microerror"
+
+	"github.com/giantswarm/aws-operator/service/awsconfig/v2/key"
+	// NOTE(PK): This import is disturbing. I'm not bothering. It's first candidate to go away.
+	"github.com/giantswarm/aws-operator/service/awsconfig/v3/cloudconfig"
+)
+
+// template related to this adapter: service/templates/cloudformation/guest/launch_configuration.yaml
+
+type launchConfigAdapter struct {
+	WorkerAssociatePublicIPAddress bool
+	WorkerBlockDeviceMappings      []BlockDeviceMapping
+	WorkerInstanceType             string
+	WorkerSecurityGroupID          string
+	WorkerSmallCloudConfig         string
+}
+
+type BlockDeviceMapping struct {
+	DeleteOnTermination bool
+	DeviceName          string
+	VolumeSize          int
+	VolumeType          string
+}
+
+func (l *launchConfigAdapter) getLaunchConfiguration(cfg Config) error {
+	l.WorkerInstanceType = key.WorkerInstanceType(cfg.CustomObject)
+	l.WorkerAssociatePublicIPAddress = false
+
+	l.WorkerBlockDeviceMappings = []BlockDeviceMapping{
+		BlockDeviceMapping{
+			DeleteOnTermination: true,
+			DeviceName:          defaultEBSVolumeMountPoint,
+			VolumeSize:          defaultEBSVolumeSize,
+			VolumeType:          defaultEBSVolumeType,
+		},
+	}
+
+	// small cloud config field.
+	accountID, err := AccountID(cfg.Clients)
+	if err != nil {
+		return microerror.Mask(err)
+	}
+	clusterID := key.ClusterID(cfg.CustomObject)
+	s3URI := fmt.Sprintf("%s-g8s-%s", accountID, clusterID)
+
+	cloudConfigConfig := SmallCloudconfigConfig{
+		MachineType:        prefixWorker,
+		Region:             cfg.CustomObject.Spec.AWS.Region,
+		S3URI:              s3URI,
+		CloudConfigVersion: cloudconfig.WorkerCloudConfigVersion,
+	}
+	smallCloudConfig, err := SmallCloudconfig(cloudConfigConfig)
+	if err != nil {
+		return microerror.Mask(err)
+	}
+	l.WorkerSmallCloudConfig = smallCloudConfig
+
+	return nil
+}
