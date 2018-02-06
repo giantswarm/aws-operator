2f6364d0014338fe81981686f25159f5f1c4c215
Paweł Kopiczko <kopiczko@gmail.com>
2018-02-06 08:32:43 +0100

    mv legacy resource to v1

--- service/awsconfig/v1/resource_set.go
+++ service/awsconfig/v1/resource_set.go
@@ -10,8 +10,8 @@ import (
 	"k8s.io/client-go/kubernetes"
 
 	awsclient "github.com/giantswarm/aws-operator/client/aws"
+	legacyv2 "github.com/giantswarm/aws-operator/service/awsconfig/v1/resource/legacy"
 	"github.com/giantswarm/aws-operator/service/awsconfig/v2/key"
-	legacyv2 "github.com/giantswarm/aws-operator/service/awsconfig/v2/resource/legacy"
 	// TODO v2 cloudconfig is used in legacy resource, v3 cloudconfig is used in v2 resources and v4 cloudconfig in v3 resources.
 	"github.com/giantswarm/aws-operator/service/awsconfig/v2/cloudconfig"
 )
