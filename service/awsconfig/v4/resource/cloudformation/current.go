291c343355f773ce3ab1093290a2e9ee549b496b
Paweł Kopiczko <kopiczko@gmail.com>
2018-02-06 13:12:30 +0100

    awsconfig/v4: copy v3 (#724)

---
+++ service/awsconfig/v4/resource/cloudformation/current.go
@@ -0,0 +1,102 @@
+package cloudformation
+
+import (
+	"context"
+
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/service/cloudformation"
+	"github.com/giantswarm/microerror"
+
+	"github.com/giantswarm/aws-operator/service/awsconfig/v3/key"
+)
+
+func (r *Resource) GetCurrentState(ctx context.Context, obj interface{}) (interface{}, error) {
+	customObject, err := key.ToCustomObject(obj)
+	if err != nil {
+		return StackState{}, microerror.Mask(err)
+	}
+
+	r.logger.LogCtx(ctx, "debug", "looking for AWS stack")
+
+	stackName := key.MainGuestStackName(customObject)
+
+	describeInput := &cloudformation.DescribeStacksInput{
+		StackName: aws.String(stackName),
+	}
+	describeOutput, err := r.Clients.CloudFormation.DescribeStacks(describeInput)
+
+	if IsStackNotFound(err) {
+		r.logger.LogCtx(ctx, "debug", "did not find a stack in AWS API")
+		return StackState{}, nil
+	}
+	if err != nil {
+		return StackState{}, microerror.Mask(err)
+	}
+
+	if len(describeOutput.Stacks) > 1 {
+		return StackState{}, microerror.Mask(notFoundError)
+	}
+
+	// current is called on cluster deletion, if the stack creation failed the
+	// outputs can be unaccessible, this can lead to a stack that cannot be deleted.
+	// it can also be called during creation, while the outputs are still not
+	// accessible.
+	status := describeOutput.Stacks[0].StackStatus
+	errorStatuses := []string{
+		"ROLLBACK_IN_PROGRESS",
+		"ROLLBACK_COMPLETE",
+		"CREATE_IN_PROGRESS",
+	}
+	for _, errorStatus := range errorStatuses {
+		if *status == errorStatus {
+			outputStackState := StackState{
+				Name: stackName,
+			}
+			return outputStackState, nil
+		}
+	}
+
+	outputs := describeOutput.Stacks[0].Outputs
+
+	masterImageID, err := getStackOutputValue(outputs, masterImageIDOutputKey)
+	if err != nil {
+		return StackState{}, microerror.Mask(err)
+	}
+	masterInstanceType, err := getStackOutputValue(outputs, masterInstanceTypeOutputKey)
+	if err != nil {
+		return StackState{}, microerror.Mask(err)
+	}
+	masterCloudConfigVersion, err := getStackOutputValue(outputs, masterCloudConfigVersionOutputKey)
+	if err != nil {
+		return StackState{}, microerror.Mask(err)
+	}
+	workers, err := getStackOutputValue(outputs, workersOutputKey)
+	if err != nil {
+		return StackState{}, microerror.Mask(err)
+	}
+	workerImageID, err := getStackOutputValue(outputs, workerImageIDOutputKey)
+	if err != nil {
+		return StackState{}, microerror.Mask(err)
+	}
+	workerInstanceType, err := getStackOutputValue(outputs, workerInstanceTypeOutputKey)
+	if err != nil {
+		return StackState{}, microerror.Mask(err)
+	}
+	workerCloudConfigVersion, err := getStackOutputValue(outputs, workerCloudConfigVersionOutputKey)
+	if err != nil {
+		return StackState{}, microerror.Mask(err)
+	}
+
+	outputStackState := StackState{
+		Name:                     stackName,
+		MasterImageID:            masterImageID,
+		MasterInstanceType:       masterInstanceType,
+		MasterCloudConfigVersion: masterCloudConfigVersion,
+		WorkerCount:              workers,
+		WorkerImageID:            workerImageID,
+		WorkerInstanceType:       workerInstanceType,
+		WorkerCloudConfigVersion: workerCloudConfigVersion,
+	}
+
+	return outputStackState, nil
+}
---
+++ service/awsconfig/v4/resource/cloudformation/spec.go
@@ -0,0 +1,34 @@
+package cloudformation
+
+const (
+	// defaultCreationTimeout is the timeout in minutes for the creation of the stack.
+	defaultCreationTimeout = 10
+
+	masterImageIDOutputKey            = "MasterImageID"
+	masterInstanceTypeOutputKey       = "MasterInstanceType"
+	masterCloudConfigVersionOutputKey = "MasterCloudConfigVersion"
+	workersOutputKey                  = "WorkerCount"
+	workerImageIDOutputKey            = "WorkerImageID"
+	workerInstanceTypeOutputKey       = "WorkerInstanceType"
+	workerCloudConfigVersionOutputKey = "WorkerCloudConfigVersion"
+
+	workerRoleKey = "WorkerRole"
+
+	cloudFormationGuestTemplatesDirectory    = "service/templates/cloudformation/guest"
+	cloudFormationHostPreTemplatesDirectory  = "service/templates/cloudformation/host-pre"
+	cloudFormationHostPostTemplatesDirectory = "service/templates/cloudformation/host-post"
+
+	namedIAMCapability = "CAPABILITY_NAMED_IAM"
+)
+
+// StackState is the state representation on which the resource methods work.
+type StackState struct {
+	Name                     string
+	MasterImageID            string
+	MasterInstanceType       string
+	MasterCloudConfigVersion string
+	WorkerCount              string
+	WorkerImageID            string
+	WorkerInstanceType       string
+	WorkerCloudConfigVersion string
+}
