4285b1a739ec880684be69e5c2027fc2c33269de
Ross Fairbanks <rossf7@users.noreply.github.com>
2018-02-14 18:14:25 +0100

    awsconfig/v5: copy v4 (#743)

---
+++ service/awsconfig/v5/resource/cloudformation/desired.go
@@ -0,0 +1,23 @@
+package cloudformation
+
+import (
+	"context"
+
+	"github.com/giantswarm/microerror"
+
+	"github.com/giantswarm/aws-operator/service/awsconfig/v4/key"
+)
+
+func (r *Resource) GetDesiredState(ctx context.Context, obj interface{}) (interface{}, error) {
+	customObject, err := key.ToCustomObject(obj)
+	if err != nil {
+		return nil, microerror.Mask(err)
+	}
+
+	mainStack, err := newMainStack(customObject)
+	if err != nil {
+		return nil, microerror.Mask(err)
+	}
+
+	return mainStack, nil
+}
---
+++ service/awsconfig/v5/templates/cloudformation/guest/main.go
@@ -0,0 +1,19 @@
+package guest
+
+const Main = `{{define "main"}}AWSTemplateFormatVersion: 2010-09-09
+Description: Main Guest CloudFormation stack.
+Resources:
+  {{template "vpc" .}}
+  {{template "iam_policies" .}}
+  {{template "security_groups" .}}
+  {{template "route_tables" .}}
+  {{template "subnets" .}}
+  {{template "internet_gateway" .}}
+  {{template "nat_gateway" .}}
+  {{template "instance" .}}
+  {{template "load_balancers" .}}
+  {{template "launch_configuration" .}}
+  {{template "autoscaling_group" .}}
+  {{template "recordsets" .}}
+{{template "outputs" .}}
+{{end}}`
