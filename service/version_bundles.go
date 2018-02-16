af06e4f23249bb288f91ad13fd23d5174c280c8d
Federico Gimenez <federico@giantswarm.io>
2018-02-16 14:53:53 +0100

    fixed indentation after online deconflict

--- service/version_bundles.go
+++ service/version_bundles.go
@@ -378,7 +378,7 @@ func NewVersionBundles() []versionbundle.Bundle {
 					Description: "Add AWS resource tag with GiantSwarm Cluster ID for reporting.",
 					Kind:        versionbundle.KindAdded,
 				},
-        {
+				{
 					Component:   "aws-operator",
 					Description: "Change default etcd data dir to /var/lib/etcd.",
 					Kind:        versionbundle.KindAdded,
