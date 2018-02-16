41281192ab322a4f8d6edf92b98839f5cd0d8ab9
Ross Fairbanks <ross@giantswarm.io>
2018-02-16 17:15:31 +0100

    Remove awsService

--- service/awsconfig/v5/resource/loadbalancer/resource.go
+++ service/awsconfig/v5/resource/loadbalancer/resource.go
@@ -6,8 +6,6 @@ import (
 	"github.com/giantswarm/microerror"
 	"github.com/giantswarm/micrologger"
 	"github.com/giantswarm/operatorkit/framework"
-
-	awsservice "github.com/giantswarm/aws-operator/service/aws"
 )
 
 const (
@@ -18,25 +16,20 @@ const (
 // Config represents the configuration used to create a new loadbalancer resource.
 type Config struct {
 	// Dependencies.
-	AwsService *awsservice.Service
-	Clients    Clients
-	Logger     micrologger.Logger
+	Clients Clients
+	Logger  micrologger.Logger
 }
 
 // Resource implements the loadbalancer resource.
 type Resource struct {
 	// Dependencies.
-	awsService *awsservice.Service
-	clients    Clients
-	logger     micrologger.Logger
+	clients Clients
+	logger  micrologger.Logger
 }
 
 // New creates a new configured loadbalancer resource.
 func New(config Config) (*Resource, error) {
 	// Dependencies.
-	if config.AwsService == nil {
-		return nil, microerror.Maskf(invalidConfigError, "config.AwsService must not be empty")
-	}
 	if reflect.DeepEqual(config.Clients, Clients{}) {
 		return nil, microerror.Maskf(invalidConfigError, "config.Clients must not be empty")
 	}
@@ -46,8 +39,7 @@ func New(config Config) (*Resource, error) {
 
 	newResource := &Resource{
 		// Dependencies.
-		awsService: config.AwsService,
-		clients:    config.Clients,
+		clients: config.Clients,
 		logger: config.Logger.With(
 			"resource", Name,
 		),
