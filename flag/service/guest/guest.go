962e2d1a58b1dbf12b9204c65670ecb024c6a271
Tim Schindler <xh3b4sd@gmail.com>
2018-02-19 21:05:39 +0100

    fixed imports

--- flag/service/guest/guest.go
+++ flag/service/guest/guest.go
@@ -1,6 +1,6 @@
 package guest
 
-import "github.com/giantswarm/kvm-operator/flag/service/guest/update"
+import "github.com/giantswarm/aws-operator/flag/service/guest/update"
 
 type Guest struct {
 	Update update.Update
