79b8846f5cbdc43a5c915dacda27974113afbfd6
Ross Fairbanks <ross@giantswarm.io>
2018-02-20 11:56:56 +0100

    Desired state should return nil

--- service/awsconfig/v5/resource/loadbalancer/desired.go
+++ service/awsconfig/v5/resource/loadbalancer/desired.go
@@ -4,12 +4,7 @@ import (
 	"context"
 )
 
-// GetDesiredState returns an empty state as this resource only implements
-// deletion.
+// GetDesiredState returns nil as this resource only implements deletion.
 func (r *Resource) GetDesiredState(ctx context.Context, obj interface{}) (interface{}, error) {
-	emptyState := &LoadBalancerState{
-		LoadBalancerNames: []string{},
-	}
-
-	return emptyState, nil
+	return nil, nil
 }
--- service/awsconfig/v5/resource/loadbalancer/desired_test.go
+++ service/awsconfig/v5/resource/loadbalancer/desired_test.go
@@ -2,7 +2,6 @@ package loadbalancer
 
 import (
 	"context"
-	"reflect"
 	"testing"
 
 	"github.com/giantswarm/apiextensions/pkg/apis/provider/v1alpha1"
@@ -18,17 +17,6 @@ func Test_DesiredState(t *testing.T) {
 		},
 	}
 
-	testCases := []struct {
-		description   string
-		expectedState *LoadBalancerState
-	}{
-		{
-			description: "basic match returns empty state",
-			expectedState: &LoadBalancerState{
-				LoadBalancerNames: []string{},
-			},
-		},
-	}
 	var err error
 	var newResource *Resource
 
@@ -43,21 +31,12 @@ func Test_DesiredState(t *testing.T) {
 		t.Error("expected", nil, "got", err)
 	}
 
-	for _, tc := range testCases {
-		t.Run(tc.description, func(t *testing.T) {
+	result, err := newResource.GetDesiredState(context.TODO(), customObject)
+	if err != nil {
+		t.Errorf("unexpected error %v", err)
+	}
 
-			result, err := newResource.GetDesiredState(context.TODO(), customObject)
-			if err != nil {
-				t.Errorf("unexpected error %v", err)
-			}
-			desiredState, ok := result.(*LoadBalancerState)
-			if !ok {
-				t.Errorf("expected '%T', got '%T'", desiredState, result)
-			}
-
-			if !reflect.DeepEqual(desiredState, tc.expectedState) {
-				t.Errorf("expected desired state '%#v', got '%#v'", tc.expectedState, desiredState)
-			}
-		})
+	if result != nil {
+		t.Errorf("expected desired state '%v', got '%v'", nil, result)
 	}
 }
