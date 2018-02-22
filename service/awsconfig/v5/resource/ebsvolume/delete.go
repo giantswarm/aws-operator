388b6f3b368080567c5b128fc44fa099ef5cdd7e
Ross Fairbanks <rossf7@users.noreply.github.com>
2018-02-22 18:06:30 +0100

    Implement deleting EBS volumes (#770)

--- service/awsconfig/v5/resource/ebsvolume/delete.go
+++ service/awsconfig/v5/resource/ebsvolume/delete.go
@@ -2,13 +2,37 @@ package ebsvolume
 
 import (
 	"context"
+	"fmt"
 
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/service/ec2"
 	"github.com/giantswarm/microerror"
 	"github.com/giantswarm/operatorkit/framework"
 )
 
-// TODO
 func (r *Resource) ApplyDeleteChange(ctx context.Context, obj, deleteChange interface{}) error {
+	deleteInput, err := toEBSVolumeState(deleteChange)
+	if err != nil {
+		return microerror.Mask(err)
+	}
+
+	if deleteInput != nil && len(deleteInput.VolumeIDs) > 0 {
+		r.logger.LogCtx(ctx, "debug", fmt.Sprintf("deleting %d ebs volumes", len(deleteInput.VolumeIDs)))
+
+		for _, volID := range deleteInput.VolumeIDs {
+			_, err := r.clients.EC2.DeleteVolume(&ec2.DeleteVolumeInput{
+				VolumeId: aws.String(volID),
+			})
+			if err != nil {
+				return microerror.Mask(err)
+			}
+		}
+
+		r.logger.LogCtx(ctx, "debug", fmt.Sprintf("deleted %d ebs volumes", len(deleteInput.VolumeIDs)))
+	} else {
+		r.logger.LogCtx(ctx, "debug", "not deleting load ebs volumes because there aren't any")
+	}
+
 	return nil
 }
 
@@ -24,7 +48,20 @@ func (r *Resource) NewDeletePatch(ctx context.Context, obj, currentState, desire
 	return patch, nil
 }
 
-// TODO
 func (r *Resource) newDeleteChange(ctx context.Context, obj, currentState, desiredState interface{}) (interface{}, error) {
-	return nil, nil
+	currentVolState, err := toEBSVolumeState(currentState)
+	if err != nil {
+		return nil, microerror.Mask(err)
+	}
+	desiredVolState, err := toEBSVolumeState(desiredState)
+	if err != nil {
+		return nil, microerror.Mask(err)
+	}
+
+	var volStateToDelete *EBSVolumeState
+	if desiredVolState == nil && currentVolState != nil && len(currentVolState.VolumeIDs) > 0 {
+		volStateToDelete = currentVolState
+	}
+
+	return volStateToDelete, nil
 }
