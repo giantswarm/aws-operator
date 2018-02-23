92d6ab2abcdbed6ccececfecc522dec932b32f0f
Ross Fairbanks <ross@giantswarm.io>
2018-02-23 14:35:22 +0100

    Deprecate 2.1.0.

--- service/version_bundles.go
+++ service/version_bundles.go
@@ -360,7 +360,7 @@ func NewVersionBundles() []versionbundle.Bundle {
 				},
 			},
 			Dependencies: []versionbundle.Dependency{},
-			Deprecated:   false,
+			Deprecated:   true,
 			Name:         "aws-operator",
 			Time:         time.Date(2018, time.February, 6, 12, 17, 0, 0, time.UTC),
 			Version:      "2.1.0",
