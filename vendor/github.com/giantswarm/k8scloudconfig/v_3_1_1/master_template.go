fdc5d8bb3182afff2ae24680e23534f1a8468542
Ross Fairbanks <ross@giantswarm.io>
2018-02-26 09:33:38 +0100

    vendor: update k8scloudconfig

--- vendor/github.com/giantswarm/k8scloudconfig/v_3_1_1/master_template.go
+++ vendor/github.com/giantswarm/k8scloudconfig/v_3_1_1/master_template.go
@@ -2402,8 +2402,9 @@ coreos:
       --pod-manifest-path=/etc/kubernetes/manifests \
       --kubeconfig=/etc/kubernetes/config/kubelet-kubeconfig.yml \
       --node-labels="node-role.kubernetes.io/master,role=master,kubernetes.io/hostname=${HOSTNAME},ip=${DEFAULT_IPV4},{{.Cluster.Kubernetes.Kubelet.Labels}}" \
-      --kube-reserved="cpu=150m,memory=250Mi" \
+      --kube-reserved="cpu=200m,memory=250Mi" \
       --system-reserved="cpu=150m,memory=250Mi" \
+      --enforce-node-allocatable=pods \
       --v=2"
       ExecStop=-/usr/bin/docker stop -t 10 $NAME
       ExecStopPost=-/usr/bin/docker rm -f $NAME
