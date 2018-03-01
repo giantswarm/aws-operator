25fc504ef333de328d7c11d7b9bf653f27c2aedc
Tim Schindler <xh3b4sd@gmail.com>
2018-03-01 13:03:20 +0100

    fixed template indentation

--- service/awsconfig/v7/templates/cloudformation/guest/lifecycle_hooks.go
+++ service/awsconfig/v7/templates/cloudformation/guest/lifecycle_hooks.go
@@ -1,13 +1,13 @@
 package guest
 
 const LifecycleHooks = `{{define "lifecycle_hooks"}}
-NodeDrainerLifecycleHook:
-  Type: "AWS::AutoScaling::LifecycleHook"
-  Properties:
-    AutoScalingGroupName:
-      Ref: {{ .LifecycleHooks.NodeDrainer.Name }}
-    DefaultResult: Continue
-    HeartbeatTimeout: 300
-    LifecycleHookName: NodeDrainer
-    LifecycleTransition: "autoscaling:EC2_INSTANCE_TERMINATING"
+  NodeDrainerLifecycleHook:
+    Type: "AWS::AutoScaling::LifecycleHook"
+    Properties:
+      AutoScalingGroupName:
+        Ref: {{ .LifecycleHooks.NodeDrainer.Name }}
+      DefaultResult: Continue
+      HeartbeatTimeout: 300
+      LifecycleHookName: NodeDrainer
+      LifecycleTransition: "autoscaling:EC2_INSTANCE_TERMINATING"
 {{end}}`
