a34aa78a0a78d86b7dba4b87ee98fcc163996226
Tim Schindler <xh3b4sd@gmail.com>
2018-03-01 11:03:51 +0100

    first try to inspect lifecycle hooks

--- service/awsconfig/v7/resource/lifecycle/current.go
+++ service/awsconfig/v7/resource/lifecycle/current.go
@@ -2,9 +2,40 @@ package lifecycle
 
 import (
 	"context"
+	"fmt"
+
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/awserr"
+	"github.com/aws/aws-sdk-go/service/autoscaling"
+)
+
+const (
+	WorkerASGName = "WorkerAutoScalingGroup"
 )
 
 func (r *Resource) GetCurrentState(ctx context.Context, obj interface{}) (interface{}, error) {
-	r.logger.LogCtx(ctx, "level", "debug", "message", "lifecycle resource got executed")
+	i := &autoscaling.DescribeLifecycleHooksInput{
+		AutoScalingGroupName: aws.String(WorkerASGName),
+	}
+	o, err := r.clients.AutoScaling.DescribeLifecycleHooks(i)
+	if err != nil {
+		if aerr, ok := err.(awserr.Error); ok {
+			r.logger.LogCtx(ctx, "code", aerr.Code(), "level", "error", "message", "describing lifecycle hooks", "stack", fmt.Sprintf("%#v\n", err))
+		} else {
+			r.logger.LogCtx(ctx, "level", "error", "message", "describing lifecycle hooks", "stack", fmt.Sprintf("%#v\n", err))
+		}
+	}
+
+	fmt.Printf("\n")
+	fmt.Printf("\n")
+	fmt.Printf("\n")
+	for _, l := range o.LifecycleHooks {
+		fmt.Printf("l.GoString(): %s\n", l.GoString())
+		fmt.Printf("\n")
+		fmt.Printf("l.String(): %s\n", l.String())
+	}
+	fmt.Printf("\n")
+	fmt.Printf("\n")
+	fmt.Printf("\n")
 	return nil, nil
 }
