fd6127a32a1192c0dd2c78bf93d9ce469daf5dc1
Tim Schindler <xh3b4sd@gmail.com>
2018-03-02 15:32:54 +0100

    moved adapters next to templates (#807)

--- service/awsconfig/v7/resource/cloudformation/main_stack.go
+++ service/awsconfig/v7/resource/cloudformation/main_stack.go
@@ -6,9 +6,9 @@ import (
 	"github.com/giantswarm/apiextensions/pkg/apis/provider/v1alpha1"
 	"github.com/giantswarm/microerror"
 
+	"github.com/giantswarm/aws-operator/service/awsconfig/v7/adapter"
 	"github.com/giantswarm/aws-operator/service/awsconfig/v7/cloudconfig"
 	"github.com/giantswarm/aws-operator/service/awsconfig/v7/key"
-	"github.com/giantswarm/aws-operator/service/awsconfig/v7/resource/cloudformation/adapter"
 	"github.com/giantswarm/aws-operator/service/awsconfig/v7/templates"
 )
 
