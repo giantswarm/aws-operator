3f4ad794598b377102907f93706f575e096105b5
Tim Schindler <xh3b4sd@gmail.com>
2018-03-28 14:03:46 +0200

    experiments with CF custom resource

--- service/awsconfig/v10/templates/cloudformation/guest/instance.go
+++ service/awsconfig/v10/templates/cloudformation/guest/instance.go
@@ -16,6 +16,51 @@ const Instance = `{{define "instance"}}
       Tags:
       - Key: Name
         Value: {{ .Instance.Cluster.ID }}-master
+    Metadata:
+      AWS::CloudFormation::Init:
+        config:
+          packages:
+            rpm:
+              aws-cfn-resource-bridge: https://s3.amazonaws.com/cloudformation-examples/aws-cfn-resource-bridge-0.1-4.noarch.rpm
+          files:
+            "/etc/cfn/bridge.d/mount.conf":
+              content:
+                Fn::Join:
+                - ''
+                - - "[mount]\n"
+                  - 'resource_type=Custom::VolumeMount'
+                  - queue_url=
+                  - Fn::GetAtt:
+                    - CustomResourcePipeline
+                    - Outputs.CustomResourceQueueURL
+                  - "\n"
+                  - 'timeout=600'
+                  - 'create_action=/home/ec2-user/create.sh'
+                  - 'update_action=/home/ec2-user/update.sh'
+                  - 'delete_action=/home/ec2-user/delete.sh'
+            "/home/ec2-user/create.sh":
+              source: https://raw.github.com/awslabs/aws-cfn-custom-resource-examples/master/examples/mount/impl/create.sh
+              mode: '000755'
+              owner: ec2-user
+            "/home/ec2-user/update.sh":
+              source: https://raw.github.com/awslabs/aws-cfn-custom-resource-examples/master/examples/mount/impl/update.sh
+              mode: '000755'
+              owner: ec2-user
+            "/home/ec2-user/delete.sh":
+              source: https://raw.github.com/awslabs/aws-cfn-custom-resource-examples/master/examples/mount/impl/delete.sh
+              mode: '000755'
+              owner: ec2-user
+          services:
+            sysvinit:
+              cfn-resource-bridge:
+                enabled: 'true'
+                ensureRunning: 'true'
+                files:
+                - "/etc/cfn/bridge.d/mount.conf"
+                - "/home/ec2-user/create.sh"
+                - "/home/ec2-user/update.sh"
+                - "/home/ec2-user/delete.sh"
+
   EtcdVolume:
     Type: AWS::EC2::Volume
     DependsOn:
@@ -34,4 +79,24 @@ const Instance = `{{define "instance"}}
       InstanceId: !Ref {{ .Instance.Master.Instance.ResourceName }}
       VolumeId: !Ref EtcdVolume
       Device: /dev/sdh
+
+  CustomResourcePipeline:
+    Type: AWS::CloudFormation::Stack
+    Properties:
+      TemplateURL: https://s3.amazonaws.com/cloudformation-examples/cr-backend-substack-template.template
+  EtcdVolumeMount:
+    Type: Custom::VolumeMount
+    Version: '1.0'
+    DependsOn:
+    - MountPoint
+    Properties:
+      ServiceToken:
+        Fn::GetAtt:
+        - CustomResourcePipeline
+        - Outputs.CustomResourceTopicARN
+      Device: "/dev/xvdh"
+      MountPoint: "/mnt/analysis"
+      FsType: ext3
+      Format: 'true'
+
 {{end}}`
