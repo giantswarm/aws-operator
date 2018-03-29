dc77db213a7038cbfcc2840644ea3732676392bd
Ross Fairbanks <ross@giantswarm.io>
2018-03-29 17:41:02 +0200

    Remove unused error matcher.

--- service/awsconfig/v10/resource/ebsvolume/error.go
+++ service/awsconfig/v10/resource/ebsvolume/error.go
@@ -1,7 +1,6 @@
 package ebsvolume
 
 import (
-	"github.com/aws/aws-sdk-go/aws/awserr"
 	"github.com/giantswarm/microerror"
 )
 
@@ -10,20 +9,6 @@ var invalidConfigError = microerror.New("invalid config")
 // IsInvalidConfig asserts invalidConfigError.
 func IsInvalidConfig(err error) bool {
 	return microerror.Cause(err) == invalidConfigError
-}
-
-// IsVolumeNotFound asserts volume not found error from upstream's API code.
-func IsVolumeNotFound(err error) bool {
-	aerr, ok := err.(awserr.Error)
-	if !ok {
-		return false
-	}
-	// TODO Find constant in the Go SDK for the error code.
-	if aerr.Code() == "NotFound" {
-		return true
-	}
-
-	return false
 }
 
 var wrongTypeError = microerror.New("wrong type")
