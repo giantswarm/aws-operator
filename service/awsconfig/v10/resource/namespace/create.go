512caeb3f00d03be03373583941d87b5685bde9f
Ross Fairbanks <ross@giantswarm.io>
2018-04-03 18:39:51 +0200

    Implement EnsureCreated.

--- service/awsconfig/v10/resource/namespace/create.go
+++ service/awsconfig/v10/resource/namespace/create.go
@@ -4,48 +4,33 @@ import (
 	"context"
 
 	"github.com/giantswarm/microerror"
-	apiv1 "k8s.io/api/core/v1"
 	apierrors "k8s.io/apimachinery/pkg/api/errors"
+
+	"github.com/giantswarm/aws-operator/service/awsconfig/v10/key"
 )
 
-func (r *Resource) ApplyCreateChange(ctx context.Context, obj, createChange interface{}) error {
-	namespaceToCreate, err := toNamespace(createChange)
+// EnsureCreated creates a Kubernetes namespace in the host cluster for the
+// guest cluster.
+func (r *Resource) EnsureCreated(ctx context.Context, obj interface{}) error {
+	customObject, err := key.ToCustomObject(obj)
 	if err != nil {
 		return microerror.Mask(err)
 	}
 
-	if namespaceToCreate != nil {
-		r.logger.LogCtx(ctx, "level", "debug", "message", "creating Kubernetes namespace")
+	r.logger.LogCtx(ctx, "level", "debug", "message", "creating Kubernetes namespace")
 
-		_, err = r.k8sClient.CoreV1().Namespaces().Create(namespaceToCreate)
-		if apierrors.IsAlreadyExists(err) {
-			// fall through
-		} else if err != nil {
-			return microerror.Mask(err)
-		}
+	namespaceToCreate := getNamespace(customObject)
 
-		r.logger.LogCtx(ctx, "level", "debug", "message", "creating Kubernetes namespace: created")
-	} else {
+	_, err = r.k8sClient.CoreV1().Namespaces().Create(namespaceToCreate)
+	if apierrors.IsAlreadyExists(err) {
 		r.logger.LogCtx(ctx, "level", "debug", "message", "creating Kubernetes namespace: already created")
+		return nil
+
+	} else if err != nil {
+		return microerror.Mask(err)
 	}
+
+	r.logger.LogCtx(ctx, "level", "debug", "message", "creating Kubernetes namespace: created")
 
 	return nil
-}
-
-func (r *Resource) newCreateChange(ctx context.Context, obj, currentState, desiredState interface{}) (interface{}, error) {
-	currentNamespace, err := toNamespace(currentState)
-	if err != nil {
-		return nil, microerror.Mask(err)
-	}
-	desiredNamespace, err := toNamespace(desiredState)
-	if err != nil {
-		return nil, microerror.Mask(err)
-	}
-
-	var namespaceToCreate *apiv1.Namespace
-	if currentNamespace == nil {
-		namespaceToCreate = desiredNamespace
-	}
-
-	return namespaceToCreate, nil
 }
