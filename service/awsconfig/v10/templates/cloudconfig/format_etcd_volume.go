23ef57223159776d4841fea51beacbaea5609fb6
Tim Schindler <xh3b4sd@gmail.com>
2018-04-04 17:15:12 +0200

    ensures etcd volume formatting to be done only once

--- service/awsconfig/v10/templates/cloudconfig/format_etcd_volume.go
+++ service/awsconfig/v10/templates/cloudconfig/format_etcd_volume.go
@@ -5,13 +5,13 @@ const FormatEtcdVolume = `
 Description=Formats EBS /dev/xvdh volume
 Requires=dev-xvdh.device
 After=dev-xvdh.device
-ConditionPathExists=!/var/lib/etcd-volume-formated
+ConditionPathExists=!/var/lib/etcd/etcd-volume-formated
 
 [Service]
 Type=oneshot
 RemainAfterExit=yes
 ExecStart=/usr/sbin/mkfs.ext4 /dev/xvdh
-ExecStartPost=/usr/bin/touch /var/lib/etcd-volume-formated
+ExecStartPost=/usr/bin/touch /var/lib/etcd/etcd-volume-formated
 
 [Install]
 WantedBy=multi-user.target
