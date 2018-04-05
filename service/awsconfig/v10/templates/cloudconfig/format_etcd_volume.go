d2c3dbdd03039ed49776d67cba803ac09f076f42
Tim Schindler <xh3b4sd@gmail.com>
2018-04-05 21:20:52 +0200

    fixes partition detection

--- service/awsconfig/v10/templates/cloudconfig/format_etcd_volume.go
+++ service/awsconfig/v10/templates/cloudconfig/format_etcd_volume.go
@@ -12,9 +12,8 @@ RemainAfterExit=yes
 
 # Do not wipe the disk if it's already being used, so the etcd data is
 # persistent across reboots and updates.
-Environment="LABEL=var-lib-etcd"
 Environment="DEV=/dev/xvdh"
-ExecStart=-/bin/bash -c "if ! findfs LABEL=$LABEL > /tmp/label.$LABEL; then wipefs -a -f $DEV && mkfs.ext4 -T news -F -L $LABEL $DEV && echo formatted file system; else echo file system already formatted; fi"
+ExecStart=-/bin/bash -c "if ! blkid $DEV; then wipefs -a -f $DEV && mkfs.ext4 $DEV && echo formatted file system; else echo file system already formatted; fi"
 
 [Install]
 WantedBy=multi-user.target
