0ef97355da12fbe03ff5c55bcdbcd3c9158b3595
Tim Schindler <xh3b4sd@gmail.com>
2018-04-06 12:10:16 +0200

    adds label to partition formatting

--- service/awsconfig/v10/templates/cloudconfig/format_etcd_volume.go
+++ service/awsconfig/v10/templates/cloudconfig/format_etcd_volume.go
@@ -13,7 +13,7 @@ RemainAfterExit=yes
 # Do not wipe the disk if it's already being used, so the etcd data is
 # persistent across reboots and updates.
 Environment="DEV=/dev/xvdh"
-ExecStart=-/bin/bash -c "if ! blkid $DEV; then wipefs -a -f $DEV && mkfs.ext4 $DEV && echo formatted file system; else echo file system already formatted; fi"
+ExecStart=-/bin/bash -c "if ! blkid $DEV; then wipefs -a -f $DEV && mkfs.ext4 -L etcd $DEV && echo formatted file system; else echo file system already formatted; fi"
 
 [Install]
 WantedBy=multi-user.target
