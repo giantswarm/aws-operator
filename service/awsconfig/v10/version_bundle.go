85e1d8d17e6b0b754d4f061e2ee460106f658472
Paweł Kopiczko <kopiczko@gmail.com>
2018-04-12 15:59:15 +0200

    awsconfig/v10: update k8scc to 3.2.5

--- service/awsconfig/v10/version_bundle.go
+++ service/awsconfig/v10/version_bundle.go
@@ -44,6 +44,11 @@ func VersionBundle() versionbundle.Bundle {
 				Description: "Fixed updating master nodes on all kinds of cluster updates.",
 				Kind:        versionbundle.KindFixed,
 			},
+			{
+				Component:   "kubernetes",
+				Description: "Updated to 1.10.0.",
+				Kind:        versionbundle.KindChanged,
+			},
 		},
 		Components: []versionbundle.Component{
 			{
@@ -68,7 +73,7 @@ func VersionBundle() versionbundle.Bundle {
 			},
 			{
 				Name:    "kubernetes",
-				Version: "1.9.5",
+				Version: "1.10.0",
 			},
 			{
 				Name:    "nginx-ingress-controller",
