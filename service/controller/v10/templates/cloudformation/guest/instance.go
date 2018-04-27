0666e510d0882eecf824dc096022fa4d3c4ee1dc
Roman Sokolkov <rsokolkov@gmail.com>
2018-04-27 10:29:16 +0200

    Add support for m5.large master (#909)
    * Add support for m5.large master
    
    * Address comments regarding m5 change

--- service/controller/v10/templates/cloudformation/guest/instance.go
+++ service/controller/v10/templates/cloudformation/guest/instance.go
@@ -16,6 +16,15 @@ const Instance = `{{define "instance"}}
       Tags:
       - Key: Name
         Value: {{ .Instance.Cluster.ID }}-master
+  DockerVolume:
+    Type: AWS::EC2::Volume
+    DependsOn:
+    - {{ .Instance.Master.Instance.ResourceName }}
+    Properties:
+      Encrypted: true
+      Size: 50
+      VolumeType: gp2
+      AvailabilityZone: !GetAtt {{ .Instance.Master.Instance.ResourceName }}.AvailabilityZone
   EtcdVolume:
     Type: AWS::EC2::Volume
     DependsOn:
@@ -28,10 +37,16 @@ const Instance = `{{define "instance"}}
       Tags:
       - Key: Name
         Value: {{ .Instance.Master.EtcdVolume.Name }}
-  {{ .Instance.Master.Instance.ResourceName }}MountPoint:
+  {{ .Instance.Master.Instance.ResourceName }}DockerMountPoint:
+    Type: AWS::EC2::VolumeAttachment
+    Properties:
+      InstanceId: !Ref {{ .Instance.Master.Instance.ResourceName }}
+      VolumeId: !Ref DockerVolume
+      Device: /dev/xvdc
+  {{ .Instance.Master.Instance.ResourceName }}EtcdMountPoint:
     Type: AWS::EC2::VolumeAttachment
     Properties:
       InstanceId: !Ref {{ .Instance.Master.Instance.ResourceName }}
       VolumeId: !Ref EtcdVolume
-      Device: /dev/sdh
+      Device: /dev/xvdh
 {{end}}`
