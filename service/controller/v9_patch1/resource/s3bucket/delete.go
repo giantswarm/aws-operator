edc9ad5d8c0bd5a689b93c9dd76e28167e99e00e
Roman Sokolkov <roman@giantswarm.io>
2018-04-30 11:52:40 +0200

    Just a copy paste of v9 to v9_patch1

---
+++ service/controller/v9_patch1/resource/s3bucket/delete.go
@@ -0,0 +1,87 @@
+package s3bucket
+
+import (
+	"context"
+
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/service/s3"
+	"github.com/giantswarm/microerror"
+	"github.com/giantswarm/operatorkit/controller"
+)
+
+func (r *Resource) ApplyDeleteChange(ctx context.Context, obj, deleteChange interface{}) error {
+	bucketInput, err := toBucketState(deleteChange)
+	if err != nil {
+		return microerror.Mask(err)
+	}
+
+	if bucketInput.Name != "" {
+		r.logger.LogCtx(ctx, "level", "debug", "message", "deleting S3 bucket")
+
+		// Make sure the bucket is empty.
+		input := &s3.ListObjectsV2Input{
+			Bucket: aws.String(bucketInput.Name),
+		}
+		result, err := r.clients.S3.ListObjectsV2(input)
+		if err != nil {
+			return microerror.Mask(err)
+		}
+
+		for _, object := range result.Contents {
+			_, err := r.clients.S3.DeleteObject(&s3.DeleteObjectInput{
+				Bucket: aws.String(bucketInput.Name),
+				Key:    object.Key,
+			})
+			if err != nil {
+				return microerror.Mask(err)
+			}
+		}
+
+		_, err = r.clients.S3.DeleteBucket(&s3.DeleteBucketInput{
+			Bucket: aws.String(bucketInput.Name),
+		})
+		if IsBucketNotFound(err) {
+			// Fall through.
+			return nil
+		}
+		if err != nil {
+			return microerror.Mask(err)
+		}
+
+		r.logger.LogCtx(ctx, "level", "debug", "message", "deleting S3 bucket: deleted")
+	} else {
+		r.logger.LogCtx(ctx, "level", "debug", "message", "deleting S3 bucket: already deleted")
+	}
+
+	return nil
+}
+
+func (r *Resource) NewDeletePatch(ctx context.Context, obj, currentState, desiredState interface{}) (*controller.Patch, error) {
+	delete, err := r.newDeleteChange(ctx, obj, currentState, desiredState)
+	if err != nil {
+		return nil, microerror.Mask(err)
+	}
+
+	patch := controller.NewPatch()
+	patch.SetDeleteChange(delete)
+
+	return patch, nil
+}
+
+func (r *Resource) newDeleteChange(ctx context.Context, obj, currentState, desiredState interface{}) (interface{}, error) {
+	currentBucket, err := toBucketState(currentState)
+	if err != nil {
+		return nil, microerror.Mask(err)
+	}
+	desiredBucket, err := toBucketState(desiredState)
+	if err != nil {
+		return nil, microerror.Mask(err)
+	}
+
+	var bucketToDelete BucketState
+	if currentBucket.Name != "" {
+		bucketToDelete = desiredBucket
+	}
+
+	return bucketToDelete, nil
+}
---
+++ service/controller/v9_patch1/version_bundle.go
@@ -0,0 +1,65 @@
+package v9_patch1
+
+import (
+	"time"
+
+	"github.com/giantswarm/versionbundle"
+)
+
+func VersionBundle() versionbundle.Bundle {
+	return versionbundle.Bundle{
+		Changelogs: []versionbundle.Changelog{
+			{
+				Component:   "AWS instance support",
+				Description: "Added support for AWS NVMe instance storage.",
+				Kind:        versionbundle.KindChanged,
+			},
+			{
+				Component:   "cloudconfig",
+				Description: "Kubernetes updated to version 1.9.5.",
+				Kind:        versionbundle.KindChanged,
+			},
+			{
+				Component:   "cloudconfig",
+				Description: "Nginx Ingress Controller updated to version 0.12.0",
+				Kind:        versionbundle.KindChanged,
+			},
+		},
+		Components: []versionbundle.Component{
+			{
+				Name:    "calico",
+				Version: "3.0.2",
+			},
+			{
+				Name:    "containerlinux",
+				Version: "1632.3.0",
+			},
+			{
+				Name:    "docker",
+				Version: "17.09.0",
+			},
+			{
+				Name:    "etcd",
+				Version: "3.3.1",
+			},
+			{
+				Name:    "coredns",
+				Version: "1.0.6",
+			},
+			{
+				Name:    "kubernetes",
+				Version: "1.9.5",
+			},
+			{
+				Name:    "nginx-ingress-controller",
+				Version: "0.12.0",
+			},
+		},
+		Dependencies: []versionbundle.Dependency{},
+		Deprecated:   true,
+		Name:         "aws-operator",
+		Time:         time.Date(2018, time.March, 22, 12, 29, 0, 0, time.UTC),
+		Version:      "3.0.2",
+		WIP:          false,
+	}
+}
