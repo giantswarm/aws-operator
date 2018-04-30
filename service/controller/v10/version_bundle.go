f7b630718b75f1f7d8ba13ff510b6a657a4dd885
Volodymyr Stoiko <me@volodymyrstoiko.com>
2018-04-30 12:27:44 +0300

    Fix date

--- service/controller/v10/version_bundle.go
+++ service/controller/v10/version_bundle.go
@@ -143,7 +143,7 @@ func VersionBundle() versionbundle.Bundle {
 		Dependencies: []versionbundle.Dependency{},
 		Deprecated:   false,
 		Name:         "aws-operator",
-		Time:         time.Date(2018, time.March, 30, 11, 50, 0, 0, time.UTC),
+		Time:         time.Date(2018, time.April, 30, 11, 50, 0, 0, time.UTC),
 		Version:      "3.1.0",
 		WIP:          false,
 	}
