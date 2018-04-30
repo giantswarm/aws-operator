f355933fc4ba6d8e0859acd68b4f1ce4f14af1c9
Volodymyr Stoiko <me@volodymyrstoiko.com>
2018-04-30 16:31:43 +0200

    Release aws-operator 3.1.0 (#906)
    * Release aws-operator 3.1.0

--- service/controller/v10/version_bundle.go
+++ service/controller/v10/version_bundle.go
@@ -143,8 +143,8 @@ func VersionBundle() versionbundle.Bundle {
 		Dependencies: []versionbundle.Dependency{},
 		Deprecated:   false,
 		Name:         "aws-operator",
-		Time:         time.Date(2018, time.March, 22, 15, 47, 0, 0, time.UTC),
+		Time:         time.Date(2018, time.April, 30, 11, 50, 0, 0, time.UTC),
 		Version:      "3.1.0",
-		WIP:          true,
+		WIP:          false,
 	}
 }
