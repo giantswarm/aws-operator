530a8d30a61239a9485961d6651a22253ada2e22
Tim Schindler <xh3b4sd@gmail.com>
2018-05-03 12:24:42 +0200

    handles missing lifecycle actions more gracefully (#926)

--- service/controller/v11/resource/lifecycle/error.go
+++ service/controller/v11/resource/lifecycle/error.go
@@ -1,6 +1,10 @@
 package lifecycle
 
-import "github.com/giantswarm/microerror"
+import (
+	"strings"
+
+	"github.com/giantswarm/microerror"
+)
 
 var executionFailedError = microerror.New("execution failed")
 
@@ -20,4 +24,27 @@ var missingAnnotationError = microerror.New("missing annotation")
 
 func IsMissingAnnotationError(err error) bool {
 	return microerror.Cause(err) == missingAnnotationError
+}
+
+var noActiveLifecycleActionError = microerror.New("no active lifecycle action")
+
+// IsNoActiveLifecycleAction asserts noActiveLifecycleActionError. It also
+// checks for some string matching in the error message to figure if the AWS API
+// gives the error we expect.
+func IsNoActiveLifecycleAction(err error) bool {
+	c := microerror.Cause(err)
+
+	if c == nil {
+		return false
+	}
+
+	if strings.Contains(c.Error(), "No active Lifecycle Action found") {
+		return true
+	}
+
+	if c == noActiveLifecycleActionError {
+		return true
+	}
+
+	return false
 }
