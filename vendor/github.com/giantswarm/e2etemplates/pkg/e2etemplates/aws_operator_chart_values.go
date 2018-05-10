4034cb0746f173f00daab6ece206c573908adabe
Fernando Ripoll <pipo02mix@gmail.com>
2018-05-10 13:07:23 +0200

    Add test value in e2e templates for testing purposes

--- vendor/github.com/giantswarm/e2etemplates/pkg/e2etemplates/aws_operator_chart_values.go
+++ vendor/github.com/giantswarm/e2etemplates/pkg/e2etemplates/aws_operator_chart_values.go
@@ -20,6 +20,7 @@ const AWSOperatorChartValues = `Installation:
     Provider:
       AWS:
         Region: ${AWS_REGION}
+        AdvancedMonitoringEC2: true
     Secret:
       AWSOperator:
         IDRSAPub: ${IDRSA_PUB}
