9449a1fe27cb8fe84b8a66afb8e700fdc1d4faac
Tim Schindler <xh3b4sd@gmail.com>
2018-05-16 12:08:09 +0200

    fixes filter conditions

--- service/collector/vpc.go
+++ service/collector/vpc.go
@@ -46,67 +46,80 @@ var (
 func (c *Collector) collectVPCs(ch chan<- prometheus.Metric) {
 	c.logger.Log("level", "debug", "message", "collecting metrics for vpcs")
 
-	i := &ec2.DescribeVpcsInput{
-		Filters: []*ec2.Filter{
+	f := func(filters []*ec2.Filter) {
+		i := &ec2.DescribeVpcsInput{
+			Filters: filters,
+		}
+		o, err := c.awsClients.EC2.DescribeVpcs(i)
+		if err != nil {
+			c.logger.Log("level", "error", "message", "could not list vpcs", "stack", fmt.Sprintf("%#v", err))
+		}
+
+		for _, vpc := range o.Vpcs {
+			cluster := ""
+			installation := ""
+			name := ""
+			organization := ""
+			stackName := ""
+
+			for _, tag := range vpc.Tags {
+				if *tag.Key == ClusterTag {
+					cluster = *tag.Value
+				}
+				if *tag.Key == InstallationTag {
+					installation = *tag.Value
+				}
+				if *tag.Key == NameTag {
+					name = *tag.Value
+				}
+				if *tag.Key == OrganizationTag {
+					organization = *tag.Value
+				}
+				if *tag.Key == StackNameTag {
+					stackName = *tag.Value
+				}
+			}
+
+			ch <- prometheus.MustNewConstMetric(
+				vpcs,
+				prometheus.GaugeValue,
+				gaugeValue,
+				*vpc.CidrBlock,
+				cluster,
+				*vpc.VpcId,
+				installation,
+				name,
+				organization,
+				stackName,
+				*vpc.State,
+			)
+		}
+	}
+
+	{
+		filters := []*ec2.Filter{
 			{
 				Name: aws.String(fmt.Sprintf("tag:%s", tagKeyInstallation)),
 				Values: []*string{
 					aws.String(c.installationName),
 				},
 			},
-			// TODO this is the deprecated tag we are only still using for old
-			// clusters. This filter condition should be removed at some point.
+		}
+		f(filters)
+	}
+
+	// TODO this is the deprecated tag we are only still using for old
+	// clusters. This filter condition should be removed at some point.
+	{
+		filters := []*ec2.Filter{
 			{
 				Name: aws.String("tag:Installation"),
 				Values: []*string{
 					aws.String(c.installationName),
 				},
 			},
-		},
-	}
-	o, err := c.awsClients.EC2.DescribeVpcs(i)
-	if err != nil {
-		c.logger.Log("level", "error", "message", "could not list vpcs", "stack", fmt.Sprintf("%#v", err))
-	}
-
-	for _, vpc := range o.Vpcs {
-		cluster := ""
-		installation := ""
-		name := ""
-		organization := ""
-		stackName := ""
-
-		for _, tag := range vpc.Tags {
-			if *tag.Key == ClusterTag {
-				cluster = *tag.Value
-			}
-			if *tag.Key == InstallationTag {
-				installation = *tag.Value
-			}
-			if *tag.Key == NameTag {
-				name = *tag.Value
-			}
-			if *tag.Key == OrganizationTag {
-				organization = *tag.Value
-			}
-			if *tag.Key == StackNameTag {
-				stackName = *tag.Value
-			}
 		}
-
-		ch <- prometheus.MustNewConstMetric(
-			vpcs,
-			prometheus.GaugeValue,
-			gaugeValue,
-			*vpc.CidrBlock,
-			cluster,
-			*vpc.VpcId,
-			installation,
-			name,
-			organization,
-			stackName,
-			*vpc.State,
-		)
+		f(filters)
 	}
 
 	c.logger.Log("level", "debug", "message", "finished collecting metrics for vpcs")
