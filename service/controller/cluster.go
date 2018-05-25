424cc2bddfaf7f88c662b74e4faebc83c9cbfa4d
Theo Brigitte <theo.brigitte@gmail.com>
2018-05-25 15:50:15 +0200

    BYOC: build master

--- service/controller/cluster.go
+++ service/controller/cluster.go
@@ -114,7 +114,7 @@ func NewCluster(config ClusterConfig) (*Cluster, error) {
 	if config.GuestAWSConfig.Region == "" {
 		return nil, microerror.Maskf(invalidConfigError, "config.GuestAWSConfig.Region must not be empty")
 	}
-	// TODO: remove this when all version prior to v11 are removed.
+	// TODO: remove this when all version prior to v11 are removed
 	if config.HostAWSConfig.AccessKeyID == "" && config.HostAWSConfig.AccessKeySecret == "" {
 		config.Logger.Log("debug", "no host cluster account credentials supplied, assuming guest and host uses same account")
 		config.HostAWSConfig = config.GuestAWSConfig
