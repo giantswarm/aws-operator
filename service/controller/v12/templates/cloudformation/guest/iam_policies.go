d2c582d8866d992748ad0660e2975111720fed50
Ross Fairbanks <ross@giantswarm.io>
2018-06-04 14:18:07 +0200

    Remove ECR from IAM role as it isn't available in China

--- service/controller/v12/templates/cloudformation/guest/iam_policies.go
+++ service/controller/v12/templates/cloudformation/guest/iam_policies.go
@@ -94,17 +94,6 @@ const IAMPolicies = `{{define "iam_policies"}}
           - Effect: "Allow"
             Action: "s3:GetObject"
             Resource: "arn:{{.RegionARN}}:s3:::{{.S3Bucket}}/*"
-
-          - Effect: "Allow"
-            Action:
-              - "ecr:GetAuthorizationToken"
-              - "ecr:BatchCheckLayerAvailability"
-              - "ecr:GetDownloadUrlForLayer"
-              - "ecr:GetRepositoryPolicy"
-              - "ecr:DescribeRepositories"
-              - "ecr:ListImages"
-              - "ecr:BatchGetImage"
-            Resource: "*"
   WorkerInstanceProfile:
     Type: "AWS::IAM::InstanceProfile"
     Properties:
