a789f086552159dc2dc4bb29dce256c34de6ea62
Fernando Ripoll <pipo02mix@gmail.com>
2018-06-06 23:13:56 +0200

    Add cluster info with status property (#980)
    * Add cluster info with status property
    
    * Deletion timestamp is not annotation is property under metadata
    
    * Add paggintation
    
    * Short g8s client alphabetically
    
    * Align var definition

---
+++ service/collector/cluster_info.go
@@ -0,0 +1,66 @@
+package collector
+
+import (
+	"github.com/prometheus/client_golang/prometheus"
+	v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+const (
+	clusterLabel           = "cluster_id"
+	statusLabel            = "status"
+	listClusterConfigLimit = 20
+)
+
+var clustersDesc *prometheus.Desc = prometheus.NewDesc(
+	prometheus.BuildFQName(Namespace, "", "cluster_info"),
+	"Cluster information.",
+	[]string{
+		clusterLabel,
+		statusLabel,
+	},
+	nil,
+)
+
+func (c *Collector) collectClusterInfo(ch chan<- prometheus.Metric) {
+	c.logger.Log("level", "debug", "message", "collecting metrics for clusters")
+
+	opts := v1.ListOptions{
+		Limit: listClusterConfigLimit,
+	}
+
+	continueToken := ""
+
+	for {
+		opts.Continue = continueToken
+
+		clustersConfigList, err := c.g8sClient.ProviderV1alpha1().AWSConfigs("").List(opts)
+		if err != nil {
+			c.logger.Log(err)
+		}
+
+		for _, clusterConfig := range clustersConfigList.Items {
+
+			clusterID := clusterConfig.Name
+			status := "Running"
+
+			if clusterConfig.DeletionTimestamp != nil {
+				status = "Terminating"
+			}
+
+			ch <- prometheus.MustNewConstMetric(
+				clustersDesc,
+				prometheus.GaugeValue,
+				GaugeValue,
+				clusterID,
+				status,
+			)
+		}
+
+		continueToken = clustersConfigList.Continue
+		if continueToken == "" {
+			break
+		}
+	}
+
+	c.logger.Log("level", "debug", "message", "finished collecting metrics for clusters")
+}
--- service/collector/collector.go
+++ service/collector/collector.go
@@ -6,23 +6,30 @@ import (
 
 	"github.com/giantswarm/microerror"
 	"github.com/giantswarm/micrologger"
+
+	"github.com/giantswarm/apiextensions/pkg/clientset/versioned"
 )
 
 type Config struct {
-	Logger micrologger.Logger
+	G8sClient versioned.Interface
+	Logger    micrologger.Logger
 
 	AwsConfig        awsutil.Config
 	InstallationName string
 }
 
 type Collector struct {
-	logger micrologger.Logger
+	g8sClient versioned.Interface
+	logger    micrologger.Logger
 
 	awsClients       awsutil.Clients
 	installationName string
 }
 
 func New(config Config) (*Collector, error) {
+	if config.G8sClient == nil {
+		return nil, microerror.Maskf(invalidConfigError, "%T.G8sClient must not be empty", config)
+	}
 	if config.Logger == nil {
 		return nil, microerror.Maskf(invalidConfigError, "%T.Logger must not be empty", config)
 	}
@@ -38,7 +45,8 @@ func New(config Config) (*Collector, error) {
 	awsClients := awsutil.NewClients(config.AwsConfig)
 
 	c := &Collector{
-		logger: config.Logger,
+		g8sClient: config.G8sClient,
+		logger:    config.Logger,
 
 		awsClients:       awsClients,
 		installationName: config.InstallationName,
@@ -49,12 +57,15 @@ func New(config Config) (*Collector, error) {
 
 func (c *Collector) Describe(ch chan<- *prometheus.Desc) {
 	ch <- vpcsDesc
+	ch <- clustersDesc
 }
 
 func (c *Collector) Collect(ch chan<- prometheus.Metric) {
 	c.logger.Log("level", "debug", "message", "collecting metrics")
 
 	c.collectVPCs(ch)
+
+	c.collectClusterInfo(ch)
 
 	c.logger.Log("level", "debug", "message", "finished collecting metrics")
 }
