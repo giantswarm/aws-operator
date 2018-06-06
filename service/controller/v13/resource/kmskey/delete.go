776ef5e7506298020413ac1065f9cdc593faa674
Ross Fairbanks <ross@giantswarm.io>
2018-06-06 15:12:50 +0200

    opsctl: copy v12 to v13

---
+++ service/controller/v13/resource/kmskey/delete.go
@@ -0,0 +1,88 @@
+package kmskey
+
+import (
+	"context"
+
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/service/kms"
+	"github.com/giantswarm/microerror"
+	"github.com/giantswarm/operatorkit/controller"
+
+	"github.com/giantswarm/aws-operator/service/controller/v12/controllercontext"
+)
+
+func (r *Resource) ApplyDeleteChange(ctx context.Context, obj, deleteChange interface{}) error {
+	deleteInput, err := toKMSKeyState(deleteChange)
+	if err != nil {
+		return microerror.Mask(err)
+	}
+
+	if deleteInput.KeyAlias != "" {
+		sc, err := controllercontext.FromContext(ctx)
+		if err != nil {
+			return microerror.Mask(err)
+		}
+
+		// Get the KMS key ID using the key alias.
+		key, err := sc.AWSClient.KMS.DescribeKey(&kms.DescribeKeyInput{
+			KeyId: aws.String(deleteInput.KeyAlias),
+		})
+		if err != nil {
+			return microerror.Mask(err)
+		}
+
+		// Delete the key alias.
+		if _, err := sc.AWSClient.KMS.DeleteAlias(&kms.DeleteAliasInput{
+			AliasName: aws.String(deleteInput.KeyAlias),
+		}); err != nil {
+			return microerror.Mask(err)
+		}
+
+		// AWS API doesn't allow to delete the KMS key immediately, but we can schedule its deletion.
+		if _, err := sc.AWSClient.KMS.ScheduleKeyDeletion(&kms.ScheduleKeyDeletionInput{
+			KeyId:               key.KeyMetadata.KeyId,
+			PendingWindowInDays: aws.Int64(pendingDeletionWindow),
+		}); err != nil {
+			return microerror.Mask(err)
+		}
+
+		r.logger.LogCtx(ctx, "level", "debug", "message", "deleting KMS Key: deleted")
+	} else {
+		r.logger.LogCtx(ctx, "level", "debug", "message", "deleting KMS Key: already deleted")
+	}
+
+	return nil
+}
+
+func (r *Resource) NewDeletePatch(ctx context.Context, obj, currentState, desiredState interface{}) (*controller.Patch, error) {
+	delete, err := r.newDeleteChange(ctx, obj, currentState, desiredState)
+	if err != nil {
+		return nil, microerror.Mask(err)
+	}
+
+	patch := controller.NewPatch()
+	patch.SetDeleteChange(delete)
+
+	return patch, nil
+}
+
+func (r *Resource) newDeleteChange(ctx context.Context, obj, currentState, desiredState interface{}) (interface{}, error) {
+	currentKeyState, err := toKMSKeyState(currentState)
+	if err != nil {
+		return nil, microerror.Mask(err)
+	}
+
+	desiredKeyState, err := toKMSKeyState(desiredState)
+	if err != nil {
+		return nil, microerror.Mask(err)
+	}
+
+	r.logger.LogCtx(ctx, "level", "debug", "message", "finding out if the KMS key should be deleted")
+
+	var kmsKeyToDelete KMSKeyState
+	if currentKeyState.KeyAlias != "" {
+		kmsKeyToDelete = desiredKeyState
+	}
+
+	return kmsKeyToDelete, nil
+}
