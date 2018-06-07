90378cd1f3796dbc439adc12fb996c6da1f078ba
Paweł Kopiczko <kopiczko@gmail.com>
2018-06-07 19:22:28 +0200

    Revert "revert: don't include the template"
    This reverts commit f2a3270e78811e1aef4981f276c45843f9c96a46.

--- service/controller/v13/templates/cloudformation/hostpost/main.go
+++ service/controller/v13/templates/cloudformation/hostpost/main.go
@@ -3,14 +3,6 @@ package hostpost
 const Main = `{{define "main"}}AWSTemplateFormatVersion: 2010-09-09
 Description: Main Host Post-Guest CloudFormation stack.
 Resources:
-  GuestNSRecordSet:
-    Type: 'AWS::Route53::RecordSet'
-    Properties:
-      HostedZoneName: '{{ .BaseDomain }}.'
-      Name: '{{ .ClusterID }}.k8s.{{ .BaseDomain }}.'
-      Type: 'NS'
-      TTL: '900'
-      ResourceRecords: !Split [ ',', '{{ .GuestHostedZoneNameServers }}' ]
   {{template "record_sets" .}}
   {{template "route_tables" .}}
 {{end}}`
