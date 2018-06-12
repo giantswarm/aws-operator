bdcd1de77da58265e4ec22b01a22c2210e2f0f7c
Ross Fairbanks <ross@giantswarm.io>
2018-06-12 10:58:58 +0200

    Add keypair for testing

--- service/controller/v13/templates/cloudformation/guest/instance.go
+++ service/controller/v13/templates/cloudformation/guest/instance.go
@@ -8,6 +8,7 @@ const Instance = `{{define "instance"}}
     - DockerVolume
     - EtcdVolume
     Properties:
+      KeyName: 'rossf7'
       AvailabilityZone: {{ .Instance.Master.AZ }}
       IamInstanceProfile: !Ref MasterInstanceProfile
       ImageId: {{ .Instance.Image.ID }}
