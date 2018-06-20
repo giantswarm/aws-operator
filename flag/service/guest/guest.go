ff7cb5b8bbc7015c7f21734cedccee59a73da06b
Volodymyr Stoiko <me@volodymyrstoiko.com>
2018-06-20 11:51:27 +0300

    Add flags and helm

--- flag/service/guest/guest.go
+++ flag/service/guest/guest.go
@@ -1,7 +1,9 @@
 package guest
 
 import "github.com/giantswarm/aws-operator/flag/service/guest/update"
+import "github.com/giantswarm/aws-operator/flag/service/guest/ssh"
 
 type Guest struct {
 	Update update.Update
+	SSH    ssh.SSH
 }
