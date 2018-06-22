b69bf819b2eb94054e6576029b6d06784ab299f8
Federico Gimenez <federico.gimenez@gmail.com>
2018-06-22 16:54:57 +0200

    use China's Elastic Container Registry

--- service/controller/v13/templates/cloudconfig/small.go
+++ service/controller/v13/templates/cloudconfig/small.go
@@ -39,6 +39,6 @@ done
     --volume=dns,kind=host,source=/etc/resolv.conf,readOnly=true --mount volume=dns,target=/etc/resolv.conf  \
     --volume=awsenv,kind=host,source=/var/run/coreos,readOnly=false --mount volume=awsenv,target=/var/run/coreos \
     --trust-keys-from-https \
-    quay.io/coreos/awscli:025a357f05242fdad6a81e8a6b520098aa65a600 -- aws s3 --region {{.Region}} cp s3://{{.S3URI}}/cloudconfig/{{.CloudConfigVersion}}/$USERDATA_FILE /var/run/coreos/temp.txt
+    306934455918.dkr.ecr.cn-north-1.amazonaws.com.cn/awscli:latest -- aws s3 --region {{.Region}} cp s3://{{.S3URI}}/cloudconfig/{{.CloudConfigVersion}}/$USERDATA_FILE /var/run/coreos/temp.txt
 base64 -d /var/run/coreos/temp.txt | gunzip > /var/run/coreos/$USERDATA_FILE
 exec /usr/bin/coreos-cloudinit --from-file /var/run/coreos/$USERDATA_FILE`
