ad7493c0d1e1fd62aec9b88cf35a682ee764576e
Ross Fairbanks <rossf7@users.noreply.github.com>
2018-07-06 12:40:37 +0200

    Change Reboot test to Cluster State test from e2etests (#1022)
    * dep: e2etests use master-node branch
    
    * Remove reboot test
    
    * Add master node test
    
    * Update Circle CI to use new test
    
    * Revert "Update Circle CI to use new test"
    
    This reverts commit d6acc39a58b77e92d205a6213048145d13f7b59b.
    
    * Rename test from masternode to clusterstate
    
    * fmt
    
    * Fix package name
    
    * Fix main test
    
    * Fix AWS client in test
    
    * Update test and rename to cluster state
    
    * Make e2e test order alphabetical
    
    * dep: update e2etests
    
    * Use updated e2eclient
    
    * vendor: updated e2etests and e2eclients
    
    * Fix e2eclient
    
    * Error handling for e2eclient
    
    * Compiling
    
    * vendor: updated e2eclients
    
    * vendor: updated e2etests
    
    * vendor: updated e2etests
    
    * vendor: updated e2etests
    
    * vendor: updated e2etests
    
    * Fix test name
    
    * dep: remove pinned e2etests branch

---
+++ vendor/github.com/giantswarm/e2etests/clusterstate/provider/error.go
@@ -0,0 +1,24 @@
+package provider
+
+import "github.com/giantswarm/microerror"
+
+var notFoundError = microerror.New("not found")
+
+// IsNotFound asserts NotFoundError.
+func IsNotFound(err error) bool {
+	return microerror.Cause(err) == notFoundError
+}
+
+var invalidConfigError = microerror.New("invalid config")
+
+// IsInvalidConfig asserts invalidConfigError.
+func IsInvalidConfig(err error) bool {
+	return microerror.Cause(err) == invalidConfigError
+}
+
+var tooManyResultsError = microerror.New("too many results")
+
+// IsTooManyResults asserts tooManyResultsError.
+func IsTooManyResults(err error) bool {
+	return microerror.Cause(err) == tooManyResultsError
+}
