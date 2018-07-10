dca513715ed1710e205560467506e604bfec8fcf
Joseph Salisbury <salisbury.joseph@gmail.com>
2018-07-10 12:10:53 +0100

    Adds support for exposing trusted advisor checks via metrics

--- service/collector/error.go
+++ service/collector/error.go
@@ -10,3 +10,17 @@ var invalidConfigError = microerror.New("invalid config")
 func IsInvalidConfig(err error) bool {
 	return microerror.Cause(err) == invalidConfigError
 }
+
+var nilLimitError = microerror.New("nil Limit")
+
+// IsNilLimit asserts nilLimitError.
+func IsNilLimit(err error) bool {
+	return microerror.Cause(err) == nilLimitError
+}
+
+var nilUsageError = microerror.New("nil usage")
+
+// IsNilUsage asserts nilUsageError.
+func IsNilUsage(err error) bool {
+	return microerror.Cause(err) == nilUsageError
+}
