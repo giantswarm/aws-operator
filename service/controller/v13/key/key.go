0a23180e91ef0a42fdff91d7c76d7546c041d70d
Vaclav Rozsypalek <rozsypalek.vaclav@gmail.com>
2018-07-10 14:51:49 +0200

    order

--- service/controller/v13/key/key.go
+++ service/controller/v13/key/key.go
@@ -156,6 +156,10 @@ func ClusterCustomer(customObject v1alpha1.AWSConfig) string {
 	return customObject.Spec.Cluster.Customer.ID
 }
 
+func ClusterEtcdDomain(customObject v1alpha1.AWSConfig) string {
+	return customObject.Spec.Cluster.Etcd.Domain
+}
+
 func ClusterID(customObject v1alpha1.AWSConfig) string {
 	return customObject.Spec.Cluster.ID
 }
@@ -184,10 +188,6 @@ func ClusterTags(customObject v1alpha1.AWSConfig, installationName string) map[s
 
 func ClusterVersion(customObject v1alpha1.AWSConfig) string {
 	return customObject.Spec.Cluster.Version
-}
-
-func ClusterEtcdDomain(customObject v1alpha1.AWSConfig) string {
-	return customObject.Spec.Cluster.Etcd.Domain
 }
 
 func CustomerID(customObject v1alpha1.AWSConfig) string {
