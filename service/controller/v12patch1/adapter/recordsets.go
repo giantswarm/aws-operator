76a0efa0755d63f38f737c3ddc90ac81f147af53
Roman Sokolkov <roman@giantswarm.io>
2018-07-10 09:58:42 +0200

    controller/v12patch1: copy v12

---
+++ service/controller/v12patch1/adapter/recordsets.go
@@ -0,0 +1,32 @@
+package adapter
+
+// The template related to this adapter can be found in the following import.
+//
+//     github.com/giantswarm/aws-operator/service/controller/v12/templates/cloudformation/guest/recordsets.go
+//
+
+type recordSetsAdapter struct {
+	APIELBHostedZones         string
+	APIELBDomain              string
+	EtcdELBHostedZones        string
+	EtcdELBDomain             string
+	IngressELBDNS             string
+	IngressELBHostedZones     string
+	IngressELBAliasHostedZone string
+	IngressELBDomain          string
+	IngressWildcardELBDomain  string
+	Route53Enabled            bool
+}
+
+func (r *recordSetsAdapter) getRecordSets(cfg Config) error {
+	r.APIELBHostedZones = cfg.CustomObject.Spec.AWS.API.HostedZones
+	r.APIELBDomain = cfg.CustomObject.Spec.Cluster.Kubernetes.API.Domain
+	r.EtcdELBHostedZones = cfg.CustomObject.Spec.AWS.Etcd.HostedZones
+	r.EtcdELBDomain = cfg.CustomObject.Spec.Cluster.Etcd.Domain
+	r.IngressELBHostedZones = cfg.CustomObject.Spec.AWS.Ingress.HostedZones
+	r.IngressELBDomain = cfg.CustomObject.Spec.Cluster.Kubernetes.IngressController.Domain
+	r.IngressWildcardELBDomain = cfg.CustomObject.Spec.Cluster.Kubernetes.IngressController.WildcardDomain
+	r.Route53Enabled = cfg.Route53Enabled
+
+	return nil
+}
---
+++ service/controller/v12patch1/resource/kmskey/create_test.go
@@ -0,0 +1,147 @@
+package kmskey
+
+import (
+	"context"
+	"testing"
+
+	"github.com/giantswarm/apiextensions/pkg/apis/provider/v1alpha1"
+	"github.com/giantswarm/micrologger/microloggertest"
+
+	"github.com/giantswarm/aws-operator/client/aws"
+	"github.com/giantswarm/aws-operator/service/controller/v12/controllercontext"
+)
+
+func Test_Resource_KMSKey_newCreate(t *testing.T) {
+	t.Parallel()
+	customObject := &v1alpha1.AWSConfig{
+		Spec: v1alpha1.AWSConfigSpec{
+			Cluster: v1alpha1.Cluster{
+				ID: "test-cluster",
+			},
+		},
+	}
+
+	testCases := []struct {
+		currentState   KMSKeyState
+		desiredState   KMSKeyState
+		expectedChange KMSKeyState
+		description    string
+	}{
+		{
+			description:    "current state empty, desired state empty, empty create change",
+			currentState:   KMSKeyState{},
+			desiredState:   KMSKeyState{},
+			expectedChange: KMSKeyState{},
+		},
+		{
+			description:  "current state empty, desired state not empty, create change == desired state",
+			currentState: KMSKeyState{},
+			desiredState: KMSKeyState{
+				KeyAlias: "mykeyid",
+			},
+			expectedChange: KMSKeyState{
+				KeyAlias: "mykeyid",
+			},
+		},
+		{
+			description: "current state not empty, desired state not empty, create change == desired state",
+			currentState: KMSKeyState{
+				KeyAlias: "currentkeyid",
+			},
+			desiredState: KMSKeyState{
+				KeyAlias: "mykeyid",
+			},
+			expectedChange: KMSKeyState{
+				KeyAlias: "mykeyid",
+			},
+		},
+	}
+
+	var err error
+	var newResource *Resource
+
+	resourceConfig := DefaultConfig()
+	awsClients := aws.Clients{
+		KMS: &KMSClientMock{},
+	}
+	resourceConfig.Logger = microloggertest.New()
+	resourceConfig.InstallationName = "test-install"
+
+	newResource, err = New(resourceConfig)
+	if err != nil {
+		t.Fatal("expected", nil, "got", err)
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.description, func(t *testing.T) {
+			ctx := context.TODO()
+			ctx = controllercontext.NewContext(ctx, controllercontext.Context{AWSClient: awsClients})
+
+			result, err := newResource.newCreateChange(ctx, customObject, tc.currentState, tc.desiredState)
+			if err != nil {
+				t.Errorf("expected '%v' got '%#v'", nil, err)
+			}
+			createChange, ok := result.(KMSKeyState)
+			if !ok {
+				t.Errorf("expected '%T', got '%T'", createChange, result)
+			}
+			if createChange.KeyAlias != tc.expectedChange.KeyAlias {
+				t.Errorf("expected %s, got %s", tc.expectedChange.KeyAlias, createChange.KeyAlias)
+			}
+		})
+	}
+}
+
+func Test_ApplyCreateChange(t *testing.T) {
+	t.Parallel()
+	customObject := &v1alpha1.AWSConfig{
+		Spec: v1alpha1.AWSConfigSpec{
+			Cluster: v1alpha1.Cluster{
+				ID: "test-cluster",
+			},
+		},
+	}
+
+	testCases := []struct {
+		createChange KMSKeyState
+		description  string
+	}{
+		{
+			description: "basic case, create",
+			createChange: KMSKeyState{
+				KeyAlias: "alias/test-cluster",
+			},
+		},
+		{
+			description:  "empty create change, not create",
+			createChange: KMSKeyState{},
+		},
+	}
+
+	var err error
+	var newResource *Resource
+
+	resourceConfig := DefaultConfig()
+	awsClients := aws.Clients{
+		KMS: &KMSClientMock{
+			clusterID: "test-cluster",
+		},
+	}
+	resourceConfig.Logger = microloggertest.New()
+	resourceConfig.InstallationName = "test-install"
+
+	newResource, err = New(resourceConfig)
+	if err != nil {
+		t.Fatal("expected", nil, "got", err)
+	}
+
+	for _, tc := range testCases {
+		ctx := context.TODO()
+		ctx = controllercontext.NewContext(ctx, controllercontext.Context{AWSClient: awsClients})
+
+		err := newResource.ApplyCreateChange(ctx, customObject, tc.createChange)
+		if err != nil {
+			t.Errorf("unexpected error %v", err)
+		}
+	}
+}
