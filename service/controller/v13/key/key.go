2e818970ec74a5e502a46acf9fe34dd82184b508
Vaclav Rozsypalek <rozsypalek.vaclav@gmail.com>
2018-07-11 11:05:14 +0200

    typo

--- service/controller/v13/key/key.go
+++ service/controller/v13/key/key.go
@@ -168,7 +168,7 @@ func ClusterCustomer(customObject v1alpha1.AWSConfig) string {
 }
 
 func ClusterEtcdDomain(customObject v1alpha1.AWSConfig) string {
-	return fmt.Sprintf("%s:%d,", customObject.Spec.Cluster.Etcd.Domain, customObject.Spec.Cluster.Etcd.Port)
+	return fmt.Sprintf("%s:%d", customObject.Spec.Cluster.Etcd.Domain, customObject.Spec.Cluster.Etcd.Port)
 }
 
 func ClusterID(customObject v1alpha1.AWSConfig) string {
