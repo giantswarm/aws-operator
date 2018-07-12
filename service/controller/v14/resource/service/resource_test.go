560106696ad1fb93518d7b7b1068be6a5a9660d2
Vaclav Rozsypalek <rozsypalek.vaclav@gmail.com>
2018-07-12 12:05:42 +0200

    fix imports

--- service/controller/v14/resource/service/resource_test.go
+++ service/controller/v14/resource/service/resource_test.go
@@ -8,7 +8,7 @@ import (
 	apismetav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/util/intstr"
 
-	"github.com/giantswarm/azure-operator/service/controller/v3/key"
+	"github.com/giantswarm/aws-operator/service/controller/v13/key"
 )
 
 func Test_toService(t *testing.T) {
