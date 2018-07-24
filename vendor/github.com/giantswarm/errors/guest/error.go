0e1f1f6a94c8f0f5d0d0f912562c168973abafc1
Tim Schindler <xh3b4sd@gmail.com>
2018-07-24 11:52:17 +0200

    updates vendor deps

--- vendor/github.com/giantswarm/errors/guest/error.go
+++ vendor/github.com/giantswarm/errors/guest/error.go
@@ -11,15 +11,15 @@ var (
 		// A regular expression representing DNS errors for the guest API domain.
 		regexp.MustCompile(`dial tcp: lookup .* on .*:53: (no such host|server misbehaving)`),
 		// A regular expression representing EOF errors for the guest API domain.
-		regexp.MustCompile(`Get https://api\..*/api/v1/nodes.* (unexpected )?EOF`),
+		regexp.MustCompile(`[Get|Patch|Post] https://api\..*/api/v1/nodes.* (unexpected )?EOF`),
 		// A regular expression representing EOF errors for the guest API domain.
-		regexp.MustCompile(`[Get|Post] https://api\..*/api/v1/namespaces/*/.* (unexpected )?EOF`),
+		regexp.MustCompile(`[Get|Patch|Post] https://api\..*/api/v1/namespaces/*/.* (unexpected )?EOF`),
 		// A regular expression representing TLS errors related to establishing
 		// connections to guest clusters while the guest API is not fully up.
-		regexp.MustCompile(`Get https://api\..*/api/v1/nodes.* net/http: (TLS handshake timeout|request canceled).*?`),
+		regexp.MustCompile(`[Get|Patch|Post] https://api\..*/api/v1/nodes.* net/http: (TLS handshake timeout|request canceled).*?`),
 		// A regular expression representing the kind of transient errors related to
 		// certificates returned while the guest API is not fully up.
-		regexp.MustCompile(`[Get|Post] https://api\..*: x509: (certificate is valid for ingress.local, not api\..*|certificate signed by unknown authority \(possibly because of "crypto/rsa: verification error" while trying to verify candidate authority certificate.*?\))`),
+		regexp.MustCompile(`[Get|Patch|Post] https://api\..*: x509: (certificate is valid for ingress.local, not api\..*|certificate signed by unknown authority \(possibly because of "crypto/rsa: verification error" while trying to verify candidate authority certificate.*?\))`),
 	}
 )
 
--- vendor/github.com/giantswarm/statusresource/delete.go
+++
@@ -1,9 +0,0 @@
-package statusresource
-
-import (
-	"context"
-)
-
-func (r *Resource) EnsureDeleted(ctx context.Context, obj interface{}) error {
-	return nil
-}
