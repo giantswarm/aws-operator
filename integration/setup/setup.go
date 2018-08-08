0669d398fce2b23de59e0f54cbc315d581a89cdb
Tim Schindler <xh3b4sd@gmail.com>
2018-08-08 19:00:14 +0200

    wip

--- integration/setup/setup.go
+++ integration/setup/setup.go
@@ -11,7 +11,7 @@ import (
 
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/service/cloudformation"
-	"github.com/cenkalti/backoff"
+	"github.com/giantswarm/backoff"
 	"github.com/giantswarm/e2e-harness/pkg/framework"
 	awsclient "github.com/giantswarm/e2eclients/aws"
 	"github.com/giantswarm/e2etemplates/pkg/e2etemplates"
