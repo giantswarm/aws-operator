5a13b27926b37513b0b3a184c1237f731d05bf9d
Ross Fairbanks <ross@giantswarm.io>
2018-08-24 10:17:48 +0200

    v14patch1: copy v14

---
+++ service/controller/v14patch1/version_bundle.go
@@ -0,0 +1,49 @@
+package v14
+
+import (
+	"github.com/giantswarm/versionbundle"
+)
+
+func VersionBundle() versionbundle.Bundle {
+	return versionbundle.Bundle{
+		Changelogs: []versionbundle.Changelog{
+			{
+				Component:   "aws-operator",
+				Description: "Added Route53 support for BYOC accounts.",
+				Kind:        versionbundle.KindAdded,
+			},
+		},
+		Components: []versionbundle.Component{
+			{
+				Name:    "calico",
+				Version: "3.0.5",
+			},
+			{
+				Name:    "containerlinux",
+				Version: "1745.4.0",
+			},
+			{
+				Name:    "docker",
+				Version: "18.03.1",
+			},
+			{
+				Name:    "etcd",
+				Version: "3.3.3",
+			},
+			{
+				Name:    "coredns",
+				Version: "1.1.1",
+			},
+			{
+				Name:    "kubernetes",
+				Version: "1.10.4",
+			},
+			{
+				Name:    "nginx-ingress-controller",
+				Version: "0.12.0",
+			},
+		},
+		Name:    "aws-operator",
+		Version: "3.3.0",
+	}
+}
