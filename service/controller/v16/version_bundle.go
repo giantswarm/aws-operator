2a00df4b5009e26f3826bc2535e8a176e0c1773f
Tuomas Makinen <tuommaki@gmail.com>
2018-09-05 19:56:06 +0300

    Add changelog message

--- service/controller/v16/version_bundle.go
+++ service/controller/v16/version_bundle.go
@@ -9,7 +9,7 @@ func VersionBundle() versionbundle.Bundle {
 		Changelogs: []versionbundle.Changelog{
 			{
 				Component:   "aws-operator",
-				Description: "Add your changes here.",
+				Description: "Changed worker node docker volume size to be configurable.",
 				Kind:        versionbundle.KindChanged,
 			},
 		},
