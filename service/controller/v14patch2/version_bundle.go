a4d09084feabafd94c2605d9a6f680873c3f0191
Volodymyr Stoiko <me@volodymyrstoiko.com>
2018-09-14 17:53:21 +0300

    Fix changelog

--- service/controller/v14patch2/version_bundle.go
+++ service/controller/v14patch2/version_bundle.go
@@ -10,7 +10,7 @@ func VersionBundle() versionbundle.Bundle {
 			{
 				Component:   "cloudconfig",
 				Description: "Add kube-proxy configuration files, required for updating kube-proxy daemonset.",
-				Kind:        versionbundle.KindRemoved,
+				Kind:        versionbundle.KindAdded,
 			},
 		},
 		Components: []versionbundle.Component{
