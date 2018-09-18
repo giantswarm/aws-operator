3dc8cd788bc70d75c3af6c09255a0b886234a914
Paweł Kopiczko <kopiczko@gmail.com>
2018-09-18 11:24:34 +0100

    update e2e-harness to trigger tests

--- vendor/github.com/google/go-github/github/github-accessors.go
+++ vendor/github.com/google/go-github/github/github-accessors.go
@@ -7300,14 +7300,6 @@ func (p *PullRequestEvent) GetNumber() int {
 	return *p.Number
 }
 
-// GetOrganization returns the Organization field.
-func (p *PullRequestEvent) GetOrganization() *Organization {
-	if p == nil {
-		return nil
-	}
-	return p.Organization
-}
-
 // GetPullRequest returns the PullRequest field.
 func (p *PullRequestEvent) GetPullRequest() *PullRequest {
 	if p == nil {
