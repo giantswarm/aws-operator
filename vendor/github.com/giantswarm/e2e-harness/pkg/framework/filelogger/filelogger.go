f543873f8566a80a0de41199360c69b4a0e2ca50
MarcelMue <marcel.mueller1@rwth-aachen.de>
2018-09-21 13:40:32 +0200

    Update e2e-harness vendor

--- vendor/github.com/giantswarm/e2e-harness/pkg/framework/filelogger/filelogger.go
+++ vendor/github.com/giantswarm/e2e-harness/pkg/framework/filelogger/filelogger.go
@@ -90,8 +90,10 @@ func (f FileLogger) scan(readCloser io.ReadCloser, name string) {
 
 	defer outFile.Close()
 
+	writers := io.MultiWriter(os.Stdout, outFile)
+
 	f.logger.Log("level", "debug", "message", fmt.Sprintf("logging output of %#q to %#q", name, outFile.Name()))
-	_, err = io.Copy(outFile, readCloser)
+	_, err = io.Copy(writers, readCloser)
 	if err != nil {
 		f.logger.Log("level", "error", "stack", microerror.Mask(err))
 	}
