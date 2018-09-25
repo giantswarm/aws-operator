bace34e636f246ee8c1df95260cbccb130d8c14f
Roma Sokolkov <roman@giantswarm.io>
2018-09-25 15:57:03 +0200

    v14patch3: wire

--- service/controller/drainer.go
+++ service/controller/drainer.go
@@ -20,6 +20,7 @@ import (
 	"github.com/giantswarm/aws-operator/service/controller/v14"
 	"github.com/giantswarm/aws-operator/service/controller/v14patch1"
 	"github.com/giantswarm/aws-operator/service/controller/v14patch2"
+	"github.com/giantswarm/aws-operator/service/controller/v14patch3"
 	"github.com/giantswarm/aws-operator/service/controller/v15"
 	"github.com/giantswarm/aws-operator/service/controller/v16"
 	"github.com/giantswarm/aws-operator/service/controller/v17"
@@ -330,6 +331,24 @@ func newDrainerResourceSets(config DrainerConfig) ([]*controller.ResourceSet, er
 		}
 
 		v14Patch2ResourceSet, err = v14patch2.NewDrainerResourceSet(c)
+		if err != nil {
+			return nil, microerror.Mask(err)
+		}
+	}
+
+	var v14Patch3ResourceSet *controller.ResourceSet
+	{
+		c := v14patch3.DrainerResourceSetConfig{
+			G8sClient:     config.G8sClient,
+			HostAWSConfig: hostAWSConfig,
+			K8sClient:     config.K8sClient,
+			Logger:        config.Logger,
+
+			GuestUpdateEnabled: config.GuestUpdateEnabled,
+			ProjectName:        config.ProjectName,
+		}
+
+		v14Patch3ResourceSet, err = v14patch3.NewDrainerResourceSet(c)
 		if err != nil {
 			return nil, microerror.Mask(err)
 		}
