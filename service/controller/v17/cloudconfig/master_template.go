36dab0940b5f54f027166fe7aff2c5b55d41c750
Roma Sokolkov <rsokolkov@gmail.com>
2018-09-25 13:43:05 +0200

    v17: Set NVME driver timeouts (#1176)
    * v17: Set NVME driver timeouts
    
    * v17: align version bundle info
    
    * add TODO
    
    * vendor latest certs
    
    * Add service to workers

--- service/controller/v17/cloudconfig/master_template.go
+++ service/controller/v17/cloudconfig/master_template.go
@@ -274,6 +274,15 @@ func (e *MasterExtension) Units() ([]k8scloudconfig.UnitAsset, error) {
 			Enable:       false,
 			Command:      "start",
 		},
+		// Set bigger timeouts for NVME driver.
+		// Workaround for https://github.com/coreos/bugs/issues/2484
+		// TODO issue: https://github.com/giantswarm/giantswarm/issues/4255
+		{
+			AssetContent: cloudconfig.NVMESetTimeoutsUnit,
+			Name:         "nvme-set-timeouts.service",
+			Enable:       true,
+			Command:      "start",
+		},
 		{
 			AssetContent: cloudconfig.DecryptTLSAssetsService,
 			Name:         "decrypt-tls-assets.service",
