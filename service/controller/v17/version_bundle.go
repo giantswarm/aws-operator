8c34a0479235c4501b542e001aa07ec0f45b4ce4
Fernando Ripoll <pipo02mix@gmail.com>
2018-10-15 17:02:31 +0200

    Remove coredns component from version newer than 4.2.0

--- service/controller/v17/version_bundle.go
+++ service/controller/v17/version_bundle.go
@@ -56,10 +56,6 @@ func VersionBundle() versionbundle.Bundle {
 				Version: "3.3.8",
 			},
 			{
-				Name:    "coredns",
-				Version: "1.1.1",
-			},
-			{
 				Name:    "kubernetes",
 				Version: "1.11.1",
 			},
