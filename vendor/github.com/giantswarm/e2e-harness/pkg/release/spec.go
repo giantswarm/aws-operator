c86a90e2a6f7c37c41b1a7b9c0f659ce8d93734c
Paweł Kopiczko <kopiczko@gmail.com>
2018-10-25 16:03:56 +0100

    vendor: update (#1222)

--- vendor/github.com/giantswarm/e2e-harness/pkg/release/spec.go
+++ vendor/github.com/giantswarm/e2e-harness/pkg/release/spec.go
@@ -2,11 +2,16 @@ package release
 
 import (
 	"context"
+
+	apiextensionsv1beta1 "k8s.io/apiextensions-apiserver/pkg/apis/apiextensions/v1beta1"
 )
 
 type ConditionFunc func() error
 
 type ConditionSet interface {
+	// CRDExists returns a function waiting for the CRD to appear in the
+	// Kubernetes API described by the given definition.
+	CRDExists(ctx context.Context, crd *apiextensionsv1beta1.CustomResourceDefinition) ConditionFunc
 	// PodExists returns a function waiting for a Pod to appear in the
 	// Kubernetes API described by the given label selector.
 	PodExists(ctx context.Context, namespace, labelSelector string) ConditionFunc
