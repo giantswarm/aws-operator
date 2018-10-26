37f2e8d5dc6270d9b8a7e0e46e3d4c3710b24b97
Tim Schindler <xh3b4sd@gmail.com>
2018-10-26 20:43:19 +0200

    Revert "adds debug logging"
    This reverts commit 5c48640ff11d91f90388e38b794e6a48c66dceac.

--- service/collector/elb.go
+++ service/collector/elb.go
@@ -1,8 +1,6 @@
 package collector
 
 import (
-	"fmt"
-
 	"github.com/aws/aws-sdk-go/service/elb"
 	"github.com/prometheus/client_golang/prometheus"
 	"golang.org/x/sync/errgroup"
@@ -78,20 +76,10 @@ func (c *ELBCollector) Collect(ch chan<- prometheus.Metric) error {
 		return microerror.Mask(err)
 	}
 
-	fmt.Printf("\n")
-	fmt.Printf("len(awsClientsList): %#v\n", len(awsClientsList))
-	fmt.Printf("\n")
-
-	fmt.Printf("\n")
-	fmt.Printf("start collect\n")
-	fmt.Printf("\n")
-
 	var g errgroup.Group
 
 	for _, awsClients := range awsClientsList {
-		fmt.Printf("1\n")
 		g.Go(func() error {
-			fmt.Printf("2\n")
 			err := c.collectForAccount(ch, awsClients)
 			if err != nil {
 				return microerror.Mask(err)
@@ -105,10 +93,6 @@ func (c *ELBCollector) Collect(ch chan<- prometheus.Metric) error {
 	if err != nil {
 		return microerror.Mask(err)
 	}
-
-	fmt.Printf("\n")
-	fmt.Printf("end collect\n")
-	fmt.Printf("\n")
 
 	return nil
 }
@@ -133,10 +117,6 @@ func (c *ELBCollector) collectForAccount(ch chan<- prometheus.Metric, awsClients
 		}
 		loadbalancers = o.LoadBalancerDescriptions
 	}
-
-	fmt.Printf("\n")
-	fmt.Printf("start loop\n")
-	fmt.Printf("\n")
 
 	for _, l := range loadbalancers {
 		var tags []*elb.Tag
@@ -193,19 +173,6 @@ func (c *ELBCollector) collectForAccount(ch chan<- prometheus.Metric, awsClients
 			}
 		}
 
-		fmt.Printf("\n")
-		fmt.Printf("\n")
-		fmt.Printf("\n")
-		fmt.Printf("  count: %#v\n", count)
-		fmt.Printf("  *l.LoadBalancerName: %#v\n", *l.LoadBalancerName)
-		fmt.Printf("  account: %#v\n", account)
-		fmt.Printf("  cluster: %#v\n", cluster)
-		fmt.Printf("  installation: %#v\n", installation)
-		fmt.Printf("  organization: %#v\n", organization)
-		fmt.Printf("\n")
-		fmt.Printf("\n")
-		fmt.Printf("\n")
-
 		ch <- prometheus.MustNewConstMetric(
 			elbsDesc,
 			prometheus.GaugeValue,
@@ -217,10 +184,6 @@ func (c *ELBCollector) collectForAccount(ch chan<- prometheus.Metric, awsClients
 			organization,
 		)
 	}
-
-	fmt.Printf("\n")
-	fmt.Printf("end loop\n")
-	fmt.Printf("\n")
 
 	return nil
 }
