ca74fd1227ada26fc940e1f61aced5796f152076
Bricktop <marcel.mueller1@rwth-aachen.de>
2018-10-31 10:27:21 +0100

    Add AvailabilityZones Flag (#1229)
    * Add AvailabilityZones Flag
    
    * fix typo
    
    * Fix namingto Availability zones
    
    Co-Authored-By: MarcelMue <marcel.mueller1@rwth-aachen.de>

--- flag/service/aws/aws.go
+++ flag/service/aws/aws.go
@@ -10,6 +10,7 @@ import (
 type AWS struct {
 	AccessKey              accesskey.AccessKey
 	AdvancedMonitoringEC2  string
+	AvailabilityZones      string
 	Encrypter              string
 	HostAccessKey          accesskey.AccessKey
 	IncludeTags            string
