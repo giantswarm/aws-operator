3decec36ca82b7cc7a10b09c55bcfeb7e4b0ac86
Timo Derstappen <teemow@gmail.com>
2018-11-07 17:41:31 +0100

    v18patch1: copy v18

---
+++ service/controller/v18patch1/encrypter/spec.go
@@ -0,0 +1,38 @@
+package encrypter
+
+import (
+	"context"
+
+	"github.com/giantswarm/apiextensions/pkg/apis/provider/v1alpha1"
+)
+
+const (
+	KMSBackend   = "kms"
+	VaultBackend = "vault"
+)
+
+type EncryptionKeyState struct {
+	KeyID   string
+	KeyName string
+}
+
+type Interface interface {
+	Encrypter
+	Resource
+}
+
+type Encrypter interface {
+	EncryptionKey(ctx context.Context, customObject v1alpha1.AWSConfig) (string, error)
+	Encrypt(ctx context.Context, key, plaintext string) (string, error)
+	IsKeyNotFound(error) bool
+}
+
+type Resource interface {
+	EnsureCreatedEncryptionKey(context.Context, v1alpha1.AWSConfig) error
+	EnsureDeletedEncryptionKey(context.Context, v1alpha1.AWSConfig) error
+}
+
+type RoleManager interface {
+	EnsureCreatedAuthorizedIAMRoles(context.Context, v1alpha1.AWSConfig) error
+	EnsureDeletedAuthorizedIAMRoles(context.Context, v1alpha1.AWSConfig) error
+}
