ce28c4c862219ec880633fe7ec28ecdae007b4c4
Paweł Kopiczko <kopiczko@gmail.com>
2018-11-08 09:00:48 +0000

    v18: add changelog about waiting for CF stacks to be deleted (#1239)

--- service/controller/v18/version_bundle.go
+++ service/controller/v18/version_bundle.go
@@ -8,6 +8,11 @@ func VersionBundle() versionbundle.Bundle {
 	return versionbundle.Bundle{
 		Changelogs: []versionbundle.Changelog{
 			{
+				Component:   "aws-operator",
+				Description: "Fixed a bug with removing CR finalizers before all Cloud Formation stacks are deleted.",
+				Kind:        versionbundle.KindFixed,
+			},
+			{
 				Component:   "kubernetes",
 				Description: "Updated Kubernetes to 1.12.1. More info here: https://github.com/kubernetes/kubernetes/blob/master/CHANGELOG-1.12.md",
 				Kind:        versionbundle.KindChanged,
