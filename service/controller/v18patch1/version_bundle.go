3fde9e8704b28bbf72f68c5d1d23247234505791
Timo Derstappen <teemow@gmail.com>
2018-11-09 19:02:39 +0100

    update k8scloudconfig to 3.7.1 (#1247)
    this makes sure
    
    * that the kube-proxy is installed before we wait for calico to be installed and ready.
    * the pod priority class for calico is correct.

--- service/controller/v18patch1/version_bundle.go
+++ service/controller/v18patch1/version_bundle.go
@@ -8,33 +8,18 @@ func VersionBundle() versionbundle.Bundle {
 	return versionbundle.Bundle{
 		Changelogs: []versionbundle.Changelog{
 			{
-				Component:   "kubernetes",
-				Description: "Updated Kubernetes to 1.12.1. More info here: https://github.com/kubernetes/kubernetes/blob/master/CHANGELOG-1.12.md",
+				Component:   "cloudconfig",
+				Description: "Updated k8scloudconfig to 3.7.1.",
+				Kind:        versionbundle.KindChanged,
+			},
+			{
+				Component:   "kube-proxy",
+				Description: "Now gets installed and upgraded before Calico.",
 				Kind:        versionbundle.KindChanged,
 			},
 			{
 				Component:   "calico",
-				Description: "Updated to 3.2.3. Also the manifest has proper resource limits to get QoS policy guaranteed.",
-				Kind:        versionbundle.KindChanged,
-			},
-			{
-				Component:   "kubernetes",
-				Description: "Enabled admission plugins: DefaultTolerationSeconds, MutatingAdmissionWebhook, ValidatingAdmissionWebhook.",
-				Kind:        versionbundle.KindChanged,
-			},
-			{
-				Component:   "container-linux",
-				Description: "Updated to latest stable 1855.5.0",
-				Kind:        versionbundle.KindChanged,
-			},
-			{
-				Component:   "etcd",
-				Description: "Updated to 3.3.9",
-				Kind:        versionbundle.KindChanged,
-			},
-			{
-				Component:   "docker",
-				Description: "Updated to 18.06.1",
+				Description: "Reapplied missing priority class.",
 				Kind:        versionbundle.KindChanged,
 			},
 		},
--- service/controller/v19/key/key.go
+++ service/controller/v19/key/key.go
@@ -18,7 +18,7 @@ import (
 const (
 	// CloudConfigVersion defines the version of k8scloudconfig in use.
 	// It is used in the main stack output and S3 object paths.
-	CloudConfigVersion = "v_3_7_0"
+	CloudConfigVersion = "v_3_7_1"
 
 	// CloudProviderTagName is used to add Cloud Provider tags to AWS resources.
 	CloudProviderTagName = "kubernetes.io/cluster/%s"
