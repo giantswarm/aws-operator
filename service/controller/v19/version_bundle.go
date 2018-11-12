66bb9aee2fcd87d0ebe6f1292a3eda8808d38c6c
Tuomas Makinen <tuommaki@gmail.com>
2018-11-12 12:30:52 +0200

    Add support for multiple availability zones

--- service/controller/v19/version_bundle.go
+++ service/controller/v19/version_bundle.go
@@ -8,9 +8,9 @@ func VersionBundle() versionbundle.Bundle {
 	return versionbundle.Bundle{
 		Changelogs: []versionbundle.Changelog{
 			{
-				Component:   "TODO",
-				Description: "Add changes here.",
-				Kind:        versionbundle.KindChanged,
+				Component:   "aws-operator",
+				Description: "Added support for using multiple availability zones for workers.",
+				Kind:        versionbundle.KindAdded,
 			},
 		},
 		Components: []versionbundle.Component{
