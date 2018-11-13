7dacc1b673feea91a7b739a5c0cad7495cc72cf4
Paweł Kopiczko <kopiczko@gmail.com>
2018-11-13 10:16:09 +0000

    v19/resource/migration: improve logs (#1253)
    https://github.com/giantswarm/giantswarm/issues/4571
    
    It looks like we update a CR that is newer than the one we observe in
    a next reconciliation loop. These logs will allow to confirm that.
    
    They also are aligned with our current standards.

--- service/controller/v19/resource/migration/resource.go
+++ service/controller/v19/resource/migration/resource.go
@@ -11,12 +11,10 @@ import (
 
 	providerv1alpha1 "github.com/giantswarm/apiextensions/pkg/apis/provider/v1alpha1"
 	"github.com/giantswarm/apiextensions/pkg/clientset/versioned"
+	"github.com/giantswarm/aws-operator/service/controller/v19/key"
 	"github.com/giantswarm/microerror"
 	"github.com/giantswarm/micrologger"
-	"github.com/giantswarm/operatorkit/controller/context/reconciliationcanceledcontext"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-
-	"github.com/giantswarm/aws-operator/service/controller/v19/key"
 )
 
 const (
@@ -79,23 +77,21 @@ func (r *Resource) EnsureCreated(ctx context.Context, obj interface{}) error {
 		if err != nil {
 			return microerror.Mask(err)
 		}
-		if reflect.DeepEqual(customObject.Spec, oldSpec) {
-			return nil
-		}
 	}
 
 	{
-		r.logger.LogCtx(ctx, "level", "debug", "message", "updating CR")
+		r.logger.LogCtx(ctx, "level", "debug", "message", fmt.Sprintf("updating CR with version %#q", customObject.ResourceVersion))
 
-		_, err := r.g8sClient.ProviderV1alpha1().AWSConfigs(customObject.GetNamespace()).Update(&customObject)
-		if err != nil {
-			return microerror.Mask(err)
+		if reflect.DeepEqual(customObject.Spec, oldSpec) {
+			r.logger.LogCtx(ctx, "level", "debug", "message", fmt.Sprintf("CR with version %#q is already up to date", customObject.ResourceVersion))
+		} else {
+			_, err := r.g8sClient.ProviderV1alpha1().AWSConfigs(customObject.GetNamespace()).Update(&customObject)
+			if err != nil {
+				return microerror.Mask(err)
+			}
+
+			r.logger.LogCtx(ctx, "level", "debug", "message", fmt.Sprintf("updated CR with version %#q", customObject.ResourceVersion))
 		}
-
-		r.logger.LogCtx(ctx, "level", "debug", "message", "updated CR")
-
-		r.logger.LogCtx(ctx, "level", "debug", "message", "canceling reconciliation")
-		reconciliationcanceledcontext.SetCanceled(ctx)
 	}
 
 	return nil
@@ -107,7 +103,9 @@ func (r *Resource) EnsureDeleted(ctx context.Context, obj interface{}) error {
 
 func (r *Resource) migrateSpec(ctx context.Context, spec *providerv1alpha1.AWSConfigSpec) error {
 	if spec.AWS.CredentialSecret.Name == "" {
-		r.logger.LogCtx(ctx, "level", "debug", "message", "CR is missing credential, setting the default")
+		r.logger.LogCtx(ctx, "level", "debug", "message", "CR is missing credential secret")
+
+		r.logger.LogCtx(ctx, "level", "debug", "message", fmt.Sprintf("setting CR credential secret to %#q in namespace %#q", credentialSecretDefaultName, credentialSecretDefaultNamespace))
 
 		spec.AWS.CredentialSecret.Namespace = credentialSecretDefaultNamespace
 		spec.AWS.CredentialSecret.Name = credentialSecretDefaultName
@@ -122,7 +120,7 @@ func (r *Resource) migrateSpec(ctx context.Context, spec *providerv1alpha1.AWSCo
 			return microerror.Mask(err)
 		}
 
-		r.logger.LogCtx(ctx, "level", "debug", "message", fmt.Sprintf("settings all zones to %q", zone))
+		r.logger.LogCtx(ctx, "level", "debug", "message", fmt.Sprintf("setting CR hosted zone names to %#q", zone))
 
 		spec.AWS.HostedZones.API.Name = zone
 		spec.AWS.HostedZones.Etcd.Name = zone
