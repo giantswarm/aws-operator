29bdc8885b266a6c1dfa422912d3147a2573ea6d
Tuomas Makinen <tuommaki@gmail.com>
2018-11-15 13:04:38 +0200

    Fix dependent route table names

--- service/controller/v19/adapter/guest_internet_gateway.go
+++ service/controller/v19/adapter/guest_internet_gateway.go
@@ -1,13 +1,22 @@
 package adapter
 
-import "github.com/giantswarm/aws-operator/service/controller/v19/key"
+import (
+	"fmt"
+
+	"github.com/giantswarm/aws-operator/service/controller/v19/key"
+)
 
 type GuestInternetGatewayAdapter struct {
-	ClusterID string
+	ClusterID          string
+	PrivateRouteTables []string
 }
 
 func (a *GuestInternetGatewayAdapter) Adapt(cfg Config) error {
 	a.ClusterID = key.ClusterID(cfg.CustomObject)
+
+	for i := 0; i < key.SpecAvailabilityZones(cfg.CustomObject); i++ {
+		a.PrivateRouteTables = append(a.PrivateRouteTables, fmt.Sprintf("PrivateRouteTable%02d", i))
+	}
 
 	return nil
 }
