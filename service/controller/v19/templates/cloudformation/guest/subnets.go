085ce16d491ddcadff9bac2653edf21e62123bf7
Tuomas Makinen <tuommaki@gmail.com>
2018-11-15 12:07:58 +0200

    Fix indentation of subnet section

--- service/controller/v19/templates/cloudformation/guest/subnets.go
+++ service/controller/v19/templates/cloudformation/guest/subnets.go
@@ -3,41 +3,41 @@ package guest
 const Subnets = `{{ define "subnets" }}
 {{- $v := .Guest.Subnets }}
   {{- range $v.PublicSubnets }}
-    {{ .Name }}:
-      Type: AWS::EC2::Subnet
-      Properties:
-        AvailabilityZone: {{ .AvailabilityZone }}
-        CidrBlock: {{ .CIDR }}
-        MapPublicIpOnLaunch: {{ .MapPublicIPOnLaunch }}
-        Tags:
-        - Key: Name
-          Value: {{ .Name }}
-        VpcId: !Ref VPC
+  {{ .Name }}:
+    Type: AWS::EC2::Subnet
+    Properties:
+      AvailabilityZone: {{ .AvailabilityZone }}
+      CidrBlock: {{ .CIDR }}
+      MapPublicIpOnLaunch: {{ .MapPublicIPOnLaunch }}
+      Tags:
+      - Key: Name
+        Value: {{ .Name }}
+      VpcId: !Ref VPC
 
-    PublicRouteTableAssociation{{ .Index }}:
-      Type: AWS::EC2::SubnetRouteTableAssociation
-      Properties:
-        RouteTableId: !Ref PublicRouteTable
-        SubnetId: !Ref {{ .Name }}
+  PublicRouteTableAssociation{{ .Index }}:
+    Type: AWS::EC2::SubnetRouteTableAssociation
+    Properties:
+      RouteTableId: !Ref PublicRouteTable
+      SubnetId: !Ref {{ .Name }}
 
   {{ end }}
 
   {{- range $v.PrivateSubnets }}
-    {{ .Name }}:
-      Type: AWS::EC2::Subnet
-      Properties:
-        AvailabilityZone: {{ .AvailabilityZone }}
-        CidrBlock: {{ .CIDR }}
-        MapPublicIpOnLaunch: {{ .MapPublicIPOnLaunch }}
-        Tags:
-        - Key: Name
-          Value: {{ .Name }}
-        VpcId: !Ref VPC
+  {{ .Name }}:
+    Type: AWS::EC2::Subnet
+    Properties:
+      AvailabilityZone: {{ .AvailabilityZone }}
+      CidrBlock: {{ .CIDR }}
+      MapPublicIpOnLaunch: {{ .MapPublicIPOnLaunch }}
+      Tags:
+      - Key: Name
+        Value: {{ .Name }}
+      VpcId: !Ref VPC
 
-    PrivateRouteTableAssociation{{ .Index }}:
-      Type: AWS::EC2::SubnetRouteTableAssociation
-      Properties:
-        RouteTableId: !Ref PrivateRouteTable{{ .Index }}
-        SubnetId: !Ref {{ .Name }}
+  PrivateRouteTableAssociation{{ .Index }}:
+    Type: AWS::EC2::SubnetRouteTableAssociation
+    Properties:
+      RouteTableId: !Ref PrivateRouteTable{{ .Index }}
+      SubnetId: !Ref {{ .Name }}
   {{ end }}
 {{ end }}`
