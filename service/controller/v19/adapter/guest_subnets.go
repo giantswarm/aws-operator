2540e074d02fcb10c3dbf3e3c40e154c1102d415
Tuomas Mäkinen <1947505+tuommaki@users.noreply.github.com>
2018-11-16 12:15:31 +0200

    Fix various mistakes in adapter & CloudFormation template (#1282)
    * Fix indentation of subnet section
    
    * Remove unnecessary index from PublicRouteTable
    
    * Fix dependent route table names
    
    * Range over public subnets for LB subnets
    
    * Iterate over subnets in ASG
    
    * Use first configured AZ for master instance
    
    * Fix RouteTableAssociation names/references
    
    * Fix ASG Availability Zones

--- service/controller/v19/adapter/guest_subnets.go
+++ service/controller/v19/adapter/guest_subnets.go
@@ -8,11 +8,18 @@ import (
 )
 
 type Subnet struct {
-	Index               int
-	AvailabilityZone    string
-	CIDR                string
-	Name                string
-	MapPublicIPOnLaunch bool
+	Index                 int
+	AvailabilityZone      string
+	CIDR                  string
+	Name                  string
+	MapPublicIPOnLaunch   bool
+	RouteTableAssociation RouteTableAssociation
+}
+
+type RouteTableAssociation struct {
+	Name           string
+	RouteTableName string
+	SubnetName     string
 }
 
 type GuestSubnetsAdapter struct {
@@ -26,21 +33,33 @@ func (s *GuestSubnetsAdapter) Adapt(cfg Config) error {
 		return zones[i].Name < zones[j].Name
 	})
 	for i, az := range zones {
+		snetName := fmt.Sprintf("PublicSubnet%02d", i)
 		snet := Subnet{
 			Index:               i,
 			AvailabilityZone:    az.Name,
 			CIDR:                az.Subnet.Public.CIDR,
-			Name:                fmt.Sprintf("PublicSubnet%02d", i),
+			Name:                snetName,
 			MapPublicIPOnLaunch: false,
+			RouteTableAssociation: RouteTableAssociation{
+				Name:           fmt.Sprintf("PublicRouteTableAssociation%02d", i),
+				RouteTableName: "PublicRouteTable",
+				SubnetName:     snetName,
+			},
 		}
 		s.PublicSubnets = append(s.PublicSubnets, snet)
 
+		snetName = fmt.Sprintf("PrivateSubnet%02d", i)
 		snet = Subnet{
 			Index:               i,
 			AvailabilityZone:    az.Name,
 			CIDR:                az.Subnet.Private.CIDR,
-			Name:                fmt.Sprintf("PrivateSubnet%02d", i),
+			Name:                snetName,
 			MapPublicIPOnLaunch: false,
+			RouteTableAssociation: RouteTableAssociation{
+				Name:           fmt.Sprintf("PrivateRouteTableAssociation%02d", i),
+				RouteTableName: fmt.Sprintf("PrivateRouteTable%02d", i),
+				SubnetName:     snetName,
+			},
 		}
 		s.PrivateSubnets = append(s.PrivateSubnets, snet)
 	}
