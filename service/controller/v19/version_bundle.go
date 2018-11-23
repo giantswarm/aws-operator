d8e3f936364fccc80ae438dca7f76b2467b7e500
Timo Derstappen <teemow@gmail.com>
2018-11-23 17:51:49 +0100

    add changelog for 6.1.0

--- service/controller/v19/version_bundle.go
+++ service/controller/v19/version_bundle.go
@@ -9,7 +9,37 @@ func VersionBundle() versionbundle.Bundle {
 		Changelogs: []versionbundle.Changelog{
 			{
 				Component:   "aws-operator",
-				Description: "Added support for using multiple availability zones for workers.",
+				Description: "Add support for using multiple availability zones. See https://docs.giantswarm.io/basics/multiaz/.",
+				Kind:        versionbundle.KindAdded,
+			},
+			{
+				Component:   "aws-operator",
+				Description: "Switch back to an internal elb for etcd. Calico connection handling problems have been fixed upstream.",
+				Kind:        versionbundle.KindAdded,
+			},
+			{
+				Component:   "aws-operator",
+				Description: "Upgrade: Terminate the old master right after detaching of its volumes.",
+				Kind:        versionbundle.KindAdded,
+			},
+			{
+				Component:   "aws-operator",
+				Description: "Add autoscaling permissions to the IAM policy of the cluster.",
+				Kind:        versionbundle.KindAdded,
+			},
+			{
+				Component:   "cloud-config",
+				Description: "The pod priority class for calico got lost. We found it again!",
+				Kind:        versionbundle.KindAdded,
+			},
+			{
+				Component:   "k8s-addons",
+				Description: "kube-proxy is now installed before calico during cluster creation and upgrades.",
+				Kind:        versionbundle.KindAdded,
+			},
+			{
+				Component:   "cloud-config",
+				Description: "Calico upgrade improvements: Remove the old master from the k8s api and wait until etcd DNS is resolvable before upgrading calico. Networking pods crashlooping isn't fun!",
 				Kind:        versionbundle.KindAdded,
 			},
 		},
