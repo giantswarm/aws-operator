dc92d5fa272d11c2ceca7bab7f5ecdf8451b8f49
corest <me@volodymyrstoiko.com>
2018-12-17 06:22:37 +0200

    Add dependencies for k8s-kubelet

--- vendor/github.com/giantswarm/k8scloudconfig/v_4_0_0/master_template.go
+++ vendor/github.com/giantswarm/k8scloudconfig/v_4_0_0/master_template.go
@@ -269,6 +269,8 @@ systemd:
       ExecStopPost=-/usr/bin/docker rm -f $NAME
       [Install]
       WantedBy=multi-user.target
+      Requires=network-online.target
+      After=network-online.target
   - name: etcd2.service
     enabled: false
     mask: true
