d026fd2bd3c8e2405aaf388ef72cb6bbc7dd1b07
Paweł Kopiczko <kopiczko@gmail.com>
2019-01-18 07:20:02 +0000

    vnedor & apply operatorkit changes

--- service/controller/v21/drainer_resource_set.go
+++ service/controller/v21/drainer_resource_set.go
@@ -91,9 +91,7 @@ func NewDrainerResourceSet(config DrainerResourceSetConfig) (*controller.Resourc
 	}
 
 	{
-		c := metricsresource.WrapConfig{
-			Name: config.ProjectName,
-		}
+		c := metricsresource.WrapConfig{}
 
 		resources, err = metricsresource.Wrap(resources, c)
 		if err != nil {
