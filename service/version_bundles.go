ae602562ad0b4939820745b1494aa2367d7412aa
calvix <rozsypalek.vaclav@gmail.com>
2019-01-21 11:59:21 +0100

    properly-expose-v22-vb (#1364)

--- service/version_bundles.go
+++ service/version_bundles.go
@@ -8,6 +8,7 @@ import (
 	v19 "github.com/giantswarm/aws-operator/service/controller/v19"
 	v20 "github.com/giantswarm/aws-operator/service/controller/v20"
 	v21 "github.com/giantswarm/aws-operator/service/controller/v21"
+	v22 "github.com/giantswarm/aws-operator/service/controller/v22"
 )
 
 // NewVersionBundles returns the array of version bundles defined for the
@@ -20,6 +21,7 @@ func NewVersionBundles() []versionbundle.Bundle {
 	versionBundles = append(versionBundles, v19.VersionBundle())
 	versionBundles = append(versionBundles, v20.VersionBundle())
 	versionBundles = append(versionBundles, v21.VersionBundle())
+	versionBundles = append(versionBundles, v22.VersionBundle())
 
 	return versionBundles
 }
