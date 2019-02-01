1279d2c44085490e9e287a269b0d2d65046b981a
Tim Schindler <xh3b4sd@gmail.com>
2019-02-01 21:53:49 +0100

    compute desired cloud configs in parallel

--- service/controller/v22/resource/s3object/desired.go
+++ service/controller/v22/resource/s3object/desired.go
@@ -2,10 +2,12 @@ package s3object
 
 import (
 	"context"
+	"sync"
 
 	"github.com/giantswarm/certs"
 	"github.com/giantswarm/microerror"
 	"github.com/giantswarm/randomkeys"
+	"golang.org/x/sync/errgroup"
 
 	"github.com/giantswarm/aws-operator/service/controller/v22/controllercontext"
 	"github.com/giantswarm/aws-operator/service/controller/v22/key"
@@ -24,41 +26,76 @@ func (r *Resource) GetDesiredState(ctx context.Context, obj interface{}) (interf
 	var clusterCerts certs.Cluster
 	var clusterKeys randomkeys.Cluster
 	{
-		clusterCerts, err = r.certsSearcher.SearchCluster(key.ClusterID(customObject))
-		if err != nil {
-			return nil, microerror.Mask(err)
-		}
-		clusterKeys, err = r.randomKeysSearcher.SearchCluster(key.ClusterID(customObject))
+		g := &errgroup.Group{}
+
+		g.Go(func() error {
+			clusterCerts, err = r.certsSearcher.SearchCluster(key.ClusterID(customObject))
+			if err != nil {
+				return microerror.Mask(err)
+			}
+
+			return nil
+		})
+
+		g.Go(func() error {
+			clusterKeys, err = r.randomKeysSearcher.SearchCluster(key.ClusterID(customObject))
+			if err != nil {
+				return microerror.Mask(err)
+			}
+
+			return nil
+		})
+
+		err = g.Wait()
 		if err != nil {
 			return nil, microerror.Mask(err)
 		}
 	}
 
 	output := map[string]BucketObjectState{}
-
 	{
-		b, err := r.cloudConfig.NewMasterTemplate(ctx, customObject, clusterCerts, clusterKeys)
+		g := &errgroup.Group{}
+		m := sync.Mutex{}
+
+		g.Go(func() error {
+			b, err := r.cloudConfig.NewMasterTemplate(ctx, customObject, clusterCerts, clusterKeys)
+			if err != nil {
+				return microerror.Mask(err)
+			}
+
+			m.Lock()
+			k := key.BucketObjectName(customObject, key.KindMaster)
+			output[k] = BucketObjectState{
+				Bucket: key.BucketName(customObject, cc.Status.Cluster.AWSAccount.ID),
+				Body:   b,
+				Key:    k,
+			}
+			m.Unlock()
+
+			return nil
+		})
+
+		g.Go(func() error {
+			b, err := r.cloudConfig.NewWorkerTemplate(ctx, customObject, clusterCerts)
+			if err != nil {
+				return microerror.Mask(err)
+			}
+
+			m.Lock()
+			k := key.BucketObjectName(customObject, key.KindWorker)
+			output[k] = BucketObjectState{
+				Bucket: key.BucketName(customObject, cc.Status.Cluster.AWSAccount.ID),
+				Body:   b,
+				Key:    k,
+			}
+			m.Unlock()
+
+			return nil
+		})
+
+		err = g.Wait()
 		if err != nil {
 			return nil, microerror.Mask(err)
-		}
-		k := key.BucketObjectName(customObject, key.KindMaster)
-		output[k] = BucketObjectState{
-			Bucket: key.BucketName(customObject, cc.Status.Cluster.AWSAccount.ID),
-			Body:   b,
-			Key:    k,
-		}
-	}
-
-	{
-		b, err := r.cloudConfig.NewWorkerTemplate(ctx, customObject, clusterCerts)
-		if err != nil {
-			return nil, microerror.Mask(err)
-		}
-		k := key.BucketObjectName(customObject, key.KindWorker)
-		output[k] = BucketObjectState{
-			Bucket: key.BucketName(customObject, cc.Status.Cluster.AWSAccount.ID),
-			Body:   b,
-			Key:    k,
 		}
 	}
 
