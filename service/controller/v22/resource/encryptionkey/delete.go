cda79d8d1e354aebce9800485c1349e99fead510
Tim Schindler <xh3b4sd@gmail.com>
2019-02-01 13:26:19 +0100

    remove encryption key fetching from delete event

--- service/controller/v22/resource/encryptionkey/delete.go
+++ service/controller/v22/resource/encryptionkey/delete.go
@@ -5,7 +5,6 @@ import (
 
 	"github.com/giantswarm/microerror"
 
-	"github.com/giantswarm/aws-operator/service/controller/v22/controllercontext"
 	"github.com/giantswarm/aws-operator/service/controller/v22/key"
 )
 
@@ -19,21 +18,6 @@ func (r *Resource) EnsureDeleted(ctx context.Context, obj interface{}) error {
 	if err != nil {
 		return microerror.Mask(err)
 	}
-
-	encryptionKey, err := r.encrypter.EncryptionKey(ctx, customObject)
-	if r.encrypter.IsKeyNotFound(err) {
-		// We can get here during deletion, if the key is already deleted we can
-		// safely exit.
-		return nil
-	} else if err != nil {
-		return microerror.Mask(err)
-	}
-
-	cc, err := controllercontext.FromContext(ctx)
-	if err != nil {
-		return microerror.Mask(err)
-	}
-	cc.Status.Cluster.EncryptionKey = encryptionKey
 
 	return nil
 }
