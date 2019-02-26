f278356baa33a9ceffa415b1039b1129c1f38686
Tim Schindler <xh3b4sd@gmail.com>
2019-02-26 11:15:49 +0100

    wire v24 (#1419)

--- service/controller/cluster.go
+++ service/controller/cluster.go
@@ -33,6 +33,9 @@ import (
 	"github.com/giantswarm/aws-operator/service/controller/v23"
 	v23adapter "github.com/giantswarm/aws-operator/service/controller/v23/adapter"
 	v23cloudconfig "github.com/giantswarm/aws-operator/service/controller/v23/cloudconfig"
+	"github.com/giantswarm/aws-operator/service/controller/v24"
+	v24adapter "github.com/giantswarm/aws-operator/service/controller/v24/adapter"
+	v24cloudconfig "github.com/giantswarm/aws-operator/service/controller/v24/cloudconfig"
 	"github.com/giantswarm/certs"
 	"github.com/giantswarm/legacycerts/legacy"
 	"github.com/giantswarm/microerror"
@@ -681,6 +684,55 @@ func newClusterResourceSets(config ClusterConfig) ([]*controller.ResourceSet, er
 		}
 	}
 
+	var resourceSetV24 *controller.ResourceSet
+	{
+		c := v24.ClusterResourceSetConfig{
+			CertsSearcher:      certsSearcher,
+			G8sClient:          config.G8sClient,
+			HostAWSConfig:      hostAWSConfig,
+			HostAWSClients:     awsHostClients,
+			K8sClient:          config.K8sClient,
+			Logger:             config.Logger,
+			RandomKeysSearcher: randomKeysSearcher,
+
+			AccessLogsExpiration:       config.AccessLogsExpiration,
+			AdvancedMonitoringEC2:      config.AdvancedMonitoringEC2,
+			DeleteLoggingBucket:        config.DeleteLoggingBucket,
+			EncrypterBackend:           config.EncrypterBackend,
+			GuestAvailabilityZones:     config.GuestAWSConfig.AvailabilityZones,
+			GuestPrivateSubnetMaskBits: config.GuestPrivateSubnetMaskBits,
+			GuestPublicSubnetMaskBits:  config.GuestPublicSubnetMaskBits,
+			GuestSubnetMaskBits:        config.GuestSubnetMaskBits,
+			GuestUpdateEnabled:         config.GuestUpdateEnabled,
+			PodInfraContainerImage:     config.PodInfraContainerImage,
+			Route53Enabled:             config.Route53Enabled,
+			IgnitionPath:               config.IgnitionPath,
+			IncludeTags:                config.IncludeTags,
+			InstallationName:           config.InstallationName,
+			IPAMNetworkRange:           config.IPAMNetworkRange,
+			OIDC: v24cloudconfig.OIDCConfig{
+				ClientID:      config.OIDC.ClientID,
+				IssuerURL:     config.OIDC.IssuerURL,
+				UsernameClaim: config.OIDC.UsernameClaim,
+				GroupsClaim:   config.OIDC.GroupsClaim,
+			},
+			APIWhitelist: v24adapter.APIWhitelist{
+				Enabled:    config.APIWhitelist.Enabled,
+				SubnetList: config.APIWhitelist.SubnetList,
+			},
+			ProjectName:       config.ProjectName,
+			PublicRouteTables: config.PublicRouteTables,
+			RegistryDomain:    config.RegistryDomain,
+			SSOPublicKey:      config.SSOPublicKey,
+			VaultAddress:      config.VaultAddress,
+		}
+
+		resourceSetV24, err = v24.NewClusterResourceSet(c)
+		if err != nil {
+			return nil, microerror.Mask(err)
+		}
+	}
+
 	resourceSets := []*controller.ResourceSet{
 		resourceSetV17Patch1,
 		resourceSetV17Patch2,
@@ -691,6 +743,7 @@ func newClusterResourceSets(config ClusterConfig) ([]*controller.ResourceSet, er
 		resourceSetV21patch1,
 		resourceSetV22,
 		resourceSetV23,
+		resourceSetV24,
 	}
 
 	return resourceSets, nil
--- service/controller/v24/version_bundle.go
+++ service/controller/v24/version_bundle.go
@@ -8,23 +8,8 @@ func VersionBundle() versionbundle.Bundle {
 	return versionbundle.Bundle{
 		Changelogs: []versionbundle.Changelog{
 			{
-				Component:   "kubernetes",
-				Description: "Update kubernetes to 1.13.3. More info here: https://github.com/kubernetes/kubernetes/blob/master/CHANGELOG-1.13.md",
-				Kind:        versionbundle.KindChanged,
-			},
-			{
-				Component:   "etcd",
-				Description: "Update etcd to 3.3.12",
-				Kind:        versionbundle.KindChanged,
-			},
-			{
-				Component:   "calico",
-				Description: "Update calico to 3.5.1",
-				Kind:        versionbundle.KindChanged,
-			},
-			{
-				Component:   "kubernetes",
-				Description: "Improved Audit policy to reduce the amount of Audit logs (high-volume and low-risk).",
+				Component:   "TODO",
+				Description: "TODO",
 				Kind:        versionbundle.KindChanged,
 			},
 		},
@@ -51,6 +36,6 @@ func VersionBundle() versionbundle.Bundle {
 			},
 		},
 		Name:    "aws-operator",
-		Version: "4.7.0",
+		Version: "4.8.0",
 	}
 }
