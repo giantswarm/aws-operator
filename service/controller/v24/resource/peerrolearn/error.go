9736b7f40360506fc1be2ab013aa03f2e5849424
Tim Schindler <xh3b4sd@gmail.com>
2019-03-11 21:53:05 +0100

    fix error handling

--- service/controller/v24/resource/peerrolearn/error.go
+++ service/controller/v24/resource/peerrolearn/error.go
@@ -1,6 +1,9 @@
 package peerrolearn
 
 import (
+	"fmt"
+
+	"github.com/aws/aws-sdk-go/aws/awserr"
 	"github.com/giantswarm/microerror"
 )
 
@@ -19,5 +22,23 @@ var notFoundError = &microerror.Error{
 
 // IsNotFound asserts notFoundError.
 func IsNotFound(err error) bool {
-	return microerror.Cause(err) == notFoundError
+	c := microerror.Cause(err)
+
+	aerr, ok := c.(awserr.Error)
+	fmt.Printf("%#v\n", aerr)
+	if ok {
+		fmt.Printf("%#v\n", aerr.Code())
+		fmt.Printf("%#v\n", aerr.Error())
+		fmt.Printf("%#v\n", aerr.Message())
+		fmt.Printf("%#v\n", aerr.OrigErr())
+		if aerr.Code() == "NoSuchEntity" {
+			return true
+		}
+	}
+
+	if c == notFoundError {
+		return true
+	}
+
+	return false
 }
