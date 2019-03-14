c7480a877badadd26b8c58a6eb0cbb9ea27c9d98
Tim Schindler <xh3b4sd@gmail.com>
2019-03-14 11:15:09 +0100

    fix indentation in templates

--- service/controller/v25/resource/tcdp/template/template_main_security_groups.go
+++ service/controller/v25/resource/tcdp/template/template_main_security_groups.go
@@ -9,49 +9,49 @@ const TemplateMainSecurityGroups = `
       VpcId: !Ref VPC
       SecurityGroupIngress:
 
-			# Allow traffic from control plane CIDR to 22 for SSH access.
+      # Allow traffic from control plane CIDR to 22 for SSH access.
       -
         IpProtocol: tcp
         FromPort: 22
         ToPort: 22
         CidrIp: {{ .SecurityGroups.ControlPlane.VPC.CIDR }}
 
-			# Allow traffic from control plane CIDR to 4194 for cadvisor scraping.
+      # Allow traffic from control plane CIDR to 4194 for cadvisor scraping.
       -
         IpProtocol: tcp
         FromPort: 4194
         ToPort: 4194
         CidrIp: {{ .SecurityGroups.ControlPlane.VPC.CIDR }}
 
-			# Allow traffic from control plane CIDR to 10250 for kubelet scraping.
+      # Allow traffic from control plane CIDR to 10250 for kubelet scraping.
       -
         IpProtocol: tcp
         FromPort: 10250
         ToPort: 10250
         CidrIp: {{ .SecurityGroups.ControlPlane.VPC.CIDR }}
 
-			# Allow traffic from control plane CIDR to 10300 for node-exporter scraping.
+      # Allow traffic from control plane CIDR to 10300 for node-exporter scraping.
       -
         IpProtocol: tcp
         FromPort: 10300
         ToPort: 10300
         CidrIp: {{ .SecurityGroups.ControlPlane.VPC.CIDR }}
 
-			# Allow traffic from control plane CIDR to 10301 for kube-state-metrics scraping.
+      # Allow traffic from control plane CIDR to 10301 for kube-state-metrics scraping.
       -
         IpProtocol: tcp
         FromPort: 10301
         ToPort: 10301
         CidrIp: {{ .SecurityGroups.ControlPlane.VPC.CIDR }}
 
-			# Allow traffic from the ingress security group to 443 for ingress-controller.
+      # Allow traffic from the ingress security group to 443 for ingress-controller.
       -
         IpProtocol: tcp
         FromPort: 30011
         ToPort: 30011
         SourceSecurityGroupId: !Ref IngressSecurityGroup
 
-			# Allow traffic from the ingress security group to 80 for ingress-controller.
+      # Allow traffic from the ingress security group to 80 for ingress-controller.
       -
         IpProtocol: tcp
         FromPort: 30010
