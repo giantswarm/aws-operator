a8ff19224b31b14b8c3ab670ac9e0a099e8f6c6c
Ross Fairbanks <rossf7@users.noreply.github.com>
2019-03-15 10:31:37 +0100

    Add kubeconfig support (#1491)
    * vendor: updated operatorkit
    
    * Flags changes
    
    * fmt
    
    * vendor: updated deps

--- flag/service/kubernetes/tls/tls.go
+++
@@ -1,7 +0,0 @@
-package tls
-
-type TLS struct {
-	CAFile  string
-	CrtFile string
-	KeyFile string
-}
--- service/service.go
+++ service/service.go
@@ -73,9 +73,10 @@ func New(config Config) (*Service, error) {
 		c := k8srestconfig.Config{
 			Logger: config.Logger,
 
-			Address:   config.Viper.GetString(config.Flag.Service.Kubernetes.Address),
-			InCluster: config.Viper.GetBool(config.Flag.Service.Kubernetes.InCluster),
-			TLS: k8srestconfig.TLSClientConfig{
+			Address:    config.Viper.GetString(config.Flag.Service.Kubernetes.Address),
+			InCluster:  config.Viper.GetBool(config.Flag.Service.Kubernetes.InCluster),
+			KubeConfig: config.Viper.GetString(config.Flag.Service.Kubernetes.KubeConfig),
+			TLS: k8srestconfig.ConfigTLS{
 				CAFile:  config.Viper.GetString(config.Flag.Service.Kubernetes.TLS.CAFile),
 				CrtFile: config.Viper.GetString(config.Flag.Service.Kubernetes.TLS.CrtFile),
 				KeyFile: config.Viper.GetString(config.Flag.Service.Kubernetes.TLS.KeyFile),
