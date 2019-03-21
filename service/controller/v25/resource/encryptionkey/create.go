f5ab503ae9c590c838d57bac335c1481929e4307
Tim Schindler <xh3b4sd@gmail.com>
2019-03-21 16:20:31 +0100

    backoff on initial encryption key creation

--- service/controller/v25/resource/encryptionkey/create.go
+++ service/controller/v25/resource/encryptionkey/create.go
@@ -2,7 +2,9 @@ package encryptionkey
 
 import (
 	"context"
+	"time"
 
+	"github.com/giantswarm/backoff"
 	"github.com/giantswarm/microerror"
 
 	"github.com/giantswarm/aws-operator/service/controller/v25/controllercontext"
@@ -10,25 +12,38 @@ import (
 )
 
 func (r *Resource) EnsureCreated(ctx context.Context, obj interface{}) error {
-	customObject, err := key.ToCustomObject(obj)
+	cr, err := key.ToCustomObject(obj)
 	if err != nil {
 		return microerror.Mask(err)
 	}
-
-	err = r.encrypter.EnsureCreatedEncryptionKey(ctx, customObject)
-	if err != nil {
-		return microerror.Mask(err)
-	}
-
-	encryptionKey, err := r.encrypter.EncryptionKey(ctx, customObject)
-	if err != nil {
-		return microerror.Mask(err)
-	}
-
 	cc, err := controllercontext.FromContext(ctx)
 	if err != nil {
 		return microerror.Mask(err)
 	}
+
+	err = r.encrypter.EnsureCreatedEncryptionKey(ctx, cr)
+	if err != nil {
+		return microerror.Mask(err)
+	}
+
+	var encryptionKey string
+	{
+		o := func() error {
+			encryptionKey, err = r.encrypter.EncryptionKey(ctx, cr)
+			if err != nil {
+				return microerror.Mask(err)
+			}
+
+			return nil
+		}
+		b := backoff.NewMaxRetries(3, 1*time.Second)
+
+		err := backoff.Retry(o, b)
+		if err != nil {
+			return microerror.Mask(err)
+		}
+	}
+
 	cc.Status.TenantCluster.EncryptionKey = encryptionKey
 
 	return nil
