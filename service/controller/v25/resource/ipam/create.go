039017f9c86500c3da88650a64b2ae9bfb38914d
Ross Fairbanks <rossf7@users.noreply.github.com>
2019-03-22 09:58:40 +0100

    Update service/controller/v25/resource/ipam/create.go
    Co-Authored-By: xh3b4sd <xh3b4sd@gmail.com>

--- service/controller/v25/resource/ipam/create.go
+++ service/controller/v25/resource/ipam/create.go
@@ -47,7 +47,7 @@ func (r *Resource) EnsureCreated(ctx context.Context, obj interface{}) error {
 		}
 		cr = *newObj
 
-		r.logger.LogCtx(ctx, "level", "debug", "message", "fetchted latest version of custom resource")
+		r.logger.LogCtx(ctx, "level", "debug", "message", "fetched latest version of custom resource")
 	}
 
 	r.logger.LogCtx(ctx, "level", "debug", "message", "finding out if subnet needs to be allocated for cluster")
