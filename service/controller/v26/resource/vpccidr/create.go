f79732fa29c39abb78e8e72b8b2a7df3b7ee8ab2
Ross Fairbanks <rossf7@users.noreply.github.com>
2019-04-15 14:57:51 +0200

    v26: copy v25 (#1539)

---
+++ service/controller/v26/resource/vpccidr/create.go
@@ -0,0 +1,23 @@
+package vpccidr
+
+import (
+	"context"
+
+	"github.com/giantswarm/microerror"
+
+	"github.com/giantswarm/aws-operator/service/controller/v25/key"
+)
+
+func (r *Resource) EnsureCreated(ctx context.Context, obj interface{}) error {
+	cr, err := key.ToCustomObject(obj)
+	if err != nil {
+		return microerror.Mask(err)
+	}
+
+	err = r.addVPCCIDRToContext(ctx, cr)
+	if err != nil {
+		return microerror.Mask(err)
+	}
+
+	return nil
+}
---
+++ service/controller/v26/version_bundle.go
@@ -0,0 +1,46 @@
+package v25
+
+import (
+	"github.com/giantswarm/versionbundle"
+)
+
+func VersionBundle() versionbundle.Bundle {
+	return versionbundle.Bundle{
+		Changelogs: []versionbundle.Changelog{
+			{
+				Component:   "cloudconfig",
+				Description: "Pin calico-kube-controllers to master.",
+				Kind:        versionbundle.KindChanged,
+			},
+			{
+				Component:   "kubernetes",
+				Description: "Mount /var/lib/kubelet directory in an EBS Volume.",
+				Kind:        versionbundle.KindAdded,
+			},
+		},
+		Components: []versionbundle.Component{
+			{
+				Name:    "calico",
+				Version: "3.5.1",
+			},
+			{
+				Name:    "containerlinux",
+				Version: "2023.4.0",
+			},
+			{
+				Name:    "docker",
+				Version: "18.06.1",
+			},
+			{
+				Name:    "etcd",
+				Version: "3.3.12",
+			},
+			{
+				Name:    "kubernetes",
+				Version: "1.13.4",
+			},
+		},
+		Name:    "aws-operator",
+		Version: "4.9.0",
+	}
+}
