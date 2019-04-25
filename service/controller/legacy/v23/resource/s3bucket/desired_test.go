d8a1c2ea9d685d1c65931fa7b329f4cd5b008a83
Tim Schindler <tim@giantswarm.io>
2019-04-25 15:28:14 +0200

    change controller structure (#1544)

---
+++ service/controller/legacy/v23/resource/s3bucket/desired_test.go
@@ -0,0 +1,85 @@
+package s3bucket
+
+import (
+	"context"
+	"testing"
+
+	"github.com/giantswarm/apiextensions/pkg/apis/provider/v1alpha1"
+	"github.com/giantswarm/micrologger/microloggertest"
+
+	"github.com/giantswarm/aws-operator/service/controller/legacy/v23/controllercontext"
+)
+
+func Test_Resource_S3Bucket_GetDesiredState(t *testing.T) {
+	t.Parallel()
+	testCases := []struct {
+		obj           interface{}
+		expectedNames []string
+		description   string
+	}{
+		{
+			description: "Get bucket name from custom object.",
+			obj: &v1alpha1.AWSConfig{
+				Spec: v1alpha1.AWSConfigSpec{
+					Cluster: v1alpha1.Cluster{
+						ID: "5xchu",
+					},
+				},
+			},
+			expectedNames: []string{
+				"5xchu-g8s-access-logs",
+				"myaccountid-g8s-5xchu",
+			},
+		},
+	}
+
+	var err error
+
+	var newResource *Resource
+	{
+		c := Config{
+			Logger:           microloggertest.New(),
+			InstallationName: "test-install",
+		}
+
+		newResource, err = New(c)
+		if err != nil {
+			t.Fatal("expected", nil, "got", err)
+		}
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.description, func(t *testing.T) {
+			ctx := controllercontext.NewContext(context.Background(), testContextWithAccountID("myaccountid"))
+
+			result, err := newResource.GetDesiredState(ctx, tc.obj)
+			if err != nil {
+				t.Fatalf("expected '%v' got '%#v'", nil, err)
+			}
+
+			desiredBuckets, ok := result.([]BucketState)
+			if !ok {
+				t.Fatalf("case expected '%T', got '%T'", desiredBuckets, result)
+			}
+
+			// Order should be respected in the slice returned (always delivery log bucket first)
+			for key, desiredBucket := range desiredBuckets {
+				if tc.expectedNames[key] != desiredBucket.Name {
+					t.Fatalf("expected bucket name %q got %q", tc.expectedNames[key], desiredBucket.Name)
+				}
+			}
+		})
+	}
+}
+
+func testContextWithAccountID(id string) controllercontext.Context {
+	return controllercontext.Context{
+		Status: controllercontext.Status{
+			Cluster: controllercontext.Cluster{
+				AWSAccount: controllercontext.ClusterAWSAccount{
+					ID: id,
+				},
+			},
+		},
+	}
+}
---
+++ service/controller/legacy/v24/resource/kmskeyarn/delete.go
@@ -0,0 +1,33 @@
+package kmskeyarn
+
+import (
+	"context"
+
+	"github.com/giantswarm/microerror"
+
+	"github.com/giantswarm/aws-operator/service/controller/legacy/v24/key"
+)
+
+func (r *Resource) EnsureDeleted(ctx context.Context, obj interface{}) error {
+	cr, err := key.ToCustomObject(obj)
+	if err != nil {
+		return microerror.Mask(err)
+	}
+
+	{
+		r.logger.LogCtx(ctx, "level", "debug", "message", "finding tenant cluster kms key arn")
+
+		err = r.addKMSKeyARNToContext(ctx, cr)
+		if IsNotFound(err) {
+			r.logger.LogCtx(ctx, "level", "debug", "message", "did not find tenant cluster kms key arn")
+			r.logger.LogCtx(ctx, "level", "debug", "message", "canceling resource")
+			return nil
+		} else if err != nil {
+			return microerror.Mask(err)
+		}
+
+		r.logger.LogCtx(ctx, "level", "debug", "message", "found tenant cluster kms key arn")
+	}
+
+	return nil
+}
