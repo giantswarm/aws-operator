bdf19551194d7c6fefed5d7d4fd7b91f993320bd
Tim Schindler <tim@giantswarm.io>
2019-04-30 16:45:20 +0200

    use new key function (#1575)

--- service/controller/clusterapi/v27/key/common.go
+++ service/controller/clusterapi/v27/key/common.go
@@ -1,14 +1,5 @@
 package key
 
-import (
-	"k8s.io/apimachinery/pkg/api/meta"
-)
-
-func IsDeleted(v interface{}) bool {
-	m, err := meta.Accessor(v)
-	if err != nil {
-		panic(err)
-	}
-
-	return m.GetDeletionTimestamp() != nil
+func IsDeleted(getter DeletionTimestampGetter) bool {
+	return getter.GetDeletionTimestamp() != nil
 }
