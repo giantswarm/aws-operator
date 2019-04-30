67af45b1909a0238276f68d25c27c6f20f501a07
Marcel Mueller <marcel.mueller1@rwth-aachen.de>
2019-04-30 09:36:36 +0200

    Copy PR for v27

---
+++ service/controller/clusterapi/v27/resource/cpi/template/params_main.go
@@ -0,0 +1,6 @@
+package template
+
+// ParamsMain is the data structure for the Control Plane Initializer template.
+type ParamsMain struct {
+	IAMRoles *ParamsMainIAMRoles
+}
---
+++ service/controller/clusterapi/v27/resource/peerrolearn/resource.go
@@ -0,0 +1,68 @@
+package peerrolearn
+
+import (
+	"context"
+
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/service/iam"
+	"github.com/giantswarm/apiextensions/pkg/apis/provider/v1alpha1"
+	"github.com/giantswarm/microerror"
+	"github.com/giantswarm/micrologger"
+
+	"github.com/giantswarm/aws-operator/service/controller/clusterapi/v26/controllercontext"
+	"github.com/giantswarm/aws-operator/service/controller/clusterapi/v26/key"
+)
+
+const (
+	Name = "peerrolearnv26"
+)
+
+type Config struct {
+	Logger micrologger.Logger
+}
+
+type Resource struct {
+	logger micrologger.Logger
+}
+
+func New(config Config) (*Resource, error) {
+	if config.Logger == nil {
+		return nil, microerror.Maskf(invalidConfigError, "%T.Logger must not be empty", config)
+	}
+
+	r := &Resource{
+		logger: config.Logger,
+	}
+
+	return r, nil
+}
+
+func (r *Resource) Name() string {
+	return Name
+}
+
+func (r *Resource) addPeerRoleARNToContext(ctx context.Context, cr v1alpha1.AWSConfig) error {
+	cc, err := controllercontext.FromContext(ctx)
+	if err != nil {
+		return microerror.Mask(err)
+	}
+
+	var peerRoleArn string
+	{
+		i := &iam.GetRoleInput{
+			RoleName: aws.String(key.PeerAccessRoleName(cr)),
+		}
+		o, err := cc.Client.ControlPlane.AWS.IAM.GetRole(i)
+		if IsNotFound(err) {
+			return microerror.Maskf(notFoundError, key.PeerAccessRoleName(cr))
+		} else if err != nil {
+			return microerror.Mask(err)
+		}
+
+		peerRoleArn = *o.Role.Arn
+	}
+
+	cc.Status.ControlPlane.PeerRole.ARN = peerRoleArn
+
+	return nil
+}
