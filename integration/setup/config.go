d012aeeec181b72ee6d8a82879353b9c8fd0e625
Theo Brigitte <theo.brigitte@gmail.com>
2019-05-03 14:26:24 +0200

    fix k8s.SetupConfig

--- integration/setup/config.go
+++ integration/setup/config.go
@@ -4,6 +4,7 @@ package setup
 
 import (
 	"github.com/giantswarm/e2e-harness/pkg/framework"
+	"github.com/giantswarm/e2e-harness/pkg/harness"
 	"github.com/giantswarm/e2e-harness/pkg/release"
 	e2eclientsaws "github.com/giantswarm/e2eclients/aws"
 	"github.com/giantswarm/e2esetup/k8s"
@@ -86,11 +87,25 @@ func NewConfig() (Config, error) {
 		}
 	}
 
+	var cpK8sClients *k8s.Clients
+	{
+		c := k8s.ClientsConfig{
+			Logger: logger,
+
+			KubeConfigPath: harness.DefaultKubeConfig,
+		}
+
+		cpK8sClients, err = k8s.NewClients(c)
+		if err != nil {
+			return Config{}, microerror.Mask(err)
+		}
+	}
+
 	var k8sSetup *k8s.Setup
 	{
 		c := k8s.SetupConfig{
-			K8sClient: host.K8sClient(),
-			Logger:    logger,
+			Clients: cpK8sClients,
+			Logger:  logger,
 		}
 
 		k8sSetup, err = k8s.NewSetup(c)
