a11534ca9d33555f51ce857c05df91da33dcd138
Tim Schindler <tim@giantswarm.io>
2019-05-09 11:09:40 +0200

    move key funcs for worker (#1615)

--- service/controller/clusterapi/v27/key/machine_deployment.go
+++ service/controller/clusterapi/v27/key/machine_deployment.go
@@ -1,9 +1,23 @@
 package key
 
 import (
+	"strconv"
+
 	"sigs.k8s.io/cluster-api/pkg/apis/cluster/v1alpha1"
 )
 
-func WorkerInstanceType(machineDeployment v1alpha1.MachineDeployment) string {
-	return machineDeploymentProviderSpec(machineDeployment).Provider.Worker.InstanceType
+func WorkerInstanceType(cr v1alpha1.MachineDeployment) string {
+	return machineDeploymentProviderSpec(cr).Provider.Worker.InstanceType
+}
+
+func WorkerScalingMax(cr v1alpha1.MachineDeployment) int {
+	return machineDeploymentProviderSpec(cr).NodePool.Scaling.Max
+}
+
+func WorkerScalingMin(cr v1alpha1.MachineDeployment) int {
+	return machineDeploymentProviderSpec(cr).NodePool.Scaling.Min
+}
+
+func WorkerDockerVolumeSizeGB(cr v1alpha1.MachineDeployment) string {
+	return strconv.Itoa(machineDeploymentProviderSpec(cr).NodePool.Machine.DockerVolumeSizeGB)
 }
