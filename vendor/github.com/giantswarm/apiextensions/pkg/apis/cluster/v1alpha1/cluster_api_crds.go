dd9a38029906759765885381976e1f227f65c6c8
Tuomas Mäkinen <tuomas@giantswarm.io>
2019-05-17 12:24:04 +0300

    Update apiextensions

--- vendor/github.com/giantswarm/apiextensions/pkg/apis/cluster/v1alpha1/cluster_api_crds.go
+++ vendor/github.com/giantswarm/apiextensions/pkg/apis/cluster/v1alpha1/cluster_api_crds.go
@@ -91,6 +91,7 @@ func NewMachineDeploymentCRD() *apiextensionsv1beta1.CustomResourceDefinition {
 			Subresources: &apiextensionsv1beta1.CustomResourceSubresources{
 				Status: &apiextensionsv1beta1.CustomResourceSubresourceStatus{},
 			},
+			Version: "v1alpha1",
 		},
 	}
 }
