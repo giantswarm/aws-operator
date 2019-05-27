92a215942ded4ca93cd3a96b740377ff1f3f67b2
Tim Schindler <xh3b4sd@gmail.com>
2019-05-27 10:46:40 +0200

    update vendor deps

--- vendor/github.com/giantswarm/e2esetup/privaterepo/ssh_user_list.go
+++ vendor/github.com/giantswarm/e2esetup/privaterepo/ssh_user_list.go
@@ -1,6 +1,11 @@
 package privaterepo
 
 import (
+	"bytes"
+	"html/template"
+	"net/url"
+	"time"
+
 	"github.com/giantswarm/microerror"
 	"github.com/giantswarm/valuemodifier/path"
 )
@@ -8,10 +13,58 @@ import (
 func ContentToSSHUserList(content string) (string, error) {
 	var err error
 
+	var rendered string
+	{
+		tmpl, err := template.New("main").Parse(content)
+		if err != nil {
+			return "", microerror.Mask(err)
+		}
+
+		v := struct {
+			AWS struct {
+				Region      string
+				HostCluster struct {
+					Account          string
+					AdminRoleARN     string
+					CloudtrailBucket string
+					GuardDuty        bool
+				}
+				GuestCluster struct {
+					Account          string
+					AdminRoleARN     string
+					CloudtrailBucket string
+					GuardDuty        bool
+				}
+			}
+			Active           bool
+			Base             string
+			Codename         string
+			Created          time.Time
+			Customer         string
+			SolutionEngineer string
+			Pipeline         string
+			Provider         string
+			SSHUser          string
+			SSHTunnelNeeded  bool
+			Services         map[string]*url.URL
+			Jumphosts        map[string]*url.URL
+			Machines         map[string]*url.URL
+			Updated          time.Time
+		}{}
+
+		var w bytes.Buffer
+		err = tmpl.Execute(&w, v)
+		if err != nil {
+			return "", microerror.Mask(err)
+		}
+
+		rendered = w.String()
+	}
+
 	var pathGetter *path.Service
 	{
 		c := path.Config{
-			InputBytes: []byte(content),
+			InputBytes: []byte(rendered),
 			Separator:  ".",
 		}
 
