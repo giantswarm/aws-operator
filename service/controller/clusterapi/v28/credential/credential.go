8c03bcbf77ba066a53307338fb3a3e3a3d0d655d
Tim Schindler <tim@giantswarm.io>
2019-06-03 15:00:07 +0200

    move credential package to new key package (#1673)

--- service/controller/clusterapi/v28/credential/credential.go
+++ service/controller/clusterapi/v28/credential/credential.go
@@ -2,16 +2,22 @@ package credential
 
 import (
 	"github.com/giantswarm/microerror"
-	v1 "k8s.io/api/core/v1"
-	apismetav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/client-go/kubernetes"
 
-	"github.com/giantswarm/aws-operator/service/controller/clusterapi/v28/legacykey"
+	"github.com/giantswarm/aws-operator/service/controller/clusterapi/v28/key"
 )
 
 const (
-	// awsOperatorArnKey is the key in the Secret under which the ARN for the aws-operator role is held.
-	awsOperatorArnKey = "aws.awsoperator.arn"
+	// AWSOperatorArnKey is the key in the Secret under which the ARN for the
+	// aws-operator role is held.
+	AWSOperatorArnKey = "aws.awsoperator.arn"
+)
+
+const (
+	DefaultName      = "credential-default"
+	DefaultNamespace = "giantswarm"
 )
 
 func GetARN(k8sClient kubernetes.Interface, obj interface{}) (string, error) {
@@ -31,9 +37,7 @@ func GetARN(k8sClient kubernetes.Interface, obj interface{}) (string, error) {
 // GetDefaultARN is used only by the bridgezone resource. It should be removed
 // when the resource is removed.
 func GetDefaultARN(k8sClient kubernetes.Interface) (string, error) {
-	ns := "giantswarm"
-	name := "credential-default"
-	credential, err := k8sClient.CoreV1().Secrets(ns).Get(name, apismetav1.GetOptions{})
+	credential, err := k8sClient.CoreV1().Secrets(DefaultNamespace).Get(DefaultName, metav1.GetOptions{})
 	if err != nil {
 		return "", microerror.Mask(err)
 	}
@@ -46,32 +50,32 @@ func GetDefaultARN(k8sClient kubernetes.Interface) (string, error) {
 	return arn, nil
 }
 
-func getARN(credential *v1.Secret) (string, error) {
-	arn, ok := credential.Data[awsOperatorArnKey]
+func getARN(credential *corev1.Secret) (string, error) {
+	arn, ok := credential.Data[AWSOperatorArnKey]
 	if !ok {
-		return "", microerror.Maskf(arnNotFound, awsOperatorArnKey)
+		return "", microerror.Maskf(arnNotFound, AWSOperatorArnKey)
 	}
 
 	return string(arn), nil
 }
 
-func readCredential(k8sClient kubernetes.Interface, obj interface{}) (*v1.Secret, error) {
-	customObject, err := legacykey.ToCustomObject(obj)
+func readCredential(k8sClient kubernetes.Interface, obj interface{}) (*corev1.Secret, error) {
+	cr, err := key.ToCluster(obj)
 	if err != nil {
 		return nil, microerror.Mask(err)
 	}
 
-	credentialName := legacykey.CredentialName(customObject)
+	credentialName := key.CredentialName(cr)
 	if credentialName == "" {
 		return nil, microerror.Mask(credentialNameEmpty)
 	}
 
-	credentialNamespace := legacykey.CredentialNamespace(customObject)
+	credentialNamespace := key.CredentialNamespace(cr)
 	if credentialName == "" {
 		return nil, microerror.Mask(credentialNamespaceEmpty)
 	}
 
-	credential, err := k8sClient.CoreV1().Secrets(credentialNamespace).Get(credentialName, apismetav1.GetOptions{})
+	credential, err := k8sClient.CoreV1().Secrets(credentialNamespace).Get(credentialName, metav1.GetOptions{})
 	if err != nil {
 		return nil, microerror.Mask(err)
 	}
