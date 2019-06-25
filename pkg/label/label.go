c3d07f8748a88eeede6e3f67532e786c34896d3b
Tim Schindler <tim@giantswarm.io>
2019-06-25 17:35:24 +0200

    use generic release version label for node labels (#1704)

--- pkg/label/label.go
+++ pkg/label/label.go
@@ -7,10 +7,12 @@ const (
 const (
 	Cluster       = "giantswarm.io/cluster"
 	Organization  = "giantswarm.io/organization"
+	Provider      = "giantswarm.io/provider"
 	VersionBundle = "giantswarm.io/version-bundle"
 )
 
 const (
 	MachineDeploymentSubnet = "machine-deployment.giantswarm.io/subnet"
 	OperatorVersion         = "aws-operator.giantswarm.io/version"
+	ReleaseVersion          = "release.giantswarm.io/version"
 )
