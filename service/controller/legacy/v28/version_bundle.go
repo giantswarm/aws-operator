3c8406d5af027b62b5071c640c1c1c3e61f2dcfc
Marcel Mueller <marcel.mueller1@rwth-aachen.de>
2019-07-08 14:08:50 +0200

    Clean unreleased release notes

--- service/controller/legacy/v28/version_bundle.go
+++ service/controller/legacy/v28/version_bundle.go
@@ -9,27 +9,27 @@ func VersionBundle() versionbundle.Bundle {
 		Changelogs: []versionbundle.Changelog{
 			{
 				Component:   "kubernetes",
-				Description: "Update kubernetes to 1.14.3. More info here: https://github.com/kubernetes/kubernetes/blob/master/CHANGELOG-1.14.md",
+				Description: "Update kubernetes to 1.14.3. https://github.com/kubernetes/kubernetes/blob/master/CHANGELOG-1.14.md",
 				Kind:        versionbundle.KindChanged,
 			},
 			{
 				Component:   "calico",
-				Description: "Update calico to 3.7.2. More info here: https://docs.projectcalico.org/v3.7/release-notes/",
+				Description: "Update calico to 3.7.2. https://docs.projectcalico.org/v3.7/release-notes/",
 				Kind:        versionbundle.KindChanged,
 			},
 			{
 				Component:   "containerlinux",
-				Description: "Update to 2079.5.1. More info here: https://github.com/coreos/manifest/releases/tag/v2079.5.1",
+				Description: "Update to 2079.5.1. https://github.com/coreos/manifest/releases/tag/v2079.5.1",
 				Kind:        versionbundle.KindChanged,
 			},
 			{
 				Component:   "etcd",
-				Description: "Update to 3.3.13. More info here: https://github.com/etcd-io/etcd/blob/master/CHANGELOG-3.3.md#v3313-2019-05-02",
+				Description: "Update to 3.3.13. https://github.com/etcd-io/etcd/blob/master/CHANGELOG-3.3.md#v3313-2019-05-02",
 				Kind:        versionbundle.KindChanged,
 			},
 			{
 				Component:   "ignition",
-				Description: "Added name label for default and kube-system namespaces.",
+				Description: "Add name label for default and kube-system namespaces.",
 				Kind:        versionbundle.KindAdded,
 			},
 		},
