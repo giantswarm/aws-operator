e384e37d08b24d6a0d2618b5048401050f3c23ca
Tim Schindler <tim@giantswarm.io>
2019-07-09 17:27:52 +0200

    add missing todo issue (#1745)

--- service/network/network.go
+++ service/network/network.go
@@ -1,4 +1,7 @@
 // Package network is DEPRECATED and can be dropped once v29 is gone. The
 // network package moved to the versioned controller packages and should further
-// be maintained there.
+// be maintained there. See also the TODO issue below.
+//
+//     https://github.com/giantswarm/giantswarm/issues/6439
+//
 package network
