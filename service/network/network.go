0a50d06ca044ae2ae46ef500f386ec558dfca985
Tim Schindler <xh3b4sd@gmail.com>
2019-07-09 16:31:41 +0200

    add deprecation notice

---
+++ service/network/network.go
@@ -0,0 +1,4 @@
+// Package network is DEPRECATED and can be dropped once v29 is gone. The
+// network package moved to the versioned controller packages and should further
+// be maintained there.
+package network
