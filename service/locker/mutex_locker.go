289ad1e82c892ab7b7869da2ca2b2a978797bd23
Tim Schindler <xh3b4sd@gmail.com>
2019-07-15 09:48:55 +0200

    fix method names

--- service/locker/mutex_locker.go
+++ service/locker/mutex_locker.go
@@ -8,25 +8,25 @@ import (
 	"github.com/giantswarm/micrologger"
 )
 
-type MutexLockConfig struct {
+type MutexLockerConfig struct {
 	Logger micrologger.Logger
 }
 
-// MutexLock implements Interface using sync.Mutex. For now we use a shared
-// instance of *MutexLock for all IPAM related activity of network packages in
+// MutexLocker implements Interface using sync.Mutex. For now we use a shared
+// instance of *MutexLocker for all IPAM related activity of network packages in
 // the legacy controllers and ipam resources in the clusterapi controllers.
-type MutexLock struct {
+type MutexLocker struct {
 	logger micrologger.Logger
 
 	mutex sync.Mutex
 }
 
-func New(config MutexLockConfig) (*MutexLock, error) {
+func NewMutexLocker(config MutexLockerConfig) (*MutexLocker, error) {
 	if config.Logger == nil {
 		return nil, microerror.Maskf(invalidConfigError, "%T.Logger must not be empty", config)
 	}
 
-	l := &MutexLock{
+	l := &MutexLocker{
 		logger: config.Logger,
 
 		mutex: sync.Mutex{},
@@ -35,12 +35,12 @@ func New(config MutexLockConfig) (*MutexLock, error) {
 	return l, nil
 }
 
-func (l *MutexLock) Lock(ctx context.Context) error {
+func (l *MutexLocker) Lock(ctx context.Context) error {
 	l.mutex.Lock()
 	return nil
 }
 
-func (l *MutexLock) Unlock(ctx context.Context) error {
+func (l *MutexLocker) Unlock(ctx context.Context) error {
 	l.mutex.Unlock()
 	return nil
 }
