2852b7dd0e25af3dc4305a7710043241d8c7daa6
Marcel Mueller <marcel.mueller1@rwth-aachen.de>
2019-07-16 13:00:43 +0200

    Update godoc

--- service/controller/clusterapi/v29/controllercontext/context.go
+++ service/controller/clusterapi/v29/controllercontext/context.go
@@ -15,13 +15,14 @@ type Context struct {
 	// specific actions.
 	Client ContextClient
 
-	// Spec holds the data of the unmodified current state and is used
-	// to compare against the Status to detect changes in the Context.
+	// Spec holds the desired state we compute in a resource and then keep
+	// inside the context to be used in a different resource.
 	Spec ContextSpec
 
 	// Status holds the data used to communicate between controller's
 	// resources. It can be edited in place as Context is stored as
-	// a pointer within context.Context.
+	// a pointer within context.Context. The content of the status should
+	// reflect the current state.
 	Status ContextStatus
 }
 
