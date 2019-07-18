fa164c394cd31fc22942f1324c465c3ab1a5a6e8
Marcel Mueller <marcel.mueller1@rwth-aachen.de>
2019-07-18 10:51:34 +0200

    change from cluster to machinedeployment

--- service/controller/clusterapi/v29/resource/tcnp/create.go
+++ service/controller/clusterapi/v29/resource/tcnp/create.go
@@ -209,7 +209,7 @@ func (r *Resource) newIAMPolicies(ctx context.Context, cl v1alpha1.Cluster, md v
 			ID: key.MachineDeploymentID(&md),
 		},
 		RegionARN: key.RegionARN(cl),
-		S3Bucket:  key.BucketName(&cl, cc.Status.TenantCluster.AWSAccountID),
+		S3Bucket:  key.BucketName(&md, cc.Status.TenantCluster.AWSAccountID),
 	}
 
 	return iamPolicies, nil
