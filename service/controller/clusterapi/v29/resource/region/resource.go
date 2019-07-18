a110c3ab0600f34d2a7d8ee990f9992f1345b021
Tim Schindler <xh3b4sd@gmail.com>
2019-07-18 21:25:29 +0200

    godoc

--- service/controller/clusterapi/v29/resource/region/resource.go
+++ service/controller/clusterapi/v29/resource/region/resource.go
@@ -1,3 +1,12 @@
+// Package region implements an operatorkit resource that addresses a problem
+// where the tcnp resource would need to fetch the Cluster CR even though the
+// MachineDeployment CR is reconciled. This is only because we need the AWS
+// region to lookup S3 bucket names and EC2 image IDs and the like. So in order
+// to free the tcnp resource from that hustle we implement a separate region
+// resource which does the lookup and puts the region into the controller
+// context. The controller context information are then simply used by the tcnp
+// resource as this is our state of the art primitive for information
+// distribution within a controller's reconciliation.
 package region
 
 import (
