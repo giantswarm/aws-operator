d3a8261066de33bd9e7f6ecdf56043dc2a0f79b2
Tim Schindler <xh3b4sd@gmail.com>
2019-07-22 11:30:45 +0200

    drop IsExecutionFailed error matcher

--- service/controller/clusterapi/v29/resource/cpf/error.go
+++ service/controller/clusterapi/v29/resource/cpf/error.go
@@ -7,6 +7,18 @@ import (
 	"github.com/giantswarm/microerror"
 )
 
+// executionFailedError is an error type for situations where Resource execution
+// cannot continue and must always fall back to operatorkit.
+//
+// This error should never be matched against and therefore there is no matcher
+// implement. For further information see:
+//
+//     https://github.com/giantswarm/fmt/blob/master/go/errors.md#matching-errors
+//
+var executionFailedError = &microerror.Error{
+	Kind: "executionFailedError",
+}
+
 var deleteInProgressError = &microerror.Error{
 	Kind: "deleteInProgressError",
 }
@@ -28,15 +40,6 @@ func IsDeleteInProgress(err error) bool {
 	}
 
 	return false
-}
-
-var executionFailedError = &microerror.Error{
-	Kind: "executionFailedError",
-}
-
-// IsExecutionFailed asserts executionFailedError.
-func IsExecutionFailed(err error) bool {
-	return microerror.Cause(err) == executionFailedError
 }
 
 var invalidConfigError = &microerror.Error{
--- service/controller/legacy/v29/resource/vpccidr/error.go
+++ service/controller/legacy/v29/resource/vpccidr/error.go
@@ -4,13 +4,16 @@ import (
 	"github.com/giantswarm/microerror"
 )
 
+// executionFailedError is an error type for situations where Resource execution
+// cannot continue and must always fall back to operatorkit.
+//
+// This error should never be matched against and therefore there is no matcher
+// implement. For further information see:
+//
+//     https://github.com/giantswarm/fmt/blob/master/go/errors.md#matching-errors
+//
 var executionFailedError = &microerror.Error{
 	Kind: "executionFailedError",
-}
-
-// IsExecutionFailed asserts executionFailedError.
-func IsExecutionFailed(err error) bool {
-	return microerror.Cause(err) == executionFailedError
 }
 
 var invalidConfigError = &microerror.Error{
