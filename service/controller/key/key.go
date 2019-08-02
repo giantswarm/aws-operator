824b9dbd08a1013a107ed785c2f076f7b857f3fc
Paweł Kopiczko <kopiczko@gmail.com>
2019-08-02 13:51:07 +0100

    remove debug

--- service/controller/key/key.go
+++ service/controller/key/key.go
@@ -1,21 +1,11 @@
 package key
 
 import (
-	"fmt"
-
 	"github.com/giantswarm/aws-operator/pkg/label"
 	"github.com/giantswarm/aws-operator/pkg/project"
 )
 
 func VersionLabelSelector(enabled bool, overridenVersion string) string {
-	s := pawelVersionLabelSelector(enabled, overridenVersion)
-
-	fmt.Printf("pawel ******* enabled=%#v overridenVersion=%#v out=%#v\n", enabled, overridenVersion, s)
-
-	return s
-}
-
-func pawelVersionLabelSelector(enabled bool, overridenVersion string) string {
 	if !enabled {
 		return ""
 	}
