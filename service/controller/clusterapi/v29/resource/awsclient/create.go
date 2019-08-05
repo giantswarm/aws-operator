f5a97e6a3d2b59580ccf72171a89c5024dc924eb
Tim Schindler <xh3b4sd@gmail.com>
2019-08-05 16:30:46 +0200

    cancel reconciliation when clients are not there

--- service/controller/clusterapi/v29/resource/awsclient/create.go
+++ service/controller/clusterapi/v29/resource/awsclient/create.go
@@ -4,13 +4,15 @@ import (
 	"context"
 
 	"github.com/giantswarm/microerror"
+	"github.com/giantswarm/operatorkit/controller/context/reconciliationcanceledcontext"
 )
 
 func (r *Resource) EnsureCreated(ctx context.Context, obj interface{}) error {
 	cr, err := r.toClusterFunc(obj)
 	if IsNotFound(err) {
 		r.logger.LogCtx(ctx, "level", "debug", "message", "cluster cr not yet availabile")
-		r.logger.LogCtx(ctx, "level", "debug", "message", "canceling resource")
+		r.logger.LogCtx(ctx, "level", "debug", "message", "canceling reconciliation")
+		reconciliationcanceledcontext.SetCanceled(ctx)
 
 		return nil
 	} else if err != nil {
