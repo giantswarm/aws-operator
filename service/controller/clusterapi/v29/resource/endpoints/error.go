55d88111605559af2da43c461fe9e2eb82093f22
Tim Schindler <xh3b4sd@gmail.com>
2019-08-06 16:24:56 +0200

    align ec2 instance management between resources

--- service/controller/clusterapi/v29/resource/endpoints/error.go
+++ service/controller/clusterapi/v29/resource/endpoints/error.go
@@ -2,6 +2,18 @@ package endpoints
 
 import "github.com/giantswarm/microerror"
 
+// executionFailedError is an error type for situations where Resource execution
+// cannot continue and must always fall back to operatorkit.
+//
+// This error should never be matched against and therefore there is no matcher
+// implement. For further information see:
+//
+//     https://github.com/giantswarm/fmt/blob/master/go/errors.md#matching-errors
+//
+var executionFailedError = &microerror.Error{
+	Kind: "executionFailedError",
+}
+
 var invalidConfigError = &microerror.Error{
 	Kind: "invalidConfigError",
 }
@@ -18,15 +30,6 @@ var notFoundError = &microerror.Error{
 // IsNotFound asserts notFoundError.
 func IsNotFound(err error) bool {
 	return microerror.Cause(err) == notFoundError
-}
-
-var tooManyResultsError = &microerror.Error{
-	Kind: "tooManyResultsError",
-}
-
-// IsTooManyResults asserts tooManyResultsError.
-func IsTooManyResults(err error) bool {
-	return microerror.Cause(err) == tooManyResultsError
 }
 
 var wrongTypeError = &microerror.Error{
