d421b217fa2f1caa4809eff019405383470908e5
Tim Schindler <xh3b4sd@gmail.com>
2019-08-30 10:06:40 +0200

    delete unused spec

--- service/controller/clusterapi/v29/resource/tccpoutputs/spec.go
+++
@@ -1,8 +0,0 @@
-package tccpoutputs
-
-import "github.com/aws/aws-sdk-go/service/ec2"
-
-type EC2 interface {
-	DescribeVpcs(input *ec2.DescribeVpcsInput) (*ec2.DescribeVpcsOutput, error)
-	DescribeVpcPeeringConnections(*ec2.DescribeVpcPeeringConnectionsInput) (*ec2.DescribeVpcPeeringConnectionsOutput, error)
-}
