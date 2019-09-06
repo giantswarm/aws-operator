343f700a1381b886d7ff4071c5203cf7f6707f3e
Tim Schindler <tim@giantswarm.io>
2019-09-06 10:10:00 +0200

    update vendor deps (#1932)

--- vendor/github.com/giantswarm/apiextensions/pkg/apis/application/v1alpha1/app_types.go
+++ vendor/github.com/giantswarm/apiextensions/pkg/apis/application/v1alpha1/app_types.go
@@ -1,6 +1,7 @@
 package v1alpha1
 
 import (
+	"github.com/ghodss/yaml"
 	apiextensionsv1beta1 "k8s.io/apiextensions-apiserver/pkg/apis/apiextensions/v1beta1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
@@ -8,6 +9,103 @@ import (
 const (
 	kindApp = "App"
 )
+
+const appCRDYAML = `
+apiVersion: apiextensions.k8s.io/v1beta1
+kind: CustomResourceDefinition
+metadata:
+  name: apps.application.giantswarm.io
+spec:
+  group: application.giantswarm.io
+  scope: Namespaced
+  version: v1alpha1
+  names:
+    kind: App
+    plural: apps
+    singular: app
+  subresources:
+    status: {}
+  validation:
+    openAPIV3Schema:
+      properties:
+        spec:
+          type: object
+          properties:
+            catalog:
+              type: string
+            name:
+              type: string
+            namespace:
+              type: string
+            version:
+              type: string
+            config:
+              type: object
+              properties:
+                configMap:
+                  type: object
+                  properties:
+                    name:
+                      type: string
+                    namespace:
+                      type: string
+                  required: ["name", "namespace"]
+                secret:
+                  type: object
+                  properties:
+                    name:
+                      type: string
+                    namespace:
+                      type: string
+                  required: ["name", "namespace"]
+            kubeConfig:
+              type: object
+              properties:
+                inCluster:
+                  type: boolean
+                context:
+                  type: object
+                  properties:
+                    name:
+                      type: string
+                secret:
+                  type: object
+                  properties:
+                    name:
+                      type: string
+                    namespace:
+                      type: string
+                  required: ["name", "namespace"]
+            userConfig:
+              type: object
+              properties:
+                configMap:
+                  type: object
+                  properties:
+                    name:
+                      type: string
+                    namespace:
+                      type: string
+                  required: ["name", "namespace"]
+                secret:
+                  type: object
+                  properties:
+                    name:
+                      type: string
+                    namespace:
+                      type: string
+                  required: ["name", "namespace"]
+          required: ["catalog", "name", "namespace", "version"]
+`
+
+var appCRD *apiextensionsv1beta1.CustomResourceDefinition
+
+func init() {
+	err := yaml.Unmarshal([]byte(appCRDYAML), &appCRD)
+	if err != nil {
+		panic(err)
+	}
+}
 
 // NewAppCRD returns a new custom resource definition for App.
 // This might look something like the following.
@@ -26,33 +124,12 @@ const (
 //         singular: app
 //
 func NewAppCRD() *apiextensionsv1beta1.CustomResourceDefinition {
-	return &apiextensionsv1beta1.CustomResourceDefinition{
-		TypeMeta: metav1.TypeMeta{
-			APIVersion: apiextensionsv1beta1.SchemeGroupVersion.String(),
-			Kind:       "CustomResourceDefinition",
-		},
-		ObjectMeta: metav1.ObjectMeta{
-			Name: "apps.application.giantswarm.io",
-		},
-		Spec: apiextensionsv1beta1.CustomResourceDefinitionSpec{
-			Group:   "application.giantswarm.io",
-			Scope:   "Namespaced",
-			Version: "v1alpha1",
-			Names: apiextensionsv1beta1.CustomResourceDefinitionNames{
-				Kind:     "App",
-				Plural:   "apps",
-				Singular: "app",
-			},
-			Subresources: &apiextensionsv1beta1.CustomResourceSubresources{
-				Status: &apiextensionsv1beta1.CustomResourceSubresourceStatus{},
-			},
-		},
-	}
+	return appCRD.DeepCopy()
 }
 
 func NewAppTypeMeta() metav1.TypeMeta {
 	return metav1.TypeMeta{
-		APIVersion: version,
+		APIVersion: SchemeGroupVersion.String(),
 		Kind:       kindApp,
 	}
 }
@@ -103,109 +180,109 @@ func NewAppTypeMeta() metav1.TypeMeta {
 type App struct {
 	metav1.TypeMeta   `json:",inline"`
 	metav1.ObjectMeta `json:"metadata"`
-	Spec              AppSpec   `json:"spec"`
-	Status            AppStatus `json:"status" yaml:"status"`
+	Spec              AppSpec   `json:"spec,omitempty"`
+	Status            AppStatus `json:"status,omitempty"`
 }
 
 type AppSpec struct {
 	// Catalog is the name of the app catalog this app belongs to.
 	// e.g. giantswarm
-	Catalog string `json:"catalog" yaml:"catalog"`
+	Catalog string `json:"catalog"`
 	// Config is the config to be applied when the app is deployed.
-	Config AppSpecConfig `json:"config" yaml:"config"`
+	Config *AppSpecConfig `json:"config,omitempty"`
 	// KubeConfig is the kubeconfig to connect to the cluster when deploying
 	// the app.
-	KubeConfig AppSpecKubeConfig `json:"kubeConfig" yaml:"kubeConfig"`
+	KubeConfig AppSpecKubeConfig `json:"kubeConfig"`
 	// Name is the name of the app to be deployed.
 	// e.g. kubernetes-prometheus
-	Name string `json:"name" yaml:"name"`
+	Name string `json:"name"`
 	// Namespace is the namespace where the app should be deployed.
 	// e.g. monitoring
-	Namespace string `json:"namespace" yaml:"namespace"`
+	Namespace string `json:"namespace"`
 	// UserConfig is the user config to be applied when the app is deployed.
-	UserConfig AppSpecUserConfig `json:"userConfig" yaml:"userConfig"`
+	UserConfig *AppSpecUserConfig `json:"userConfig,omitempty"`
 	// Version is the version of the app that should be deployed.
 	// e.g. 1.0.0
-	Version string `json:"version" yaml:"version"`
+	Version string `json:"version"`
 }
 
 type AppSpecConfig struct {
 	// ConfigMap references a config map containing values that should be
 	// applied to the app.
-	ConfigMap AppSpecConfigConfigMap `json:"configMap" yaml:"configMap"`
+	ConfigMap AppSpecConfigConfigMap `json:"configMap"`
 	// Secret references a secret containing secret values that should be
 	// applied to the app.
-	Secret AppSpecConfigSecret `json:"secret" yaml:"secret"`
+	Secret AppSpecConfigSecret `json:"secret"`
 }
 
 type AppSpecConfigConfigMap struct {
 	// Name is the name of the config map containing app values to apply,
 	// e.g. prometheus-values.
-	Name string `json:"name" yaml:"name"`
+	Name string `json:"name"`
 	// Namespace is the namespace of the values config map,
 	// e.g. monitoring.
-	Namespace string `json:"namespace" yaml:"namespace"`
+	Namespace string `json:"namespace"`
 }
 
 type AppSpecConfigSecret struct {
 	// Name is the name of the secret containing app values to apply,
 	// e.g. prometheus-secret.
-	Name string `json:"name" yaml:"name"`
+	Name string `json:"name"`
 	// Namespace is the namespace of the secret,
 	// e.g. kube-system.
-	Namespace string `json:"namespace" yaml:"namespace"`
+	Namespace string `json:"namespace"`
 }
 
 type AppSpecKubeConfig struct {
 	// InCluster is a flag for whether to use InCluster credentials. When true the
 	// context name and secret should not be set.
-	InCluster bool `json:"inCluster" yaml:"inCluster"`
+	InCluster bool `json:"inCluster"`
 	// Context is the kubeconfig context.
-	Context AppSpecKubeConfigContext `json:"context" yaml:"context"`
+	Context *AppSpecKubeConfigContext `json:"context,omitempty"`
 	// Secret references a secret containing the kubconfig.
-	Secret AppSpecKubeConfigSecret `json:"secret" yaml:"secret"`
+	Secret *AppSpecKubeConfigSecret `json:"secret,omitempty"`
 }
 
 type AppSpecKubeConfigContext struct {
 	// Name is the name of the kubeconfig context.
 	// e.g. giantswarm-12345.
-	Name string `json:"name" yaml:"name"`
+	Name string `json:"name"`
 }
 
 type AppSpecKubeConfigSecret struct {
 	// Name is the name of the secret containing the kubeconfig,
 	// e.g. app-operator-kubeconfig.
-	Name string `json:"name" yaml:"name"`
+	Name string `json:"name"`
 	// Namespace is the namespace of the secret containing the kubeconfig,
 	// e.g. giantswarm.
-	Namespace string `json:"namespace" yaml:"namespace"`
+	Namespace string `json:"namespace"`
 }
 
 type AppSpecUserConfig struct {
 	// ConfigMap references a config map containing user values that should be
 	// applied to the app.
-	ConfigMap AppSpecUserConfigConfigMap `json:"configMap" yaml:"configMap"`
+	ConfigMap AppSpecUserConfigConfigMap `json:"configMap"`
 	// Secret references a secret containing user secret values that should be
 	// applied to the app.
-	Secret AppSpecUserConfigSecret `json:"secret" yaml:"secret"`
+	Secret AppSpecUserConfigSecret `json:"secret"`
 }
 
 type AppSpecUserConfigConfigMap struct {
 	// Name is the name of the config map containing user values to apply,
 	// e.g. prometheus-user-values.
-	Name string `json:"name" yaml:"name"`
+	Name string `json:"name"`
 	// Namespace is the namespace of the user values config map on the control plane,
 	// e.g. 123ab.
-	Namespace string `json:"namespace" yaml:"namespace"`
+	Namespace string `json:"namespace"`
 }
 
 type AppSpecUserConfigSecret struct {
 	// Name is the name of the secret containing user values to apply,
 	// e.g. prometheus-user-secret.
-	Name string `json:"name" yaml:"name"`
+	Name string `json:"name"`
 	// Namespace is the namespace of the secret,
 	// e.g. kube-system.
-	Namespace string `json:"namespace" yaml:"namespace"`
+	Namespace string `json:"namespace"`
 }
 
 type AppStatus struct {
@@ -214,24 +291,24 @@ type AppStatus struct {
 	// component being deployed.
 	// e.g. 0.21.0.
 	// https://docs.helm.sh/developing_charts/#the-chart-yaml-file
-	AppVersion string `json:"appVersion" yaml:"appVersion"`
+	AppVersion string `json:"appVersion,omitempty"`
 	// Release is the status of the Helm release for the deployed app.
-	Release AppStatusRelease `json:"release" yaml:"release"`
+	Release *AppStatusRelease `json:"release,omitempty"`
 	// Version is the value of the Version field in the Chart.yaml of the
 	// deployed app.
 	// e.g. 1.0.0.
-	Version string `json:"version" yaml:"version"`
+	Version string `json:"version,omitempty"`
 }
 
 type AppStatusRelease struct {
 	// LastDeployed is the time when the app was last deployed.
-	LastDeployed DeepCopyTime `json:"lastDeployed" yaml:"lastDeployed"`
+	LastDeployed DeepCopyTime `json:"lastDeployed"`
 	// Reason is the description of the last status of helm release when the app is
 	// not installed successfully, e.g. deploy resource already exists.
-	Reason string `json:"reason,omitempty" yaml:"reason,omitempty"`
+	Reason string `json:"reason,omitempty"`
 	// Status is the status of the deployed app,
 	// e.g. DEPLOYED.
-	Status string `json:"status" yaml:"status"`
+	Status string `json:"status"`
 }
 
 // +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
--- vendor/github.com/giantswarm/k8scloudconfig/CHANGELOG.md
+++ vendor/github.com/giantswarm/k8scloudconfig/CHANGELOG.md
@@ -9,7 +9,13 @@ The latest version is considered WIP and it is a subject of change. All other
 versions are frozen. To freeze current version all files are copied to a new
 version directory, and  then changes are introduced.
 
-## [v4.7.0] WIP
+## [v4.8.0] WIP
+
+### Added
+
+- TODO
+
+## [v4.7.0]
 
 ### Added
 
