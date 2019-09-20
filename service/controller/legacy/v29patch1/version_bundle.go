19fe559a580e11d78041c564ca9640d315d0debb
Marcel Mueller <marcel.mueller1@rwth-aachen.de>
2019-09-20 14:19:05 +0200

    Add internal elb for ingress backport to v29patch1

--- service/controller/legacy/v29patch1/version_bundle.go
+++ service/controller/legacy/v29patch1/version_bundle.go
@@ -12,6 +12,11 @@ func VersionBundle() versionbundle.Bundle {
 				Description: "Duplicate etcd record set into public hosted zone.",
 				Kind:        versionbundle.KindAdded,
 			},
+			{
+				Component:   "cloudformation",
+				Description: "Add ingress internal load-balancer in private hosted zone.",
+				Kind:        versionbundle.KindAdded,
+			},
 		},
 		Components: []versionbundle.Component{
 			{
