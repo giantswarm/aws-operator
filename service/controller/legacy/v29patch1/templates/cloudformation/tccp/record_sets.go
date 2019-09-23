93660bc7d4563c1b68e1c82bff9f8d1f0e18a173
Bricktop <marcel.mueller1@rwth-aachen.de>
2019-09-23 18:51:40 +0200

    Add internal hostedzone wildcard for ingress (#1956)
    * Add internal hostedzone wildcard for ingress
    
    * Update service/controller/legacy/v29patch1/templates/cloudformation/tccp/record_sets.go
    
    Co-Authored-By: Fernando Ripoll <fernando@giantswarm.io>
    
    * Add to v30 as well
    
    * whitespace

--- service/controller/legacy/v29patch1/templates/cloudformation/tccp/record_sets.go
+++ service/controller/legacy/v29patch1/templates/cloudformation/tccp/record_sets.go
@@ -67,6 +67,15 @@ const RecordSets = `
       Name: 'ingress.{{ $v.ClusterID }}.k8s.{{ $v.BaseDomain }}.'
       HostedZoneId: !Ref 'InternalHostedZone'
       Type: A
+  IngressInternalWildcardRecordSet:
+    Type: AWS::Route53::RecordSet
+    Properties:
+      Name: '*.{{ $v.ClusterID }}.k8s.{{ $v.BaseDomain }}.'
+      HostedZoneId: !Ref 'InternalHostedZone'
+      TTL: '300'
+      Type: CNAME
+      ResourceRecords:
+        - !Ref 'IngressInternalRecordSet'
   IngressRecordSet:
     Type: AWS::Route53::RecordSet
     Properties:
