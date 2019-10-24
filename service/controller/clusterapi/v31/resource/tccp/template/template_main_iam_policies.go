9a1e89af7c63a5a8cc55706d0c0c8673bfc886c8
Volodymyr Stoiko <me@volodymyrstoiko.com>
2019-10-24 12:58:10 +0300

    Add route53manager role

--- service/controller/clusterapi/v31/resource/tccp/template/template_main_iam_policies.go
+++ service/controller/clusterapi/v31/resource/tccp/template/template_main_iam_policies.go
@@ -84,7 +84,36 @@ const TemplateMainIAMPolicies = `
           Effect: "Allow"
           Action: "sts:AssumeRole"
           Resource: "*"
-
+  Route53ManagerRole:
+    Type: "AWS::IAM::Role"
+    Properties:
+      RoleName: {{ $v.ClusterID }}-Route53Manager-Role
+      AssumeRolePolicyDocument:
+        Version: "2012-10-17"
+        Statement:
+          Effect: "Allow"
+          Principal:
+            AWS: !GetAtt IAMManagerRole.Arn
+          Action: "sts:AssumeRole"
+  Route53ManagerRolePolicy:
+    Type: "AWS::IAM::Policy"
+    Properties:
+      PolicyName: {{ $v.ClusterID }}-Route53Manager-Policy
+      Roles:
+        - Ref: "Route53ManagerRole"
+      PolicyDocument:
+        Version: "2012-10-17"
+        Statement:
+          - Effect: "Allow"
+            Action: "route53:ChangeResourceRecordSets"
+            Resource:
+              - "arn:aws:route53:::hostedzone/!Ref 'HostedZone'"
+              - "arn:aws:route53:::hostedzone/!Ref 'InternalHostedZone'"
+          - Effect: "Allow"
+            Action:
+			  - "route53:ListHostedZones"
+			  - "route53:ListResourceRecordSets"
+            Resource: "*"
   MasterInstanceProfile:
     Type: "AWS::IAM::InstanceProfile"
     Properties:
