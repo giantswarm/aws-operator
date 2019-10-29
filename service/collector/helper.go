c35294c3eb3bd5a8bef4ef6f38c29aa661405aed
Paweł Kopiczko <pawel@giantswarm.io>
2019-10-29 10:11:36 +0000

    service: flatten credential package (#2027)

--- service/collector/helper.go
+++ service/collector/helper.go
@@ -12,7 +12,7 @@ import (
 
 	clientaws "github.com/giantswarm/aws-operator/client/aws"
 	"github.com/giantswarm/aws-operator/service/accountid"
-	"github.com/giantswarm/aws-operator/service/controller/clusterapi/v31/credential"
+	"github.com/giantswarm/aws-operator/service/credential"
 )
 
 type helperConfig struct {
