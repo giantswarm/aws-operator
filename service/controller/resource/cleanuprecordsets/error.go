179a0184643b28b02972745efd27d5094d54798e
Volodymyr Stoiko <me@volodymyrstoiko.com>
2019-10-29 14:36:29 +0200

    Remove unused error types

--- service/controller/resource/cleanuprecordsets/error.go
+++ service/controller/resource/cleanuprecordsets/error.go
@@ -1,7 +1,6 @@
 package cleanuprecordsets
 
 import (
-	"github.com/aws/aws-sdk-go/aws/awserr"
 	"github.com/giantswarm/microerror"
 )
 
@@ -15,33 +14,6 @@ import (
 //
 var executionFailedError = &microerror.Error{
 	Kind: "executionFailedError",
-}
-
-var dependencyViolationError = &microerror.Error{
-	Kind: "dependencyViolationError",
-}
-
-// IsDependencyViolation asserts dependencyViolationError. Additionally it
-// asserts AWS errors which may look like the following.
-//
-//     DependencyViolation: resource sg-07423aeb02946f323 has a dependent object\n\tstatus code: 400, request id: c16da859-433c-4e59-b598-ef17f9faa770
-//
-func IsDependencyViolation(err error) bool {
-	c := microerror.Cause(err)
-
-	if c == dependencyViolationError {
-		return true
-	}
-
-	aerr, ok := c.(awserr.Error)
-	if !ok {
-		return false
-	}
-	if aerr.Code() == "DependencyViolation" {
-		return true
-	}
-
-	return false
 }
 
 var invalidConfigError = &microerror.Error{
