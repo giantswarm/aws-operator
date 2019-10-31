1705dde4b8be28144aa5d6e8cee3f5d0582b7a89
Thomas Fussell <thomas@fussell.io>
2019-10-31 11:52:56 -0400

    remove component updates from changelg, included in previous release

--- service/versionbundle/version_bundle.go
+++ service/versionbundle/version_bundle.go
@@ -20,26 +20,6 @@ func New() versionbundle.Bundle {
 				Kind:        versionbundle.KindAdded,
 			},
 			{
-				Component:   "calico",
-				Description: "Updated from v3.8.2 to v3.9.1.",
-				Kind:        versionbundle.KindChanged,
-			},
-			{
-				Component:   "containerlinux",
-				Description: "Updated from v2135.4.0 to v2191.5.0.",
-				Kind:        versionbundle.KindChanged,
-			},
-			{
-				Component:   "etcd",
-				Description: "Updated from v3.3.13 to v3.3.15.",
-				Kind:        versionbundle.KindChanged,
-			},
-			{
-				Component:   "kubernetes",
-				Description: "Updated from v1.14.6 to v1.15.5.",
-				Kind:        versionbundle.KindAdded,
-			},
-			{
 				Component:   "clusterapi",
 				Description: "Add cleanuprecordsets resource to cleanup non-managed route53 records.",
 				Kind:        versionbundle.KindAdded,
