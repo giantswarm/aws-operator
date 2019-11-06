0abfa341adf7a335a07979abb93bd00a436a4e4b
Vaclav Rozsypalek <vaclav@giantswarm.io>
2019-11-06 12:02:38 +0100

    use custom versionbundle

--- pkg/project/project.go
+++ pkg/project/project.go
@@ -1,7 +1,7 @@
 package project
 
 var (
-	bundleVersion        = "7.0.0-dev"
+	bundleVersion        = "100.0.0-vaclav"
 	description          = "The aws-operator handles Kubernetes clusters running on a Kubernetes cluster inside of AWS."
 	gitSHA               = "n/a"
 	name          string = "aws-operator"
