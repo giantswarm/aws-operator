5b10627bc5274b973aead48e1f44424db89a1c85
Vaclav Rozsypalek <vaclav@giantswarm.io>
2019-11-15 17:14:54 +0100

    fix e2e error

--- integration/setup/setup.go
+++ integration/setup/setup.go
@@ -135,7 +135,7 @@ func installAWSOperator(ctx context.Context, config Config) error {
 	if err != nil {
 		return microerror.Mask(err)
 	}
-	err := ensureHostVPCCreated(ctx, config)
+	err = ensureHostVPCCreated(ctx, config)
 	if err != nil {
 		return microerror.Mask(err)
 	}
