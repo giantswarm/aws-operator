59edf6485db0992417b61ec27264d51dd5a1f9d3
Théo Brigitte <theo@giantswarm.io>
2019-11-22 09:43:57 +0100

    exposes reconciled_version in build_info metric (#2076)

--- vendor/github.com/giantswarm/microendpoint/service/version/metric.go
+++ vendor/github.com/giantswarm/microendpoint/service/version/metric.go
@@ -12,7 +12,7 @@ var buildInfo = prometheus.NewGaugeVec(
 		Name:      "build_info",
 		Help:      "A metric with a constant '1' value labeled by commit, golang version, golang os, and golang arch.",
 	},
-	[]string{"commit", "golang_version", "golang_goos", "golang_goarch"},
+	[]string{"commit", "golang_version", "golang_goos", "golang_goarch", "reconciled_version"},
 )
 
 func init() {
@@ -20,5 +20,7 @@ func init() {
 }
 
 func (s *Service) updateBuildInfoMetric() {
-	buildInfo.WithLabelValues(s.gitCommit, runtime.Version(), runtime.GOOS, runtime.GOARCH).Set(1)
+	for _, bundle := range s.versionBundles {
+		buildInfo.WithLabelValues(s.gitCommit, runtime.Version(), runtime.GOOS, runtime.GOARCH, bundle.Version).Set(1)
+	}
 }
