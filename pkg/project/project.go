537f0873d08bdfcc0907a7c770c9535d30ac7816
Antonia von den Driesch <antonia@giantswarm.io>
2019-11-26 20:45:34 +0900

    change version back

--- pkg/project/project.go
+++ pkg/project/project.go
@@ -1,7 +1,7 @@
 package project
 
 var (
-	bundleVersion        = "10.9.9"
+	bundleVersion        = "7.0.1-dev"
 	description          = "The aws-operator handles Kubernetes clusters running on a Kubernetes cluster inside of AWS."
 	gitSHA               = "n/a"
 	name          string = "aws-operator"
