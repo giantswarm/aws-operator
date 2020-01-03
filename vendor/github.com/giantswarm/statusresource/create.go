18af67a769960fcfe8d4cf5d083f70fe5e1aff03
Stevo Slavić <stevo@giantswarm.io>
2020-01-03 02:07:51 +0100

    Add even more debug logging

--- vendor/github.com/giantswarm/statusresource/create.go
+++ vendor/github.com/giantswarm/statusresource/create.go
@@ -45,6 +45,7 @@ func (r *Resource) EnsureCreated(ctx context.Context, obj interface{}) error {
 			if err != nil {
 				return microerror.Mask(err)
 			}
+			r.logger.LogCtx(ctx, "level", "debug", "message", fmt.Sprintf("DEBUG status before %#v", newObj))
 
 			newAccessor, err := meta.Accessor(newObj)
 			if err != nil {
@@ -68,6 +69,11 @@ func (r *Resource) EnsureCreated(ctx context.Context, obj interface{}) error {
 				}
 
 				modified = true
+				updatedObj, err := r.restClient.Get().AbsPath(newAccessor.GetSelfLink()).Do().Get()
+				if err != nil {
+					return microerror.Mask(err)
+				}
+				r.logger.LogCtx(ctx, "level", "debug", "message", fmt.Sprintf("DEBUG status after %#v", updatedObj))
 			}
 
 			return nil
@@ -85,6 +91,7 @@ func (r *Resource) EnsureCreated(ctx context.Context, obj interface{}) error {
 
 	if modified {
 		r.logger.LogCtx(ctx, "level", "debug", "message", "patched CR status")
+
 		reconciliationcanceledcontext.SetCanceled(ctx)
 		r.logger.LogCtx(ctx, "level", "debug", "message", "canceling reconciliation")
 	} else {
