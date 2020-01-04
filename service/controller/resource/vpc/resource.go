7a617022905ae2b7288df6fd2561796e70fde761
Stevo Slavić <stevo@giantswarm.io>
2020-01-04 16:57:08 +0100

    Lookup vpc by Name tag

--- service/controller/resource/vpc/resource.go
+++ service/controller/resource/vpc/resource.go
@@ -107,7 +107,7 @@ func (r *Resource) lookup(ctx context.Context, client EC2, installationName stri
 		i := &ec2.DescribeVpcsInput{
 			Filters: []*ec2.Filter{
 				{
-					Name: aws.String("tag:giantswarm.io/installation"),
+					Name: aws.String("tag:Name"),
 					Values: []*string{
 						aws.String(installationName),
 					},
