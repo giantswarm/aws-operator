49d5624f9f85d249f03e6bf833c21352e0ed9ab5
Tim Schindler <xh3b4sd@gmail.com>
2020-01-12 14:14:22 +0100

    revert version change

--- pkg/project/project.go
+++ pkg/project/project.go
@@ -1,7 +1,7 @@
 package project
 
 var (
-	bundleVersion        = "8.0.3-xh3b4sd"
+	bundleVersion        = "8.0.3-dev"
 	description          = "The aws-operator handles Kubernetes clusters running on a Kubernetes cluster inside of AWS."
 	gitSHA               = "n/a"
 	name          string = "aws-operator"
