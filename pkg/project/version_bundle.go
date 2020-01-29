9897b297ea9dcd53f615f17472f8d2b368e7e346
Thomas Fussell <thomas@fussell.io>
2020-01-29 10:23:22 -0500

    reset changelogs, bump to next dev version

--- pkg/project/version_bundle.go
+++ pkg/project/version_bundle.go
@@ -9,42 +9,10 @@ func NewBundle() versionbundle.Bundle {
 		Changelogs: []versionbundle.Changelog{
 			{
 				Component:   "aws-operator",
-				Description: "Update to support Kubernetes 1.16.",
+				Description: "TODO.",
 				Kind:        versionbundle.KindChanged,
 				URLs: []string{
-					"https://github.com/giantswarm/aws-operator/pull/2090",
-				},
-			},
-			{
-				Component:   "calico",
-				Description: "Update from v3.9.1 to v3.10.1.",
-				Kind:        versionbundle.KindChanged,
-				URLs: []string{
-					"https://github.com/giantswarm/k8scloudconfig/pull/615",
-				},
-			},
-			{
-				Component:   "containerlinux",
-				Description: "Update from v2191.5.0 to 2247.6.0.",
-				Kind:        versionbundle.KindChanged,
-				URLs: []string{
-					"https://github.com/giantswarm/k8scloudconfig/pull/615",
-				},
-			},
-			{
-				Component:   "etcd",
-				Description: "Update from v3.3.15 to v3.3.17.",
-				Kind:        versionbundle.KindChanged,
-				URLs: []string{
-					"https://github.com/giantswarm/k8scloudconfig/pull/615",
-				},
-			},
-			{
-				Component:   "kubernetes",
-				Description: "Update from v1.15.5 to v1.16.3.",
-				Kind:        versionbundle.KindChanged,
-				URLs: []string{
-					"https://github.com/giantswarm/k8scloudconfig/pull/615",
+					"https://github.com/giantswarm/aws-operator/pull/X",
 				},
 			},
 		},
