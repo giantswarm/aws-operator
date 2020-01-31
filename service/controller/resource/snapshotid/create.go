4d74a01b071209299a3f0c1df9a02f3e21d7dec5
lachlanh <lachlan@hesp.es>
2020-01-31 17:30:16 +0100

    draft of ensure created

--- service/controller/resource/snapshotid/create.go
+++ service/controller/resource/snapshotid/create.go
@@ -2,7 +2,10 @@ package snapshotid
 
 import (
 	"context"
+	"fmt"
 
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/service/ec2"
 	"github.com/giantswarm/api/pkg/key"
 	"github.com/giantswarm/microerror"
 	"k8s.io/apimachinery/pkg/api/meta"
@@ -25,6 +28,30 @@ func (r *Resource) EnsureCreated(ctx context.Context, obj interface{}) error {
 	// TODO define key function
 
 	// TODO lookup snapshot
+	input := &ec2.DescribeSnapshotsInput{
+		//use the filter here
+		Filters: []*ec2.Filter{
+			{
+				Name: aws.String(fmt.Sprintf("tag:%s", key.TagCluster)),
+				Values: []*string{
+					aws.String(key.ClusterID(&cr)),
+				},
+			},
+			{
+				Name: aws.String(fmt.Sprintf("tag:%s", key.TagSnapshot)),
+				Values: []*string{
+					aws.String(key.HAMasterSnapshotIDValue),
+				},
+			},
+		},
+	}
+	result, err := cc.Client.TenantCluster.AWS.EC2.DescribeSnapshots(input)
+	// 1 is good
+	// 0 bad
+	// > 1 bad
+	if err != nil {
+		return microerror.Mask(err)
+	}
 
 	// TODO define structure
 	cc.Status.TenantCluster.TCNP.ASG.MinSize = minSize
