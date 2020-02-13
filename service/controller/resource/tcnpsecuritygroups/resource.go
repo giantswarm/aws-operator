08d53f0929d13cf57bec9ff7e2cc524222f5debb
calvix <vaclav@giantswarm.io>
2020-02-13 11:08:19 +0100

    'rename stackName'

--- service/controller/resource/tcnpsecuritygroups/resource.go
+++ service/controller/resource/tcnpsecuritygroups/resource.go
@@ -89,8 +89,6 @@ func (r *Resource) addInfoToCtx(ctx context.Context, cr infrastructurev1alpha2.A
 			}
 			// ignore security group for this node pool machine deployment
 			if machineDeploymentID == key.MachineDeploymentID(&cr) {
-				r.logger.LogCtx(ctx, "level", "debug", "message", fmt.Sprintf("skipping sg %s for stack %s", *sg.GroupId, stackName), stackName)
-
 				continue
 			}
 
@@ -118,7 +116,7 @@ func (r *Resource) addInfoToCtx(ctx context.Context, cr infrastructurev1alpha2.A
 			if err != nil {
 				return microerror.Mask(err)
 			}
-			r.logger.LogCtx(ctx, "level", "debug", "message", fmt.Sprintf("found %d ec2 instances for stack %s", len(o.Reservations[0].Instances), stackName))
+			r.logger.LogCtx(ctx, "level", "debug", "message", fmt.Sprintf("found %d ec2 instances for machineDeployment %s", len(o.Reservations[0].Instances), machineDeploymentID))
 
 			// add security group to the list only if the node pool machine deployment has any running instances
 			// if there are no running or pending instances the node pool machine deployment might be deleted
