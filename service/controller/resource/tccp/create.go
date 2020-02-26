20722738faa8731c46aa3e113f8b15b68a42893e
Tim Schindler <xh3b4sd@gmail.com>
2020-02-26 11:04:56 +0100

    simplify controller context

--- service/controller/resource/tccp/create.go
+++ service/controller/resource/tccp/create.go
@@ -71,7 +71,6 @@ func (r *Resource) EnsureCreated(ctx context.Context, obj interface{}) error {
 			r.logger.LogCtx(ctx, "level", "debug", "message", "canceling resource")
 			return nil
 		}
-
 	}
 
 	{
