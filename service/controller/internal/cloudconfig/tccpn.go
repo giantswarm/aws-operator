45f12927939f07fc04adad7a803ce3205a875617
lachlanh <lachlan@hesp.es>
2020-02-26 17:30:23 +0100

    make consistent with tccp

--- service/controller/internal/cloudconfig/tccpn.go
+++ service/controller/internal/cloudconfig/tccpn.go
@@ -2,6 +2,7 @@ package cloudconfig
 
 import (
 	"context"
+	"fmt"
 
 	infrastructurev1alpha2 "github.com/giantswarm/apiextensions/pkg/apis/infrastructure/v1alpha2"
 	"github.com/giantswarm/certs"
@@ -34,36 +35,67 @@ func NewTCCPN(config TCCPNConfig) (*TCCPN, error) {
 	return t, nil
 }
 
-//TODO LH-AVDD figure out what should go in render from copy of tccp
 func (t *TCCPN) Render(ctx context.Context, cr infrastructurev1alpha2.AWSCluster, clusterCerts certs.Cluster, clusterKeys randomkeys.Cluster, labels string) ([]byte, error) {
-	_, err := controllercontext.FromContext(ctx)
+	cc, err := controllercontext.FromContext(ctx)
 	if err != nil {
 		return nil, microerror.Mask(err)
+	}
+
+	randomKeyTmplSet, err := renderRandomKeyTmplSet(ctx, t.config.Encrypter, cc.Status.TenantCluster.Encryption.Key, clusterKeys)
+	if err != nil {
+		return nil, microerror.Mask(err)
+	}
+
+	var apiExtraArgs []string
+	{
+		if key.OIDCClientID(cr) != "" {
+			apiExtraArgs = append(apiExtraArgs, fmt.Sprintf("--oidc-client-id=%s", key.OIDCClientID(cr)))
+		}
+		if key.OIDCIssuerURL(cr) != "" {
+			apiExtraArgs = append(apiExtraArgs, fmt.Sprintf("--oidc-issuer-url=%s", key.OIDCIssuerURL(cr)))
+		}
+		if key.OIDCUsernameClaim(cr) != "" {
+			apiExtraArgs = append(apiExtraArgs, fmt.Sprintf("--oidc-username-claim=%s", key.OIDCUsernameClaim(cr)))
+		}
+		if key.OIDCGroupsClaim(cr) != "" {
+			apiExtraArgs = append(apiExtraArgs, fmt.Sprintf("--oidc-groups-claim=%s", key.OIDCGroupsClaim(cr)))
+		}
+
+		apiExtraArgs = append(apiExtraArgs, t.config.APIExtraArgs...)
+	}
+
+	var kubeletExtraArgs []string
+	{
+		if t.config.PodInfraContainerImage != "" {
+			kubeletExtraArgs = append(kubeletExtraArgs, fmt.Sprintf("--pod-infra-container-image=%s", t.config.PodInfraContainerImage))
+		}
+
+		kubeletExtraArgs = append(kubeletExtraArgs, t.config.KubeletExtraArgs...)
 	}
 
 	var params k8scloudconfig.Params
 	{
 		params = k8scloudconfig.DefaultParams()
 
-		// params.Cluster = cmaClusterToG8sConfig(t.config, cr, labels).Cluster
+		params.Cluster = cmaClusterToG8sConfig(t.config, cr, labels).Cluster
 		params.DisableEncryptionAtREST = true
 		// Ingress Controller service is not created via ignition.
 		// It gets created by the Ingress Controller app if it is installed in the tenant cluster.
 		params.DisableIngressControllerService = true
 		params.EtcdPort = key.EtcdPort
-		// params.Extension = &MasterExtension{
-		// 	awsConfigSpec: ,//cmaClusterToG8sConfig(t.config, cr, labels),
-		// 	baseExtension: baseExtension{
-		// 		cluster:       cr,
-		// 		encrypter:     t.config.Encrypter,
-		// 		encryptionKey: cc.Status.TenantCluster.Encryption.Key,
-		// 	},
-		// 	cc:               cc,
-		// 	clusterCerts:     clusterCerts,
-		// 	randomKeyTmplSet: randomKeyTmplSet,
-		// }
-		//params.Hyperkube.Apiserver.Pod.CommandExtraArgs = apiExtraArgs
-		//params.Hyperkube.Kubelet.Docker.CommandExtraArgs = kubeletExtraArgs
+		params.Extension = &MasterExtension{
+			awsConfigSpec: cmaClusterToG8sConfig(t.config, cr, labels),
+			baseExtension: baseExtension{
+				cluster:       cr,
+				encrypter:     t.config.Encrypter,
+				encryptionKey: cc.Status.TenantCluster.Encryption.Key,
+			},
+			cc:               cc,
+			clusterCerts:     clusterCerts,
+			randomKeyTmplSet: randomKeyTmplSet,
+		}
+		params.Hyperkube.Apiserver.Pod.CommandExtraArgs = apiExtraArgs
+		params.Hyperkube.Kubelet.Docker.CommandExtraArgs = kubeletExtraArgs
 		params.ImagePullProgressDeadline = t.config.ImagePullProgressDeadline
 		params.RegistryDomain = t.config.RegistryDomain
 		params.SSOPublicKey = t.config.SSOPublicKey
