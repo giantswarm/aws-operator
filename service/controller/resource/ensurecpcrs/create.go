ac72a32ccf14258837d644c75ffd69e600dcfc0e
Tim Schindler <xh3b4sd@gmail.com>
2020-02-28 19:02:52 +0100

    implement exists methods for the ensurecpcrs resource (#2159)

--- service/controller/resource/ensurecpcrs/create.go
+++ service/controller/resource/ensurecpcrs/create.go
@@ -6,7 +6,9 @@ import (
 
 	infrastructurev1alpha2 "github.com/giantswarm/apiextensions/pkg/apis/infrastructure/v1alpha2"
 	"github.com/giantswarm/microerror"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 
+	"github.com/giantswarm/aws-operator/pkg/label"
 	"github.com/giantswarm/aws-operator/service/controller/key"
 )
 
@@ -55,18 +57,54 @@ func (r *Resource) EnsureCreated(ctx context.Context, obj interface{}) error {
 	return nil
 }
 
-func (r *Resource) createAWSControlPlaneCR(ctx context.Context, obj interface{}) error {
+func (r *Resource) createAWSControlPlaneCR(ctx context.Context, cr infrastructurev1alpha2.AWSCluster) error {
 	return nil
 }
 
-func (r *Resource) createG8sControlPlaneCR(ctx context.Context, obj interface{}) error {
+func (r *Resource) createG8sControlPlaneCR(ctx context.Context, cr infrastructurev1alpha2.AWSCluster) error {
 	return nil
 }
 
-func (r *Resource) awsControlPlaneCRExists(ctx context.Context, obj interface{}) (bool, error) {
-	return false, nil
+func (r *Resource) awsControlPlaneCRExists(ctx context.Context, cr infrastructurev1alpha2.AWSCluster) (bool, error) {
+	o := metav1.ListOptions{
+		LabelSelector: fmt.Sprintf("%s=%s", label.Cluster, key.ClusterID(&cr)),
+	}
+
+	list, err := r.k8sClient.G8sClient().InfrastructureV1alpha2().AWSControlPlanes(cr.GetNamespace()).List(o)
+	if err != nil {
+		return false, microerror.Mask(err)
+	}
+
+	// We simply list Control Plane CRs by filtering for the cluster ID label.
+	// This may lead to one or more results as the intention for Control Plane
+	// stacks on the infrastructure maintenance level is to run multiple ones if
+	// desired or necessary. Reason for this may be scaling, management, or
+	// refactoring of the underlying infrastructure of AWS resources.
+	if len(list.Items) == 0 {
+		return false, nil
+	}
+
+	return true, nil
 }
 
-func (r *Resource) g8sControlPlaneCRExists(ctx context.Context, obj interface{}) (bool, error) {
-	return false, nil
+func (r *Resource) g8sControlPlaneCRExists(ctx context.Context, cr infrastructurev1alpha2.AWSCluster) (bool, error) {
+	o := metav1.ListOptions{
+		LabelSelector: fmt.Sprintf("%s=%s", label.Cluster, key.ClusterID(&cr)),
+	}
+
+	list, err := r.k8sClient.G8sClient().InfrastructureV1alpha2().G8sControlPlanes(cr.GetNamespace()).List(o)
+	if err != nil {
+		return false, microerror.Mask(err)
+	}
+
+	// We simply list Control Plane CRs by filtering for the cluster ID label.
+	// This may lead to one or more results as the intention for Control Plane
+	// stacks on the infrastructure maintenance level is to run multiple ones if
+	// desired or necessary. Reason for this may be scaling, management, or
+	// refactoring of the underlying infrastructure of AWS resources.
+	if len(list.Items) == 0 {
+		return false, nil
+	}
+
+	return true, nil
 }
