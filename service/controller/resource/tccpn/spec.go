64bca3ce811cea8be4029f8a806273ac6fc7a5c5
lachlanh <lachlan@hesp.es>
2020-02-28 17:26:01 +0100

    remove unreferenced constants

--- service/controller/resource/tccpn/spec.go
+++ service/controller/resource/tccpn/spec.go
@@ -6,51 +6,6 @@ import (
 )
 
 const (
-	// defaultEBSVolumeMountPoint is the path for mounting the EBS volume.
-	defaultEBSVolumeMountPoint = "/dev/xvdh"
-	// defaultEBSVolumeSize is expressed in GB.
-	defaultEBSVolumeSize = "100"
-	// defaultEBSVolumeType is the EBS volume type.
-	defaultEBSVolumeType = "gp2"
-	// logEBSVolumeMountPoint is the path for mounting the log EBS volume
-	logEBSVolumeMountPoint = "/dev/xvdf"
-	// kubeletEBSVolumeMountPoint is the path for mounting the log EBS volume
-	kubeletEBSVolumeMountPoint = "/dev/xvdg"
-
-	// Subnet keys
-	subnetDescription = "description"
-	subnetGroupName   = "group-name"
-
-	// accountIDIndex represents the index in which we can find the account ID in the user ARN
-	// (splitting by colon)
-	accountIDIndex = 4
-
-	// The number of seconds AWS will wait, before issuing a health check on
-	// instances in an Auto Scaling Group.
-	gracePeriodSeconds = 10
-
-	tagKeyName = "Name"
-
-	suffixPublic  = "public"
-	suffixPrivate = "private"
-
-	externalELBScheme = "internet-facing"
-	internalELBScheme = "internal"
-
-	httpPort  = 80
-	httpsPort = 443
-)
-
-const (
-	// Default values for health checks.
-	healthCheckHealthyThreshold   = 2
-	healthCheckInterval           = 5
-	healthCheckTimeout            = 3
-	healthCheckUnhealthyThreshold = 2
-)
-
-const (
-	allPorts             = -1
 	cadvisorPort         = 4194
 	etcdPort             = 2379
 	kubeletPort          = 10250
@@ -58,16 +13,10 @@ const (
 	kubeStateMetricsPort = 10301
 	sshPort              = 22
 
-	allProtocols = "-1"
-	tcpProtocol  = "tcp"
+	tcpProtocol = "tcp"
 
 	defaultCIDR = "0.0.0.0/0"
 )
-
-type templateParams struct {
-	DockerVolumeResourceName   string
-	MasterInstanceResourceName string
-}
 
 // APIWhitelist defines guest cluster k8s public/private api whitelisting.
 type APIWhitelist struct {
