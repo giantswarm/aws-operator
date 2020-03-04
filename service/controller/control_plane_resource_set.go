e0328a3a18a57fb3230af363d6bc1dbe96c3880a
Antonia von den Driesch <antonia@giantswarm.io>
2020-03-04 16:23:56 +0900

    fix version bundle

--- service/controller/control_plane_resource_set.go
+++ service/controller/control_plane_resource_set.go
@@ -261,7 +261,7 @@ func newControlPlaneResourceSet(config controlPlaneResourceSetConfig) (*controll
 			return false
 		}
 
-		if key.OperatorVersion(&cr) == project.BundleVersion() {
+		if key.OperatorVersion(&cr) == project.Version() {
 			return true
 		}
 
