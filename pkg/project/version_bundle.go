c713ed71309c38792eed3c960df027375051e976
Thomas Fussell <thomas@giantswarm.io>
2020-03-10 20:32:59 -0400

    changelog must not be empty

--- pkg/project/version_bundle.go
+++ pkg/project/version_bundle.go
@@ -6,6 +6,35 @@ import (
 
 func NewBundle() versionbundle.Bundle {
 	return versionbundle.Bundle{
+		Changelogs: []versionbundle.Changelog{
+			{
+				Component:   "aws-operator",
+				Description: "Modified to get component versions from releases",
+				Kind:        versionbundle.KindChanged,
+			},
+		},
+		Components: []versionbundle.Component{
+			{
+				Name:    "calico",
+				Version: "0.0.0",
+			},
+			{
+				Name:    "containerlinux",
+				Version: "0.0.0",
+			},
+			{
+				Name:    "docker",
+				Version: "0.0.0",
+			},
+			{
+				Name:    "etcd",
+				Version: "0.0.0",
+			},
+			{
+				Name:    "kubernetes",
+				Version: "0.0.0",
+			},
+		},
 		Name:    Name(),
 		Version: BundleVersion(),
 	}
