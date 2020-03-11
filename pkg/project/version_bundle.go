0cc5769e731e0663d8aa403bed4697cf34ac4180
Thomas Fussell <thomas@giantswarm.io>
2020-03-11 08:03:27 -0400

    prepare for development

--- pkg/project/version_bundle.go
+++ pkg/project/version_bundle.go
@@ -8,46 +8,9 @@ func NewBundle() versionbundle.Bundle {
 	return versionbundle.Bundle{
 		Changelogs: []versionbundle.Changelog{
 			{
-				Component:   "calico",
-				Description: "Updated from v3.8.2 to v3.9.1.",
+				Component:   "TODO",
+				Description: "TODO",
 				Kind:        versionbundle.KindChanged,
-			},
-			{
-				Component:   "containerlinux",
-				Description: "Updated from v2135.4.0 to v2191.5.0.",
-				Kind:        versionbundle.KindChanged,
-			},
-			{
-				Component:   "etcd",
-				Description: "Updated from v3.3.13 to v3.3.15.",
-				Kind:        versionbundle.KindChanged,
-			},
-			{
-				Component:   "kubernetes",
-				Description: "Updated from v1.14.6 to v1.15.5.",
-				Kind:        versionbundle.KindChanged,
-			},
-		},
-		Components: []versionbundle.Component{
-			{
-				Name:    "calico",
-				Version: "3.9.1",
-			},
-			{
-				Name:    "containerlinux",
-				Version: "2191.5.0",
-			},
-			{
-				Name:    "docker",
-				Version: "18.06.3",
-			},
-			{
-				Name:    "etcd",
-				Version: "3.3.15",
-			},
-			{
-				Name:    "kubernetes",
-				Version: "1.15.5",
 			},
 		},
 		Name:    Name(),
