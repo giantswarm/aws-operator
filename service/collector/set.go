2b8ff22e8e998faa8afc3d88ed5bb4dc527c65d2
pipo02mix <fernando@giantswarm.io>
2020-03-16 10:54:39 +0100

    Add service quota collector to the set

--- service/collector/set.go
+++ service/collector/set.go
@@ -107,6 +107,21 @@ func NewSet(config SetConfig) (*Set, error) {
 		}
 	}
 
+	var sqCollector *ServiceQuota
+	{
+		c := ServiceQuotaConfig{
+			Helper: h,
+			Logger: config.Logger,
+
+			InstallationName: config.InstallationName,
+		}
+
+		sqCollector, err = NewServiceQuota(c)
+		if err != nil {
+			return nil, microerror.Mask(err)
+		}
+	}
+
 	var natCollector *NAT
 	{
 		c := NATConfig{
@@ -158,6 +173,7 @@ func NewSet(config SetConfig) (*Set, error) {
 				asgCollector,
 				ec2InstancesCollector,
 				elbCollector,
+				sqCollector,
 				natCollector,
 				vpcCollector,
 			},
