a60f4c7828743885f9c7835040309f2b6b38b634
Pau Rosello <paurosello@gmail.com>
2020-03-16 15:15:25 +0100

    set version

--- pkg/project/project.go
+++ pkg/project/project.go
@@ -1,7 +1,7 @@
 package project
 
 var (
-	bundleVersion        = "8.0.4"
+	bundleVersion        = "8.0.5-dev"
 	description          = "The aws-operator handles Kubernetes clusters running on a Kubernetes cluster inside of AWS."
 	gitSHA               = "n/a"
 	name          string = "aws-operator"
