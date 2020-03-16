0a5e71f37d93f8931ce075a673cd33e0ddda625b
Tim Schindler <xh3b4sd@gmail.com>
2020-03-16 15:56:21 +0100

    release v8.2.0

--- pkg/project/project.go
+++ pkg/project/project.go
@@ -5,7 +5,7 @@ var (
 	gitSHA             = "n/a"
 	name        string = "aws-operator"
 	source      string = "https://github.com/giantswarm/aws-operator"
-	version            = "8.1.2-dev"
+	version            = "8.2.0"
 )
 
 func Description() string {
