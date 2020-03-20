8b0df3ddbd474aa859ee2528f1c8e6e701295ee8
calvix <vaclav@giantswarm.io>
2020-03-20 11:08:08 +0100

    custom version

--- pkg/project/project.go
+++ pkg/project/project.go
@@ -5,7 +5,7 @@ var (
 	gitSHA             = "n/a"
 	name        string = "aws-operator"
 	source      string = "https://github.com/giantswarm/aws-operator"
-	version            = "8.2.2-xh3b4sd"
+	version            = "8.2.2-calvix"
 )
 
 func Description() string {
