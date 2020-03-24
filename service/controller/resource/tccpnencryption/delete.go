2f8082df8e40d1e716e862c7a14f8bb062bc3361
calvix <vaclav@giantswarm.io>
2020-03-24 18:28:10 +0100

    'remove deletion from tccpnencryption'

--- service/controller/resource/tccpnencryption/delete.go
+++ service/controller/resource/tccpnencryption/delete.go
@@ -2,22 +2,8 @@ package tccpnencryption
 
 import (
 	"context"
-
-	"github.com/giantswarm/microerror"
-
-	"github.com/giantswarm/aws-operator/service/controller/key"
 )
 
 func (r *Resource) EnsureDeleted(ctx context.Context, obj interface{}) error {
-	cr, err := key.ToCluster(obj)
-	if err != nil {
-		return microerror.Mask(err)
-	}
-
-	err = r.encrypter.EnsureDeletedEncryptionKey(ctx, cr)
-	if err != nil {
-		return microerror.Mask(err)
-	}
-
 	return nil
 }
