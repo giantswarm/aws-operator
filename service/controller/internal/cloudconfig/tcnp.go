f5085916c8d1b5b371e3898b16375f9a844d504a
Tim Schindler <xh3b4sd@gmail.com>
2020-03-31 17:32:56 +0200

    migrate from dep to Go modules (#2229)
    Co-authored-by: Tim Schindler <xh3b4sd@gmail.com>

--- service/controller/internal/cloudconfig/tcnp.go
+++ service/controller/internal/cloudconfig/tcnp.go
@@ -8,7 +8,7 @@ import (
 	infrastructurev1alpha2 "github.com/giantswarm/apiextensions/pkg/apis/infrastructure/v1alpha2"
 	g8sv1alpha1 "github.com/giantswarm/apiextensions/pkg/apis/provider/v1alpha1"
 	"github.com/giantswarm/certs"
-	k8scloudconfig "github.com/giantswarm/k8scloudconfig/v_5_2_0"
+	k8scloudconfig "github.com/giantswarm/k8scloudconfig/v6/v_5_2_0"
 	"github.com/giantswarm/microerror"
 	"github.com/giantswarm/randomkeys"
 
--- vendor/github.com/giantswarm/microendpoint/service/version/request.go
+++
@@ -1,5 +0,0 @@
-package version
-
-// Request is the configuration for the service action.
-type Request struct {
-}
