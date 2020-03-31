a2d670f51dae28be138b8ad49187ae08b0d902e1
wtty-fool <wtty.fool@gmail.com>
2020-03-31 10:51:34 +0200

    Revert "revert me later: change version for test purposes"
    This reverts commit 274939b7809686163f6f5300d7d46a89ca77c0c2.

--- pkg/project/project.go
+++ pkg/project/project.go
@@ -5,7 +5,7 @@ var (
 	gitSHA             = "n/a"
 	name        string = "aws-operator"
 	source      string = "https://github.com/giantswarm/aws-operator"
-	version            = "666.0.0"
+	version            = "8.2.2-dev"
 )
 
 func Description() string {
