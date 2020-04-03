0fdc45c1b0f3a5bba64b124d21c1bc473e0d05eb
Tim Schindler <xh3b4sd@gmail.com>
2020-04-03 18:24:59 +0200

    set dev version 8.2.3-dev (#2262)

--- pkg/project/project.go
+++ pkg/project/project.go
@@ -5,7 +5,7 @@ var (
 	gitSHA             = "n/a"
 	name        string = "aws-operator"
 	source      string = "https://github.com/giantswarm/aws-operator"
-	version            = "8.2.2"
+	version            = "8.2.3-dev"
 )
 
 func Description() string {
