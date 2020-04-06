a91c7b7daa9973c7ae48ec43bf1152cf621126d4
Tim Schindler <xh3b4sd@gmail.com>
2020-04-06 16:41:44 +0200

    bump project version

--- pkg/project/project.go
+++ pkg/project/project.go
@@ -5,7 +5,7 @@ var (
 	gitSHA             = "n/a"
 	name        string = "aws-operator"
 	source      string = "https://github.com/giantswarm/aws-operator"
-	version            = "8.2.3-dev"
+	version            = "8.2.3-xh3b4sd"
 )
 
 func Description() string {
