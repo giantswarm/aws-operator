0ecba92243193547f43a3e60a747c65744e08d92
Tim Schindler <xh3b4sd@gmail.com>
2020-04-08 21:08:19 +0200

    cleanup encrypter abstraction (#2279)

--- service/controller/spec.go
+++
@@ -1,9 +0,0 @@
-package controller
-
-import "github.com/giantswarm/micrologger"
-
-type encrypterConfigGetter interface {
-	GetInstallationName() string
-	GetLogger() micrologger.Logger
-	GetVaultAddress() string
-}
