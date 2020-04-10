e7ee284ff38e698634db90dbdedf5c02a6b78849
Tim Schindler <xh3b4sd@gmail.com>
2020-04-10 14:38:47 +0200

    fix linter issues

--- service/controller/resource/tcnpstatus/error.go
+++ service/controller/resource/tcnpstatus/error.go
@@ -4,18 +4,6 @@ import (
 	"github.com/giantswarm/microerror"
 )
 
-// executionFailedError is an error type for situations where Resource execution
-// cannot continue and must always fall back to operatorkit.
-//
-// This error should never be matched against and therefore there is no matcher
-// implement. For further information see:
-//
-//     https://github.com/giantswarm/fmt/blob/master/go/errors.md#matching-errors
-//
-var executionFailedError = &microerror.Error{
-	Kind: "executionFailedError",
-}
-
 var invalidConfigError = &microerror.Error{
 	Kind: "invalidConfigError",
 }
