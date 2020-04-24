02bd879bf52d241ce2152028c2b7d9205d48a937
Thomas Fussell <thomas@giantswarm.io>
2020-04-23 22:12:55 -0400

    update version

--- pkg/project/project.go
+++ pkg/project/project.go
@@ -1,12 +1,12 @@
 package project
 
 var (
-	bundleVersion        = "5.6.1-dev"
+	bundleVersion        = "5.6.2-dev"
 	description          = "The aws-operator handles Kubernetes clusters running on a Kubernetes cluster inside of AWS."
 	gitSHA               = "n/a"
 	name          string = "aws-operator"
 	source        string = "https://github.com/giantswarm/aws-operator"
-	version              = "5.6.1-dev"
+	version              = "5.6.2-dev"
 )
 
 func BundleVersion() string {
