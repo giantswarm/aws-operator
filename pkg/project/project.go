d4a4abbfac75163598aac35df71dcddfc8804fb1
Tim Schindler <xh3b4sd@gmail.com>
2020-04-27 13:00:34 +0200

    cleanup RBAC rules (#2355)

--- pkg/project/project.go
+++ pkg/project/project.go
@@ -5,7 +5,7 @@ var (
 	gitSHA             = "n/a"
 	name        string = "aws-operator"
 	source      string = "https://github.com/giantswarm/aws-operator"
-	version            = "8.4.1-dev"
+	version            = "8.4.1-xh3b4sd"
 )
 
 func Description() string {
