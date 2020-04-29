8e50310805f83bc3a63b09dec4ccd39ef856a274
Thomas Fussell <thomas@giantswarm.io>
2020-04-29 14:12:49 -0400

    clean up

--- service/controller/internal/encrypter/mock.go
+++ service/controller/internal/encrypter/mock.go
@@ -3,10 +3,8 @@ package encrypter
 import (
 	"context"
 	"fmt"
-	"strings"
 
 	"github.com/giantswarm/apiextensions/pkg/apis/provider/v1alpha1"
-	"github.com/giantswarm/microerror"
 )
 
 type EncrypterMock struct {
@@ -18,14 +16,6 @@ type EncrypterMock struct {
 func (e *EncrypterMock) Encrypt(ctx context.Context, key, plaintext string) (string, error) {
 	ciphertext := fmt.Sprintf("<encrypted>--%s", plaintext)
 	return ciphertext, nil
-}
-
-func (e *EncrypterMock) Decrypt(ctx context.Context, key, ciphertext string) (string, error) {
-	if !strings.HasPrefix(ciphertext, "<encrypted>--") {
-		return "", microerror.Mask(fmt.Errorf("InvalidCiphertextException"))
-	}
-	plaintext := strings.TrimPrefix(ciphertext, "<encrypted>--")
-	return plaintext, nil
 }
 
 func (e *EncrypterMock) EncryptionKey(ctx context.Context, customObject v1alpha1.AWSConfig) (string, error) {
