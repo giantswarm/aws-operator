feb8d46b52a51eae7886fdc70196303ce095791a
Thomas Fussell <thomas@giantswarm.io>
2020-05-01 08:15:14 -0400

    bump version to 5.7.1-dev (#2383)

--- pkg/project/project.go
+++ pkg/project/project.go
@@ -1,12 +1,12 @@
 package project
 
 var (
-	bundleVersion        = "5.7.0"
+	bundleVersion        = "5.7.1-dev"
 	description          = "The aws-operator handles Kubernetes clusters running on a Kubernetes cluster inside of AWS."
 	gitSHA               = "n/a"
 	name          string = "aws-operator"
 	source        string = "https://github.com/giantswarm/aws-operator"
-	version              = "5.7.0"
+	version              = "5.7.1-dev"
 )
 
 func BundleVersion() string {
