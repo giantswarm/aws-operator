0bd99a3d167368180c7f8c8f3cdab4da1f533439
Pau Rosello <paurosello@gmail.com>
2020-05-06 14:43:08 +0200

    fix reconciling version

--- pkg/project/project.go
+++ pkg/project/project.go
@@ -1,7 +1,7 @@
 package project
 
 var (
-	bundleVersion        = "5.5.1"
+	bundleVersion        = "5.5.2"
 	description          = "The aws-operator handles Kubernetes clusters running on a Kubernetes cluster inside of AWS."
 	gitSHA               = "n/a"
 	name          string = "aws-operator"
