75b1f8d1df3321ee749bd11487cfbe8a1cc98241
Nick Jüttner <nick@juni.io>
2020-05-07 13:46:45 +0200

    Update github.com/giantswarm/errors to the latest release (#2402)
    * Update github.com/giantswarm/errors to the latest release
    
    * Bump the version of aws-operator

--- pkg/project/project.go
+++ pkg/project/project.go
@@ -1,7 +1,7 @@
 package project
 
 var (
-	bundleVersion        = "5.6.0"
+	bundleVersion        = "5.6.1-dev"
 	description          = "The aws-operator handles Kubernetes clusters running on a Kubernetes cluster inside of AWS."
 	gitSHA               = "n/a"
 	name          string = "aws-operator"
