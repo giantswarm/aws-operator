98880b8e2e0b31f40fe48fe983b937e0a8135029
calvix <vaclav@giantswarm.io>
2020-05-11 15:48:44 +0200

    'update image'

--- service/controller/internal/cloudconfig/template/etcd3_attach_dep_service.go
+++ service/controller/internal/cloudconfig/template/etcd3_attach_dep_service.go
@@ -8,7 +8,7 @@ After=network.target
 
 [Service]
 # image is from https://github.com/giantswarm/aws-attach-etcd-dep
-Environment="IMAGE={{ .RegistryDomain }}/giantswarm/aws-attach-etcd-dep:b4050bd0f0079ef2f01bbccea39b0212b86ee264"
+Environment="IMAGE={{ .RegistryDomain }}/giantswarm/aws-attach-etcd-dep:ba6078639682ef65afc58fadfc1fb78962d4ef08"
 Environment="NAME=%p.service"
 Type=oneshot
 RemainAfterExit=yes
