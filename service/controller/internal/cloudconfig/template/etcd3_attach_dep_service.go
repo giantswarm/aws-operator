d82e96b8b1315c0d67b41f483169244a7a8eb953
calvix <vaclav@giantswarm.io>
2020-05-13 15:29:51 +0200

    'change-eni-static-ip-settings' (#2421)
    * 'change-eni-static-ip-settings'
    
    * 'gofmt'
    
    * 'custom version'
    
    * 'update image'
    
    * 'update image'
    
    * 'update image'
    
    * 'fix typo'
    
    * 'remove comments'
    
    * 'update image'
    
    * 'add extra eth1 config'
    
    * 'recreate asg recourse'
    
    * 'recreate asg recourse'
    
    * 'revert resource name, change eni name'
    
    * 'fix order'
    
    * 'add changelog entry'
    
    * 'fix changelog'
    
    Co-authored-by: Tim Schindler <xh3b4sd@gmail.com>

--- service/controller/internal/cloudconfig/template/etcd3_attach_dep_service.go
+++ service/controller/internal/cloudconfig/template/etcd3_attach_dep_service.go
@@ -8,12 +8,13 @@ After=network.target
 
 [Service]
 # image is from https://github.com/giantswarm/aws-attach-etcd-dep
-Environment="IMAGE={{ .RegistryDomain }}/giantswarm/aws-attach-etcd-dep:65a157aeb6db5cd4fa925efff792ef6fba4d02f3"
+Environment="IMAGE={{ .RegistryDomain }}/giantswarm/aws-attach-etcd-dep:5a1d732d48bea22c174825dc5ab9f403a1b7d27c"
 Environment="NAME=%p.service"
 Type=oneshot
 RemainAfterExit=yes
 ExecStart=/bin/bash -c "docker run --rm -i \
       -v /dev:/dev \
+      -v /etc/systemd/network:/etc/systemd/network \
       --privileged \
       --name ${NAME} \
       ${IMAGE} \
@@ -25,6 +26,9 @@ ExecStart=/bin/bash -c "docker run --rm -i \
       --volume-device-label=etcd \
       --volume-tag-key=Name \
       --volume-tag-value={{ .MasterEtcdVolumeName }}"
+ExecStartPost=/usr/bin/systemctl daemon-reload
+ExecStartPost=/usr/bin/systemctl restart systemd-networkd
+
 [Install]
 WantedBy=multi-user.target
 `
--- service/controller/resource/tccpn/template/params_main_eni.go
+++ service/controller/resource/tccpn/template/params_main_eni.go
@@ -1,7 +1,6 @@
 package template
 
 type ParamsMainENI struct {
-	IpAddress       string
 	Name            string
 	SecurityGroupID string
 	SubnetID        string
