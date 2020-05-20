1a60a603e65cb0ed76530f0fc1064ef36b310122
calvix <vaclav@giantswarm.io>
2020-05-20 14:56:32 +0200

    'remove tccp az check for eniconfigs'

--- service/controller/resource/eniconfigcrs/create.go
+++ service/controller/resource/eniconfigcrs/create.go
@@ -34,13 +34,6 @@ func (r *Resource) EnsureCreated(ctx context.Context, obj interface{}) error {
 		return nil
 	}
 
-	if len(cc.Spec.TenantCluster.TCCP.AvailabilityZones) == 0 {
-		r.logger.LogCtx(ctx, "level", "debug", "message", "tccp availability zones is not ready yet")
-		r.logger.LogCtx(ctx, "level", "debug", "message", "canceling resource")
-
-		return nil
-	}
-
 	// We need to configure our security group ID to all the ENIConfig CRs.
 	// Therefore we look it up from the controller context, where it is already
 	// available.
