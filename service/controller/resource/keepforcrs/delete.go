e766057cd0075b472437e7ac09765be3ea3e3162
Tim Schindler <xh3b4sd@gmail.com>
2020-05-21 22:29:48 +0200

    add keepforcrs handler

---
+++ service/controller/resource/keepforcrs/delete.go
@@ -0,0 +1,61 @@
+package keepforcrs
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/giantswarm/microerror"
+	"github.com/giantswarm/operatorkit/controller/context/finalizerskeptcontext"
+	"k8s.io/apimachinery/pkg/api/meta"
+	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+	"sigs.k8s.io/controller-runtime/pkg/client/apiutil"
+
+	"github.com/giantswarm/aws-operator/pkg/label"
+	"github.com/giantswarm/aws-operator/service/controller/key"
+)
+
+func (r *Resource) EnsureDeleted(ctx context.Context, obj interface{}) error {
+	cr, err := meta.Accessor(obj)
+	if err != nil {
+		return microerror.Mask(err)
+	}
+
+	var list *unstructured.UnstructuredList
+	{
+		gvk, err := apiutil.GVKForObject(r.newObjFunc(), r.k8sClient.Scheme())
+		if err != nil {
+			return microerror.Mask(err)
+		}
+		gvk.Kind += "List"
+
+		l := &unstructured.UnstructuredList{}
+		l.SetGroupVersionKind(gvk)
+
+		list = l
+	}
+
+	{
+		r.logger.LogCtx(ctx, "level", "debug", "message", fmt.Sprintf("finding objects of type %T for tenant cluster %#q", r.newObjFunc(), key.ClusterID(cr)))
+
+		err = r.k8sClient.CtrlClient().List(
+			ctx,
+			list,
+			client.InNamespace(cr.GetNamespace()),
+			client.MatchingLabels{label.Cluster: key.ClusterID(cr)},
+		)
+		if err != nil {
+			return microerror.Mask(err)
+		}
+
+		r.logger.LogCtx(ctx, "level", "debug", "message", fmt.Sprintf("found %d object(s) of type %T for tenant cluster %#q", len(list.Items), r.newObjFunc(), key.ClusterID(cr)))
+	}
+
+	if len(list.Items) != 0 {
+		r.logger.LogCtx(ctx, "level", "debug", "message", fmt.Sprintf("found object of type %T for tenant cluster %#q", r.newObjFunc(), key.ClusterID(cr)))
+		r.logger.LogCtx(ctx, "level", "debug", "message", "keeping finalizers")
+		finalizerskeptcontext.SetKept(ctx)
+	}
+
+	return nil
+}
---
+++ service/controller/resource/keepforcrs/resource.go
@@ -0,0 +1,64 @@
+package keepforcrs
+
+import (
+	"github.com/giantswarm/k8sclient/v3/pkg/k8sclient"
+	"github.com/giantswarm/microerror"
+	"github.com/giantswarm/micrologger"
+	"k8s.io/apimachinery/pkg/runtime"
+)
+
+const (
+	Name = "keepforcrs"
+)
+
+type Config struct {
+	K8sClient k8sclient.Interface
+	Logger    micrologger.Logger
+	// NewObjFunc is to return an instance of a pointer for the CR type that
+	// should be considered for keeping finalizers.
+	//
+	//     &infrastructurev1alpha2.AWSControlPlane{}
+	//     &infrastructurev1alpha2.AWSMachineDeployment{}
+	//
+	NewObjFunc func() runtime.Object
+}
+
+// Resource receives the runtime object of the underlying controller it is wired
+// into and keeps finalizers for that very controller in case the configured
+// runtime objects do still exist. This is to initialize deletion for the
+// following CRs.
+//
+//     watch         |    delete
+//     ---------------------------------------
+//     AWSCluster    |    AWSControlPlane
+//     AWSCluster    |    AWSMachineDeployment
+//
+type Resource struct {
+	k8sClient  k8sclient.Interface
+	logger     micrologger.Logger
+	newObjFunc func() runtime.Object
+}
+
+func New(config Config) (*Resource, error) {
+	if config.K8sClient == nil {
+		return nil, microerror.Maskf(invalidConfigError, "%T.K8sClient must not be empty", config)
+	}
+	if config.Logger == nil {
+		return nil, microerror.Maskf(invalidConfigError, "%T.Logger must not be empty", config)
+	}
+	if config.NewObjFunc == nil {
+		return nil, microerror.Maskf(invalidConfigError, "%T.NewObjFunc must not be empty", config)
+	}
+
+	r := &Resource{
+		k8sClient:  config.K8sClient,
+		logger:     config.Logger,
+		newObjFunc: config.NewObjFunc,
+	}
+
+	return r, nil
+}
+
+func (r *Resource) Name() string {
+	return Name
+}
