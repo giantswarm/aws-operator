8661ee1d5357779c24dea96f96561fca8d8403d1
Tim Schindler <xh3b4sd@gmail.com>
2020-05-21 09:39:12 +0200

    revert project version

--- pkg/project/project.go
+++ pkg/project/project.go
@@ -5,7 +5,7 @@ var (
 	gitSHA             = "n/a"
 	name        string = "aws-operator"
 	source      string = "https://github.com/giantswarm/aws-operator"
-	version            = "8.5.1-dev"
+	version            = "8.5.0-dev"
 )
 
 func Description() string {
