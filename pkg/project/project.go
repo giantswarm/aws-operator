6045aaa6c9fa83eaca00dd0fbeda268c7ac0bb50
Tim Schindler <xh3b4sd@gmail.com>
2020-05-27 13:26:54 +0200

    set dev version 8.6.2-xh3b4sd

--- pkg/project/project.go
+++ pkg/project/project.go
@@ -5,7 +5,7 @@ var (
 	gitSHA             = "n/a"
 	name        string = "aws-operator"
 	source      string = "https://github.com/giantswarm/aws-operator"
-	version            = "8.6.2-hama"
+	version            = "8.6.2-xh3b4sd"
 )
 
 func Description() string {
