4d7ccef7eabb6151cf9d4d80b9fb33c865c401f1
calvix <vaclav@giantswarm.io>
2020-06-25 16:33:07 +0200

    fix metric name

--- service/controller/resource/nodeautorepair/metrics.go
+++ service/controller/resource/nodeautorepair/metrics.go
@@ -6,25 +6,15 @@ import (
 
 const (
 	gaugeValue float64 = 1
-
-	// prometheusNamespace is the namespace to use for Prometheus metrics.
-	// See: https://godoc.org/github.com/prometheus/client_golang/prometheus#Opts
-	prometheusNamespace = "giantswarm"
-
-	// prometheusSubsystem is the subsystem to use for Prometheus metrics.
-	// See: https://godoc.org/github.com/prometheus/client_golang/prometheus#Opts
-	prometheusSubsystem = "node_auto_repair"
 )
 
 var (
 	nodeAutoRepairTermination = prometheus.NewGaugeVec(
 		prometheus.GaugeOpts{
-			Namespace: prometheusNamespace,
-			Subsystem: prometheusSubsystem,
-			Name:      "aws_operator_node_auto_repair_termination",
-			Help:      "Metric representing node termination due node auto repair feature.",
+			Name: "aws_operator_node_auto_repair_termination",
+			Help: "Metric representing node termination due node auto repair feature.",
 		},
-		[]string{"cluster_id", "node_name", "instance_id"},
+		[]string{"cluster_id", "terminated_node", "terminated_instance_id"},
 	)
 )
 
@@ -35,8 +25,8 @@ func init() {
 
 // reportNodeTermination is a utility function for updating metrics related to
 // node auto repair node termination.
-func reportNodeTermination(clusterID string, nodeName string, instance_id string) {
+func reportNodeTermination(clusterID string, nodeName string, instanceID string) {
 	nodeAutoRepairTermination.WithLabelValues(
-		clusterID, nodeName, instance_id,
+		clusterID, nodeName, instanceID,
 	).Set(gaugeValue)
 }
