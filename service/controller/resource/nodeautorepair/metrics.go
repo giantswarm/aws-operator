b9fc952b8df4be5c442720cb5339e0f62fc3a441
calvix <vaclav@giantswarm.io>
2020-06-26 10:09:09 +0200

    Update service/controller/resource/nodeautorepair/metrics.go
    Co-authored-by: Marian Steinbach <marian@giantswarm.io>

--- service/controller/resource/nodeautorepair/metrics.go
+++ service/controller/resource/nodeautorepair/metrics.go
@@ -12,7 +12,7 @@ var (
 	nodeAutoRepairTermination = prometheus.NewGaugeVec(
 		prometheus.GaugeOpts{
 			Name: "aws_operator_node_auto_repair_termination",
-			Help: "Metric representing node termination due node auto repair feature.",
+			Help: "Gauge representing node termination due to node auto repair feature.",
 		},
 		[]string{"cluster_id", "terminated_node", "terminated_instance_id"},
 	)
