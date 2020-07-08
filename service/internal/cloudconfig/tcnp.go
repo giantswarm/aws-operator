0cb1b9f29396266bfe66f9f2a0fa030445978f7b
Paweł Kopiczko <kopiczko@gmail.com>
2020-07-08 10:15:37 +0100

    update k8scloudconfig

--- service/internal/cloudconfig/tcnp.go
+++ service/internal/cloudconfig/tcnp.go
@@ -7,7 +7,7 @@ import (
 
 	infrastructurev1alpha2 "github.com/giantswarm/apiextensions/pkg/apis/infrastructure/v1alpha2"
 	"github.com/giantswarm/certs/v2/pkg/certs"
-	k8scloudconfig "github.com/giantswarm/k8scloudconfig/v6/pkg/template"
+	k8scloudconfig "github.com/giantswarm/k8scloudconfig/v7/pkg/template"
 	"github.com/giantswarm/microerror"
 	"github.com/giantswarm/randomkeys"
 	"golang.org/x/sync/errgroup"
