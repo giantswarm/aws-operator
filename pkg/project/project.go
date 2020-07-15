7269c2516c3180ccc19b312a7b613098f362e56d
wtty-fool <wtty.fool@gmail.com>
2020-07-15 14:16:56 +0200

    bump version to v8.7.4-dev

--- pkg/project/project.go
+++ pkg/project/project.go
@@ -5,7 +5,7 @@ var (
 	gitSHA             = "n/a"
 	name        string = "aws-operator"
 	source      string = "https://github.com/giantswarm/aws-operator"
-	version            = "8.7.3"
+	version            = "8.7.4-dev"
 )
 
 func Description() string {
