0e790b4ab7e7cb752f9c5899608962c9e75310f6
Antonia von den Driesch <antonia@giantswarm.io>
2020-08-12 22:17:34 +0900

    change back

--- pkg/project/project.go
+++ pkg/project/project.go
@@ -5,7 +5,7 @@ var (
 	gitSHA             = "n/a"
 	name        string = "aws-operator"
 	source      string = "https://github.com/giantswarm/aws-operator"
-	version            = "8.7.6-anvdd"
+	version            = "8.7.6-dev"
 )
 
 func Description() string {
