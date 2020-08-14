7b535a8388c7b3ec93a0fb83c3ac65b584055780
calvix <vaclav@giantswarm.io>
2020-08-14 12:07:05 +0200

    roll-to-8.7.6 (#2672)

--- pkg/project/project.go
+++ pkg/project/project.go
@@ -5,7 +5,7 @@ var (
 	gitSHA             = "n/a"
 	name        string = "aws-operator"
 	source      string = "https://github.com/giantswarm/aws-operator"
-	version            = "8.7.7-dev"
+	version            = "8.7.6-dev"
 )
 
 func Description() string {
