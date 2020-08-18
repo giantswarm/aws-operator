00d9fdea4cbe27e206cc06966c84477c9982a5b1
calvix <vaclav@giantswarm.io>
2020-08-18 09:29:02 +0200

    add key changes

--- service/controller/key/key.go
+++ service/controller/key/key.go
@@ -9,6 +9,7 @@ import (
 	"time"
 
 	"github.com/giantswarm/apiextensions/pkg/apis/infrastructure/v1alpha2"
+	infrastructurev1alpha2 "github.com/giantswarm/apiextensions/pkg/apis/infrastructure/v1alpha2"
 	"github.com/giantswarm/apiextensions/pkg/apis/provider/v1alpha1"
 	releasev1alpha1 "github.com/giantswarm/apiextensions/pkg/apis/release/v1alpha1"
 	k8scloudconfig "github.com/giantswarm/k8scloudconfig/v6/pkg/template"
