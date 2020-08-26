0aade0e08caa2d76977949bd2a04bf3dea53c7cf
Paweł Kopiczko <pawel@giantswarm.io>
2020-08-26 16:34:54 +0100

    Back to 8.8.1-dev

--- pkg/project/project.go
+++ pkg/project/project.go
@@ -5,7 +5,7 @@ var (
 	gitSHA             = "n/a"
 	name        string = "aws-operator"
 	source      string = "https://github.com/giantswarm/aws-operator"
-	version            = "8.8.1-pawel"
+	version            = "8.8.1-dev"
 )
 
 func Description() string {
