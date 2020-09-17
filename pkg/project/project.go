2b3d7f7aecf0e0d0fdce21fdf5d0b80f120501c4
Pau Rosello <paurosello@gmail.com>
2020-09-17 10:51:00 +0200

    change project version

--- pkg/project/project.go
+++ pkg/project/project.go
@@ -5,7 +5,7 @@ var (
 	gitSHA             = "n/a"
 	name        string = "aws-operator"
 	source      string = "https://github.com/giantswarm/aws-operator"
-	version            = "8.7.6"
+	version            = "8.7.7-disablecni"
 )
 
 func Description() string {
