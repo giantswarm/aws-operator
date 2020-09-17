1945c55bb8533e4ade4d8228b654d2c2c3a09a38
GitHub Action <action@github.com>
2020-09-17 09:38:40 +0000

    Bump version to 9.0.2-dev

--- pkg/project/project.go
+++ pkg/project/project.go
@@ -5,7 +5,7 @@ var (
 	gitSHA             = "n/a"
 	name        string = "aws-operator"
 	source      string = "https://github.com/giantswarm/aws-operator"
-	version            = "9.0.1"
+	version            = "9.0.2-dev"
 )
 
 func Description() string {
