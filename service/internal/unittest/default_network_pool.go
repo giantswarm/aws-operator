f42db34f9c46beffb9bb8dda6cc3f587ce803211
Tim Schindler <xh3b4sd@gmail.com>
2020-09-25 11:29:37 +0200

    consider custom network pool cidr during cluster creation (#2741)

---
+++ service/internal/unittest/default_network_pool.go
@@ -0,0 +1,20 @@
+package unittest
+
+import (
+	infrastructurev1alpha2 "github.com/giantswarm/apiextensions/v2/pkg/apis/infrastructure/v1alpha2"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+func DefaultNetworkPool(cidr string) infrastructurev1alpha2.NetworkPool {
+	cr := infrastructurev1alpha2.NetworkPool{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      DefaultClusterID,
+			Namespace: metav1.NamespaceDefault,
+		},
+		Spec: infrastructurev1alpha2.NetworkPoolSpec{
+			CIDRBlock: cidr,
+		},
+	}
+
+	return cr
+}
