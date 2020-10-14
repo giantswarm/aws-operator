f68e7bf9d837752c1c7a8759bfb7f6391233b8b2
Volodymyr Stoiko <me@volodymyrstoiko.com>
2020-10-14 14:46:48 +0300

    Add api/ingress records for TC into CP internal hosted zone

--- service/controller/resource/cphostedzone/resource.go
+++ service/controller/resource/cphostedzone/resource.go
@@ -27,8 +27,10 @@ type Config struct {
 type Resource struct {
 	logger micrologger.Logger
 
-	cachedCPHostedZoneID string
-	mutex                sync.Mutex
+	cachedCPHostedZoneID         string
+	cachedCPInternalHostedZoneID string
+
+	mutex sync.Mutex
 
 	route53Enabled bool
 }
@@ -61,68 +63,90 @@ func (r *Resource) addHostedZoneInfoToContext(ctx context.Context, cr infrastruc
 	}
 
 	{
-		hostedZoneID, err := r.lookup(ctx, cc.Client.ControlPlane.AWS.Route53, cr)
+		cpHostedZoneID, cpInternalHostedZoneID, err := r.lookup(ctx, cc.Client.ControlPlane.AWS.Route53, cr)
 		if err != nil {
 			return microerror.Mask(err)
 		}
 
-		cc.Status.ControlPlane.HostedZone.ID = hostedZoneID
+		cc.Status.ControlPlane.HostedZone.ID = cpHostedZoneID
+		cc.Status.ControlPlane.InternalHostedZone.ID = cpInternalHostedZoneID
 	}
 
 	return nil
 }
 
-func (r *Resource) lookup(ctx context.Context, client Route53, cr infrastructurev1alpha2.AWSCluster) (string, error) {
+func (r *Resource) lookup(ctx context.Context, client Route53, cr infrastructurev1alpha2.AWSCluster) (string, string, error) {
 	r.mutex.Lock()
 	defer r.mutex.Unlock()
 
-	// We check if we have CP HostedZone info cached.
+	// We check if we have CP public HostedZone info cached.
+	var cpHostedZoneID string
 	{
 		r.logger.LogCtx(ctx, "level", "debug", "message", "finding cached CP public HostedZone ID")
 
 		if r.cachedCPHostedZoneID != "" {
 			r.logger.LogCtx(ctx, "level", "debug", "message", fmt.Sprintf("found cached CP public HostedZone ID %#q", r.cachedCPHostedZoneID))
-			return r.cachedCPHostedZoneID, nil
+			cpHostedZoneID = r.cachedCPHostedZoneID
 		}
 
 		r.logger.LogCtx(ctx, "level", "debug", "message", "did not find cached CP public HostedZone ID")
 	}
 
-	// We do not have a cached CP public HostedZone Info for the requested
+	// We check if we have CP public HostedZone info cached.
+	var cpInternalHostedZoneID string
+	{
+		r.logger.LogCtx(ctx, "level", "debug", "message", "finding cached CP private HostedZone ID")
+
+		if r.cachedCPInternalHostedZoneID != "" {
+			r.logger.LogCtx(ctx, "level", "debug", "message", fmt.Sprintf("found cached CP private HostedZone ID %#q", r.cachedCPInternalHostedZoneID))
+			cpInternalHostedZoneID = r.cachedCPInternalHostedZoneID
+		}
+
+		r.logger.LogCtx(ctx, "level", "debug", "message", "did not find cached CP private HostedZone ID")
+	}
+
+	if cpHostedZoneID != "" && cpInternalHostedZoneID != "" {
+		return cpHostedZoneID, cpInternalHostedZoneID, nil
+	}
+
+	// We do not have a cached CP HostedZones Info for the requested
 	// installation. So we look it up.
-	var cpHostedZoneID string
-	{
-		r.logger.LogCtx(ctx, "level", "debug", "message", "finding CP public HostedZone ID")
+	r.logger.LogCtx(ctx, "level", "debug", "message", "finding CP HostedZone IDs")
 
-		hostedZonesInput := &route53.ListHostedZonesByNameInput{}
+	hostedZonesInput := &route53.ListHostedZonesByNameInput{}
 
-		o, err := client.ListHostedZonesByName(hostedZonesInput)
-		if err != nil {
-			return "", microerror.Mask(err)
-		}
-
-		baseDomain := fmt.Sprintf("%s.", key.ClusterBaseDomain(cr))
-
-		for _, zone := range o.HostedZones {
-			if *zone.Name == baseDomain && !*zone.Config.PrivateZone {
-				cpHostedZoneID = *zone.Id
-				break
-			}
-		}
-
-		if cpHostedZoneID == "" {
-			return "", microerror.Maskf(executionFailedError, "failed to find CP public HostedZone ID for base domain %#q", baseDomain)
-		}
-
-		r.logger.LogCtx(ctx, "level", "debug", "message", fmt.Sprintf("found CP public HostedZone ID %#q", cpHostedZoneID))
+	o, err := client.ListHostedZonesByName(hostedZonesInput)
+	if err != nil {
+		return "", "", microerror.Mask(err)
 	}
 
-	// At this point we found a public HostedZone ID info and we cache it.
-	{
-		r.logger.LogCtx(ctx, "level", "debug", "message", "caching CP public HostedZone ID")
-		r.cachedCPHostedZoneID = cpHostedZoneID
-		r.logger.LogCtx(ctx, "level", "debug", "message", "cached CP public HostedZone ID")
+	baseDomain := fmt.Sprintf("%s.", key.ClusterBaseDomain(cr))
+
+	for _, zone := range o.HostedZones {
+		if *zone.Name == baseDomain && !*zone.Config.PrivateZone {
+			r.logger.LogCtx(ctx, "level", "debug", "message", fmt.Sprintf("found CP public HostedZone ID %#q", cpHostedZoneID))
+			cpHostedZoneID = *zone.Id
+
+			r.logger.LogCtx(ctx, "level", "debug", "message", "caching CP public HostedZone ID")
+			r.cachedCPHostedZoneID = cpHostedZoneID
+			r.logger.LogCtx(ctx, "level", "debug", "message", "cached CP public HostedZone ID")
+
+		}
+
+		if *zone.Name == baseDomain && *zone.Config.PrivateZone {
+			r.logger.LogCtx(ctx, "level", "debug", "message", fmt.Sprintf("found CP private HostedZone ID %#q", cpInternalHostedZoneID))
+			cpInternalHostedZoneID = *zone.Id
+
+			r.logger.LogCtx(ctx, "level", "debug", "message", "caching CP private HostedZone ID")
+			r.cachedCPInternalHostedZoneID = cpInternalHostedZoneID
+			r.logger.LogCtx(ctx, "level", "debug", "message", "cached CP private HostedZone ID")
+		}
 	}
 
-	return cpHostedZoneID, nil
+	// Fail only if public hosted zone is missing as having private hosted zone in CP is not a requirement
+	if cpHostedZoneID == "" {
+		return "", "", microerror.Maskf(executionFailedError, "failed to find CP public HostedZone ID for base domain %#q", baseDomain)
+	}
+
+	return cpHostedZoneID, cpInternalHostedZoneID, nil
 }
--- service/controller/resource/tccpf/template/template_main_record_sets.go
+++ service/controller/resource/tccpf/template/template_main_record_sets.go
@@ -6,11 +6,31 @@ const TemplateMainRecordSets = `
   GuestNSRecordSet:
     Type: 'AWS::Route53::RecordSet'
     Properties:
-      HostedZoneId: '{{ .RecordSets.ControlPlanePublicHostedZoneID }}'
+      HostedZoneId: '{{ .RecordSets.ControlPlaneHostedZoneID }}'
       Name: '{{ .RecordSets.ClusterID }}.k8s.{{ .RecordSets.BaseDomain }}.'
       Type: 'NS'
       TTL: '300'
-      ResourceRecords: !Split [ ',', '{{ .RecordSets.GuestHostedZoneNameServers }}' ]
+      ResourceRecords: !Split [ ',', '{{ .RecordSets.TenantHostedZoneNameServers }}' ]
+{{- if ne .RecordSets.ControlPlaneInternalHostedZoneID "" -}}
+  TenantAPIServerRecordSet:
+    Type: 'AWS::Route53::RecordSet'
+    Properties:
+      HostedZoneId: '{{ .RecordSets.ControlPlaneInternalHostedZoneID }}'
+      Name: 'api.{{ .RecordSets.ClusterID }}.k8s.{{ .RecordSets.BaseDomain }}.'
+      Type: 'CNAME'
+      TTL: '300'
+      ResourceRecords:
+	    - '{{.RecordSets.TenantAPIPublicLoadBalancer }}'
+  TenantIngressRecordSet:
+    Type: 'AWS::Route53::RecordSet'
+    Properties:
+      HostedZoneId: '{{ .RecordSets.ControlPlaneInternalHostedZoneID }}'
+      Name: '*.{{ .RecordSets.ClusterID }}.k8s.{{ .RecordSets.BaseDomain }}.'
+      Type: 'CNAME'
+      TTL: '300'
+      ResourceRecords:
+	    - 'ingress.{{ .RecordSets.ClusterID }}.k8s.{{ .RecordSets.BaseDomain }}'
+{{- end -}}
 {{- end -}}
 {{- end -}}
 `
