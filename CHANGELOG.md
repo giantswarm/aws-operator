6964ad0e6c96ba349f4fce268cc0d72100f564c9
Pau Rosello <paurosello@gmail.com>
2020-10-27 11:54:23 +0100

    Use alpha instead of beta

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -10,7 +10,7 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 
 ### Added
 
-- Annotation `beta.giantswarm.io/aws-metadata-v2` to enable AWS Metadata API v2
+- Annotation `alpha.giantswarm.io/aws-metadata-v2` to enable AWS Metadata API v2
 
 ## [9.1.3] - 2020-10-21
 
--- pkg/annotation/annotation.go
+++ pkg/annotation/annotation.go
@@ -4,5 +4,5 @@ const (
 	Docs                    = "giantswarm.io/docs"
 	InstanceID              = "aws-operator.giantswarm.io/instance"
 	MachineDeploymentSubnet = "machine-deployment.giantswarm.io/subnet"
-	AWSMetadata             = "beta.giantswarm.io/aws-metadata-v2"
+	AWSMetadata             = "alpha.giantswarm.io/aws-metadata-v2"
 )
