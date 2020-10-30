7523e32044b5a1912e19c9d1b3bf59b9ca40d9c2
calvix <vaclav@giantswarm.io>
2020-10-30 09:36:59 +0100

    add alpha

--- pkg/annotation/annotation.go
+++ pkg/annotation/annotation.go
@@ -5,6 +5,6 @@ const (
 	InstanceID              = "aws-operator.giantswarm.io/instance"
 	MachineDeploymentSubnet = "machine-deployment.giantswarm.io/subnet"
 	AWSMetadata             = "alpha.giantswarm.io/aws-metadata-v2"
-	UpdateMaxBatchSize      = "aws.giantswarm.io/update-max-batch-size"
-	UpdatePauseTime         = "aws.giantswarm.io/update-pause-time"
+	UpdateMaxBatchSize      = "alpha.aws.giantswarm.io/update-max-batch-size"
+	UpdatePauseTime         = "alpha.aws.giantswarm.io/update-pause-time"
 )
