931af315b4740d309c83f0aff7e28b8c5b87be08
calvix <vaclav@giantswarm.io>
2020-10-30 09:46:35 +0100

    order

--- pkg/annotation/annotation.go
+++ pkg/annotation/annotation.go
@@ -4,7 +4,6 @@ const (
 	Docs                    = "giantswarm.io/docs"
 	InstanceID              = "aws-operator.giantswarm.io/instance"
 	MachineDeploymentSubnet = "machine-deployment.giantswarm.io/subnet"
-	// Annotation used to enable feature to terminate unhealthy nodes on a cluster CR.
-	NodeTerminateUnhealthy = "alpha.node.giantswarm.io/terminate-unhealthy"
+	NodeTerminateUnhealthy  = "alpha.node.giantswarm.io/terminate-unhealthy"
 	AWSMetadata             = "alpha.giantswarm.io/aws-metadata-v2"
 )
