67ec35963b367cfea8febd700005e941e5bd7772
Paweł Kopiczko <kopiczko@gmail.com>
2020-11-02 14:43:21 +0000

    Set custom version

--- pkg/project/project.go
+++ pkg/project/project.go
@@ -5,7 +5,7 @@ var (
 	gitSHA             = "n/a"
 	name        string = "aws-operator"
 	source      string = "https://github.com/giantswarm/aws-operator"
-	version            = "9.1.4-dev"
+	version            = "9.1.4-pawel-qps"
 )
 
 func Description() string {
