eb6a60f008dbb8d20ed72bf26d64465b914567ca
GitHub Action <action@github.com>
2020-11-02 09:43:13 +0000

    Bump version to 8.7.11-dev

--- pkg/project/project.go
+++ pkg/project/project.go
@@ -5,7 +5,7 @@ var (
 	gitSHA             = "n/a"
 	name        string = "aws-operator"
 	source      string = "https://github.com/giantswarm/aws-operator"
-	version            = "8.7.10"
+	version            = "8.7.11-dev"
 )
 
 func Description() string {
