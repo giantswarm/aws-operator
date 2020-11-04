c5b0f9b95879d0af5524ea11bc9436521f06631d
Pau Rosello <paurosello@gmail.com>
2020-11-04 12:02:47 +0100

    update tags

--- pkg/annotation/annotation.go
+++ pkg/annotation/annotation.go
@@ -5,6 +5,6 @@ const (
 	InstanceID              = "aws-operator.giantswarm.io/instance"
 	MachineDeploymentSubnet = "machine-deployment.giantswarm.io/subnet"
 	NodeTerminateUnhealthy  = "alpha.node.giantswarm.io/terminate-unhealthy"
-	AWSMetadata             = "alpha.giantswarm.io/aws-metadata-v2"
-	AWSSubnetSize           = "alpha.giantswarm.io/aws-subnet-size"
+	AWSMetadata             = "alpha.aws.giantswarm.io/metadata-v2"
+	AWSSubnetSize           = "alpha.aws.giantswarm.io/aws-subnet-size"
 )
