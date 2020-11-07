534506623bec1b41acc05d5d32f717a0c97bb785
calvix <vaclav@giantswarm.io>
2020-11-07 08:48:02 +0100

    set-max-duration-to-hour

--- service/controller/key/machine_deployment.go
+++ service/controller/key/machine_deployment.go
@@ -262,9 +262,19 @@ func MachineDeploymentMinInstanceInServiceFromMaxBatchSize(maxBatchSize string, 
 }
 
 // MachineDeploymentPauseTimeIsValid checks if the value is in proper ISO 8601 duration format
+// and ensure that the duration is not bigger than 1 Hour
 func MachineDeploymentPauseTimeIsValid(val string) bool {
-	_, err := iso8601.ParseDuration(val)
-	return err == nil
+	d, err := iso8601.ParseDuration(val)
+	if err != nil {
+		return false
+	}
+
+	// AWS limits the duration to 1 hour
+	if d.Hours() > 1.0 {
+		return false
+	}
+
+	return true
 }
 
 func MachineDeploymentScalingMax(cr infrastructurev1alpha2.AWSMachineDeployment) int {
