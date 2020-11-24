5ad85bf93cb5ffb02bea497f847272b1885c1e5b
vol <me@volodymyrstoiko.com>
2020-11-24 15:13:10 +0200

    voldev version

--- pkg/project/project.go
+++ pkg/project/project.go
@@ -5,7 +5,7 @@ var (
 	gitSHA             = "n/a"
 	name        string = "aws-operator"
 	source      string = "https://github.com/giantswarm/aws-operator"
-	version            = "9.2.1-dev"
+	version            = "9.2.1-voldev"
 )
 
 func Description() string {
