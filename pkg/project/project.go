0163252b6062621551bfe75a5921a9a7673fadf2
kubasobon <wtty.fool@gmail.com>
2021-01-14 12:46:36 +0100

    add unique config version

--- pkg/project/project.go
+++ pkg/project/project.go
@@ -5,7 +5,7 @@ var (
 	gitSHA             = "n/a"
 	name        string = "aws-operator"
 	source      string = "https://github.com/giantswarm/aws-operator"
-	version            = "9.3.6-voldev"
+	version            = "9.3.6-jakub"
 )
 
 func Description() string {
