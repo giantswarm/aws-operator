14f64020103d13e613e9eb678a4571bb54d56d02
Tim Schindler <xh3b4sd@gmail.com>
2021-01-18 13:31:55 +0100

    Revert "set dev version 9.3.6-xh3b4sd"
    This reverts commit 1510e3810f97feaa4bcacf3c32f01eccd90293c1.

--- pkg/project/project.go
+++ pkg/project/project.go
@@ -5,7 +5,7 @@ var (
 	gitSHA             = "n/a"
 	name        string = "aws-operator"
 	source      string = "https://github.com/giantswarm/aws-operator"
-	version            = "9.3.6-xh3b4sd"
+	version            = "9.3.6-dev"
 )
 
 func Description() string {
