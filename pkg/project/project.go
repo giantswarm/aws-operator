eaf98673c4b63e48cdeed0ac4b6bcf51da41cd59
Paweł Kopiczko <pawel@giantswarm.io>
2021-01-26 05:42:51 +0000

    Do not merge: Frozen master

--- pkg/project/project.go
+++ pkg/project/project.go
@@ -5,7 +5,7 @@ var (
 	gitSHA             = "n/a"
 	name        string = "aws-operator"
 	source      string = "https://github.com/giantswarm/aws-operator"
-	version            = "10.0.1-dev"
+	version            = "10.0.1-pawel"
 )
 
 func Description() string {
