e7c1b1dac9a8cae4f824472e722c869043dcd073
Pau Rosello <paurosello@gmail.com>
2021-02-01 14:03:16 +0100

    reverst version

--- pkg/project/project.go
+++ pkg/project/project.go
@@ -5,7 +5,7 @@ var (
 	gitSHA             = "n/a"
 	name        string = "aws-operator"
 	source      string = "https://github.com/giantswarm/aws-operator"
-	version            = "10.0.1-cni"
+	version            = "10.0.1-dev"
 )
 
 func Description() string {
