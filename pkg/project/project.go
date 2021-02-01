3bf75535b89490859014efdbef37f3520e093b2c
pipo02mix <fernando@giantswarm.io>
2021-02-01 15:01:58 +0100

    Add changelog entry

--- pkg/project/project.go
+++ pkg/project/project.go
@@ -5,7 +5,7 @@ var (
 	gitSHA             = "n/a"
 	name        string = "aws-operator"
 	source      string = "https://github.com/giantswarm/aws-operator"
-	version            = "10.1.1"
+	version            = "10.0.1-dev"
 )
 
 func Description() string {
