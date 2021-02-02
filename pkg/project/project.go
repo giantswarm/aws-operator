f14ec8865d89efb92ee7dfbb21d45bd1e5a139b2
Pau Rosello <paurosello@gmail.com>
2021-02-02 11:34:43 +0100

    update version to final

--- pkg/project/project.go
+++ pkg/project/project.go
@@ -5,7 +5,7 @@ var (
 	gitSHA             = "n/a"
 	name        string = "aws-operator"
 	source      string = "https://github.com/giantswarm/aws-operator"
-	version            = "9.3.6-cni"
+	version            = "9.3.6"
 )
 
 func Description() string {
