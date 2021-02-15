ca1ab5b4ea69ea05555194f0467b7bc922443eb2
Pau Rosello <paurosello@gmail.com>
2021-02-15 16:22:55 +0100

    set project version

--- pkg/project/project.go
+++ pkg/project/project.go
@@ -5,7 +5,7 @@ var (
 	gitSHA             = "n/a"
 	name        string = "aws-operator"
 	source      string = "https://github.com/giantswarm/aws-operator"
-	version            = "9.3.7"
+	version            = "9.3.8"
 )
 
 func Description() string {
