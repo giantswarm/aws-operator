25acb9395c35d3282d2f1170592eb752b2ad6804
GitHub Action <action@github.com>
2021-05-25 11:46:19 +0000

    Bump version to 10.4.1-dev

--- pkg/project/project.go
+++ pkg/project/project.go
@@ -5,7 +5,7 @@ var (
 	gitSHA             = "n/a"
 	name        string = "aws-operator"
 	source      string = "https://github.com/giantswarm/aws-operator"
-	version            = "10.4.0"
+	version            = "10.4.1-dev"
 )
 
 func Description() string {
