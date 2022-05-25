2c7f25a0b547d0f4a827791909144d5d2ece5db5
GitHub Action <action@github.com>
2022-05-25 15:11:37 +0000

    Bump version to 11.12.1-dev

--- pkg/project/project.go
+++ pkg/project/project.go
@@ -5,7 +5,7 @@ var (
 	gitSHA             = "n/a"
 	name        string = "aws-operator"
 	source      string = "https://github.com/giantswarm/aws-operator"
-	version            = "11.12.0"
+	version            = "11.12.1-dev"
 )
 
 func Description() string {
