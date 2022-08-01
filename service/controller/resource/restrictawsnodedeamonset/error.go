b93a9e8b272a482e8de25b0a8c881de42c767705
Christian Bianchi <whites11@gmail.com>
2022-08-01 15:31:57 +0200

    fix typo

--- service/controller/resource/restrictawsnodedeamonset/error.go
+++ service/controller/resource/restrictawsnodedeamonset/error.go
@@ -12,12 +12,3 @@ var invalidConfigError = &microerror.Error{
 func IsInvalidConfig(err error) bool {
 	return microerror.Cause(err) == invalidConfigError
 }
-
-var invalidProviderIDError = &microerror.Error{
-	Kind: "invalidProviderID",
-}
-
-// IsInvalidProviderID asserts invalidConfigError.
-func IsInvalidProviderID(err error) bool {
-	return microerror.Cause(err) == invalidProviderIDError
-}
