93c2c8c1a0a17ab2355b651e214d62d2ffb3aded
Nick Jüttner <nick@giantswarm.io>
2022-09-28 13:39:47 +0200

    adding systemd units

---
+++ service/internal/cloudconfig/template/instance_health_check.go
@@ -0,0 +1,136 @@
+package template
+
+const MasterInstanceLifeCycleCompletionService = `
+[Unit]
+Description=master-instance-lifecyle-completion job
+After=docker.service etcd3.service k8s-kubelet.service k8s-setup-network-env.service
+Requires=docker.service etcd3.service k8s-kubelet.service k8s-setup-network-env.service
+[Service]
+ExecStartPre=/bin/bash -c "while [ ! -f /opt/bin/master-instance-healthcheck ]; do /opt/bin/master-instance-healtcheck && sleep 1; done"
+ExecStart=/opt/bin/master-instance-lifecycle-completion
+[Install]
+WantedBy=multi-user.target
+`
+const MasterInstanceHealtCheckService = `
+[Unit]
+Description=master-instance-healthcheck job
+After=docker.service etcd3.service k8s-kubelet.service k8s-setup-network-env.service
+Requires=docker.service etcd3.service k8s-kubelet.service k8s-setup-network-env.service
+[Service]
+Type=oneshot
+ExecStart=/opt/bin/master-instance-healthcheck
+[Install]
+WantedBy=multi-user.target
+`
+
+const MasterInstanceHealtCheckTimer = `
+[Unit]
+Description=Execute master-instance-healthcheck every 5 minutes
+[Timer]
+OnCalendar=*:0/5
+[Install]
+WantedBy=multi-user.target
+`
+
+const MasterInstanceHealthCheck = `#!/bin/bash
+set -o errexit
+set -o nounset
+set -o pipefail
+
+# AWS Metadata
+export INSTANCEID=$(curl -s http://169.254.169.254/latest/meta-data/instance-id 2> /dev/null)
+export LOCALIP=$(curl -s http://169.254.169.254/latest/meta-data/local-ipv4 2> /dev/null)
+
+# Color
+export Red=$'\033[1;31m'
+export Green=$'\033[1;32m'
+export Yellow=$'\033[1;33m'
+export NoColor=$'\033[0m'
+
+
+function retry() {
+  local max_attempts=${ATTEMPTS-5}
+  local timeout=${TIMEOUT-5}
+  local attempt=0
+  local exitCode=0
+
+  while [[ $attempt < $max_attempts ]]
+  do
+    "$@"
+    exitCode=$?
+
+    if [[ $exitCode == 0 ]]
+    then
+      break
+    fi
+
+    echo "${Red}Failed to get status from $2.${NoColor} Retrying in $timeout.. seconds" 1>&2
+    sleep $timeout
+    attempt=$(( attempt + 1 ))
+    timeout=$(( timeout * 2 ))
+  done
+
+  if [[ $exitCode != 0 ]]
+  then
+    echo "Mark EC2 instance ${Yellow}$INSTANCEID${NoColor} as ${Red}UNHEALTHY${NoColor}"
+    docker run --rm -it amazon/aws-cli autoscaling set-instance-health --instance-id $INSTANCEID --health-status Unhealthy
+    return $exitCode
+  fi
+
+  echo "STATUS is ${Green}OK${NoColor} for $2"
+  return $exitCode
+}
+
+# Checking etcd
+function etcd_status {
+  local statuscode
+
+  statuscode=$(ETCDCTL_API=3 etcdctl \
+    --cert /etc/kubernetes/ssl/etcd/client-crt.pem \
+    --key /etc/kubernetes/ssl/etcd/client-key.pem \
+    --cacert /etc/kubernetes/ssl/etcd/client-ca.pem \
+    --endpoints "127.0.0.1:2379" endpoint health --write-out json | jq .[].health)
+
+  if [ $statuscode = "true" ]; then
+     return 0
+  fi
+  return 1
+}
+
+function kubelet_status {
+  local statuscode
+
+  statuscode=$(curl -k -LI https://$LOCALIP:10250/metrics -o /dev/null -w '%{http_code}\n' -s)
+
+  if [ $statuscode -eq 200 ]; then
+     return 0
+  fi
+  return 1
+}
+
+function apiserver_status {
+  local statuscode
+
+  statuscode=$(curl -k -LI https://$LOCALIP/healthz -o /dev/null -w '%{http_code}\n' -s)
+
+  if [ $statuscode -eq 200 ]; then
+    return 0
+  fi
+  return 1
+}
+
+retry etcd_status "ETCD" || true
+retry kubelet_status "KUBELET" || true
+retry apiserver_status "APISERVER" || true
+`
+
+const MasterInstanceLifeCycle = `#!/bin/bash
+set -o errexit
+set -o nounset
+set -o pipefail
+
+# AWS Metadata
+export INSTANCEID=$(curl -s http://169.254.169.254/latest/meta-data/instance-id 2> /dev/null)
+
+docker run --rm -it amazon/aws-cli autoscaling complete-lifecycle-action --auto-scaling-group-name $TODO --lifecycle-hook-name ControlPlaneLaunching --instance-id $INSTANCEID --lifecycle-action-result CONTINUE
+`
