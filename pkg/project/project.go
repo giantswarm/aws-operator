3ebb165cac46c0de0a00682beb568648e01a9930
Pau <paurosello@gmail.com>
2022-11-04 10:05:48 +0100

    change selector

--- pkg/project/project.go
+++ pkg/project/project.go
@@ -5,7 +5,7 @@ var (
 	gitSHA             = "n/a"
 	name        string = "aws-operator"
 	source      string = "https://github.com/giantswarm/aws-operator"
-	version            = "14.0.1-dev"
+	version            = "14.0.1-iamrole"
 )
 
 func Description() string {
