1fdd4349f8629133f547c06543a1ce4d57d32eeb
Pau <paurosello@gmail.com>
2023-01-19 13:02:39 +0100

    new project

--- pkg/project/project.go
+++ pkg/project/project.go
@@ -5,7 +5,7 @@ var (
 	gitSHA             = "n/a"
 	name        string = "aws-operator"
 	source      string = "https://github.com/giantswarm/aws-operator"
-	version            = "14.4.0"
+	version            = "14.4.1-dev"
 )
 
 func Description() string {
