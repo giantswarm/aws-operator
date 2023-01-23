31c867b779eeca6f8cd39699e5a5c6974eb0e4b5
Pau <paurosello@gmail.com>
2023-01-23 10:33:13 +0100

    Fix "string has 3 occurrences, make it a constant"

--- service/internal/cloudconfig/tcnp.go
+++ service/internal/cloudconfig/tcnp.go
@@ -187,14 +187,16 @@ func (t *TCNP) NewTemplates(ctx context.Context, obj interface{}) ([]string, err
 	hasCilium, err := key.HasCilium(&cl)
 	var awsCNIPrefix bool
 
+	true_value := "true"
+
 	if !hasCilium {
 		{
-			if v, ok := cl.GetAnnotations()[annotation.AWSCNIPrefixDelegation]; ok && v == "true" {
+			if v, ok := cl.GetAnnotations()[annotation.AWSCNIPrefixDelegation]; ok && v == true_value {
 				awsCNIPrefix = true
 			}
 		}
 		{
-			if v, ok := md.GetAnnotations()[annotation.AWSCNIPrefixDelegation]; ok && v == "true" {
+			if v, ok := md.GetAnnotations()[annotation.AWSCNIPrefixDelegation]; ok && v == true_value {
 				awsCNIPrefix = true
 			}
 		}
