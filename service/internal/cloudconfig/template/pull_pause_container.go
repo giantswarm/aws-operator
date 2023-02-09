49a0e7149b96df83242825517b67b95ef8ab3103
Christian Bianchi <whites11@gmail.com>
2023-02-09 16:30:09 +0100

    add pause image pull unit

--- service/internal/cloudconfig/template/pull_pause_container.go
+++ service/internal/cloudconfig/template/pull_pause_container.go
@@ -3,12 +3,12 @@ package template
 const PullPauseContainer = `
 [Unit]
 Description=Pull pause container with containerd using docker image pull secret
-After=containerd.service
-Wants=containerd.service
+After=docker.service
+Wants=docker.service
 
 [Service]
 Type=oneshot
-ExecStart=/opt/bin/pull-image docker.io/giantswarm/pause:3.7'
+ExecStart=/opt/bin/pull-image docker.io/giantswarm/pause:3.7
 Restart=on-failure
 
 [Install]
