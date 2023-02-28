c53b0da14b4989214be650f29f30a3c615ec0224
Nick Jüttner <nick@giantswarm.io>
2023-02-28 15:49:15 +0100

    add etcd healthcheck

--- service/internal/cloudconfig/template/lifecycle_continue.go
+++ service/internal/cloudconfig/template/lifecycle_continue.go
@@ -16,7 +16,6 @@ WantedBy=multi-user.target
 `
 
 const MasterInstanceLifecycleContinue = `#!/bin/bash
-set -o errexit
 set -o nounset
 set -o pipefail
 
@@ -24,16 +23,14 @@ set -o pipefail
 export INSTANCEID=$(curl -s http://169.254.169.254/latest/meta-data/instance-id 2> /dev/null)
 
 # AWS Autoscaling Group Name
-export AUTOSCALINGGROUP=$(docker run --rm {{ .RegistryDomain }}/giantswarm/awscli:2.7.35 autoscaling describe-auto-scaling-instances --instance-ids=$INSTANCEID --query 'AutoScalingInstances[*].AutoScalingGroupName' --output text)
+export AUTOSCALINGGROUP=$(docker run --rm docker.io/giantswarm/awscli:2.7.35 autoscaling describe-auto-scaling-instances --instance-ids=$INSTANCEID --query 'AutoScalingInstances[*].AutoScalingGroupName' --output text)
 
-output=$(docker run --rm -i {{ .RegistryDomain }}/giantswarm/awscli:2.7.35 autoscaling complete-lifecycle-action --auto-scaling-group-name $AUTOSCALINGGROUP --lifecycle-hook-name ControlPlaneLaunching --instance-id $INSTANCEID --lifecycle-action-result CONTINUE)
-if [ $? -eq 0 ]; then
-    exit 0
-fi
+output=$(docker run --rm -i docker.io/giantswarm/awscli:2.7.35 autoscaling complete-lifecycle-action --auto-scaling-group-name $AUTOSCALINGGROUP --lifecycle-hook-name ControlPlaneLaunching --instance-id $INSTANCEID --lifecycle-action-result CONTINUE 2>&1 > /dev/null)
 
 # We ignore the following error: An error occurred (ValidationError) when calling the CompleteLifecycleAction operation: No active Lifecycle Action found with instance ID i-0f9f9f9f9f9f9f9f9
 # This happens when the lifecycle hook is already completed.
-if [[ $output == *"No active Lifecycle Action found"* ]];
+if [[ $output != *"No active Lifecycle Action found"* ]]
+then
     echo "Successfully completed lifecycle action. Master instance is ready receiving traffic."
     exit 0
 else
@@ -43,7 +40,6 @@ fi
 `
 
 const ETCDHealthCheck = `#!/bin/bash
-set -o errexit
 set -o nounset
 set -o pipefail
 
