c6d04d1cafd2d8f6b9a7b32e77445a9d13641aa0
Pau <paurosello@gmail.com>
2023-03-01 11:48:19 +0100

    improve systemd unit

--- service/internal/cloudconfig/template/lifecycle_continue.go
+++ service/internal/cloudconfig/template/lifecycle_continue.go
@@ -1,10 +1,14 @@
 package template
 
+// Startlimitburst is set to 70 because the hook has a timelimit of 17 minutes. (60/15sec retry)*17 = 68
+// StartLimitIntervalSec is set to 17 minutes as anyways the hook will be sent automatically
 const MasterInstanceLifecycleContinueService = `
 [Unit]
 Description=master-instance-lifecycle-continue job
 Requires=network.target
 After=network.target
+StartLimitIntervalSec=1020
+StartLimitBurst=68
 [Service]
 Type=simple
 Restart=on-failure
@@ -16,6 +20,7 @@ WantedBy=multi-user.target
 `
 
 const MasterInstanceLifecycleContinue = `#!/bin/bash
+set -o errexit
 set -o nounset
 set -o pipefail
 
@@ -27,10 +32,7 @@ export AUTOSCALINGGROUP=$(docker run --rm {{ .RegistryDomain }}/giantswarm/awscl
 
 output=$(docker run --rm -i {{ .RegistryDomain }}/giantswarm/awscli:2.7.35 autoscaling complete-lifecycle-action --auto-scaling-group-name $AUTOSCALINGGROUP --lifecycle-hook-name ControlPlaneLaunching --instance-id $INSTANCEID --lifecycle-action-result CONTINUE 2>&1 > /dev/null)
 
-# We ignore the following error: An error occurred (ValidationError) when calling the CompleteLifecycleAction operation: No active Lifecycle Action found with instance ID i-0f9f9f9f9f9f9f9f9
-# This happens when the lifecycle hook is already completed.
-if [[ $output != *"No active Lifecycle Action found"* ]]
-then
+if [ $? == 0 ]; then
     echo "Successfully completed lifecycle action. Master instance is ready receiving traffic."
     exit 0
 else
@@ -40,6 +42,7 @@ fi
 `
 
 const ETCDHealthCheck = `#!/bin/bash
+set -o errexit
 set -o nounset
 set -o pipefail
 
@@ -49,11 +52,11 @@ statuscode=$(ETCDCTL_API=3 etcdctl \
     --cacert /etc/kubernetes/ssl/etcd/client-ca.pem \
     --endpoints "127.0.0.1:2379" endpoint health --write-out json | jq .[].health)
 
-  if [ $statuscode = "true" ]; then
-     echo "ETCD is healthy."
-     exit 0
-  fi
+if [ $statuscode = "true" ]; then 
+  echo "ETCD is healthy."
+  exit 0
+fi
 
-  echo "ETCD is not healthy yet."
-  exit 1
+echo "ETCD is not healthy yet."
+exit 1
 `
