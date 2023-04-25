fcc74ad37d6499a7f32e64671627e6dd409094bf
Nick Jüttner <nick@giantswarm.io>
2023-04-25 11:09:36 +0200

    allow to enable ACLs for a S3 buckets

--- service/controller/resource/s3bucket/create.go
+++ service/controller/resource/s3bucket/create.go
@@ -37,7 +37,7 @@ func (r *Resource) ApplyCreateChange(ctx context.Context, obj, createChange inte
 
 		{
 			i := &s3.CreateBucketInput{
-				ACL:             aws.String(s3.BucketCannedACLPrivate),
+				ACL:             aws.String(s3.PermissionFullControl),
 				Bucket:          aws.String(bucketInput.Name),
 				ObjectOwnership: aws.String(s3.ObjectOwnershipBucketOwnerPreferred),
 			}
