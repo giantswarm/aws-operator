fe21a11f8c474c3dd118024e1a291d8d11967842
Christian Bianchi <whites11@gmail.com>
2023-04-26 15:38:57 +0200

    keep aws cni subnets on upgrade to v19

--- service/controller/resource/tccp/create_test.go
+++ service/controller/resource/tccp/create_test.go
@@ -198,7 +198,7 @@ func Test_Controller_Resource_TCCP_Template_Render(t *testing.T) {
 
 			cl := unittest.DefaultCAPIClusterWithLabels(tc.cr.Name, map[string]string{})
 
-			params, err := r.newParamsMain(tc.ctx, cl, tc.cr, time.Time{})
+			params, err := r.newParamsMain(tc.ctx, cl, tc.cr, time.Time{}, true)
 			if err != nil {
 				t.Fatal(err)
 			}
