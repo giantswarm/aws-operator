aab51c6a39db6d4a30a830c2e28986733ce787a5
taylorbot <dev@giantswarm.io>
2023-05-03 07:53:45 +0000

    Release v14.13.0

--- pkg/project/project.go
+++ pkg/project/project.go
@@ -5,7 +5,7 @@ var (
 	gitSHA             = "n/a"
 	name        string = "aws-operator"
 	source      string = "https://github.com/giantswarm/aws-operator"
-	version            = "14.15.1-dev"
+	version            = "14.13.0"
 )
 
 func Description() string {
