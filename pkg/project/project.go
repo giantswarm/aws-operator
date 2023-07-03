102d6711de13081a8ceb02461495213d8d554982
Francesco Ciocchetti <primeroznl@gmail.com>
2023-07-03 11:15:11 +0200

    Fake version so i can test on gaia without conflict

--- pkg/project/project.go
+++ pkg/project/project.go
@@ -5,7 +5,7 @@ var (
 	gitSHA             = "n/a"
 	name        string = "aws-operator"
 	source      string = "https://github.com/giantswarm/aws-operator"
-	version            = "14.17.2-dev"
+	version            = "14.17.2-dev-fc1"
 )
 
 func Description() string {
