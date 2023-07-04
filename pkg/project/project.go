8022de457fb132f48f58e7fc63578f1b4d6309c6
Francesco Ciocchetti <primeroznl@gmail.com>
2023-07-04 09:40:24 +0200

    revert fake version

--- pkg/project/project.go
+++ pkg/project/project.go
@@ -5,7 +5,7 @@ var (
 	gitSHA             = "n/a"
 	name        string = "aws-operator"
 	source      string = "https://github.com/giantswarm/aws-operator"
-	version            = "14.17.2-dev-fc1"
+	version            = "14.17.2-dev"
 )
 
 func Description() string {
